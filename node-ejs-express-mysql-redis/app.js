95990d4ca5ea58a1919a1fb4a258e8f9eca2a8f2
Draco <draco.coder@gmail.com>
2016-09-21 20:21:01 +0800

    chore: extend node-ejs-express-mysql-redis/app.js

--- node-ejs-express-mysql-redis/app.js
+++ node-ejs-express-mysql-redis/app.js
@@ -88,3 +88,6 @@ if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
+      message: err.message,
+      error: err
+    });
