bbbd5f4f3ec6f77477818bf7058cb544b0715d09
Draco <draco.coder@gmail.com>
2016-12-06 17:23:15 +0800

    chore: extend node-ejs-express-mysql-redis/routes/index.js

--- node-ejs-express-mysql-redis/routes/index.js
+++ node-ejs-express-mysql-redis/routes/index.js
@@ -14,3 +14,4 @@ exports.index = function(req, res){
   res.render('index', { title: 'Gemini,contact:Gemini-90s@Hotmail.com'});
 };
 /*exports.index = function(req, res){
+  res.redirect("/summer_boy/index.html");
