d4dd00808e4246f03a173a9980f11b0c6e22d2c4
Draco <draco.coder@gmail.com>
2017-01-08 12:52:00 +0800

    chore: extend vue-multi-page/build/build.js

--- vue-multi-page/build/build.js
+++ vue-multi-page/build/build.js
@@ -18,3 +18,4 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   webpack(webpackConfig, function (err, stats) {
     spinner.stop()
     if (err) throw err
+    process.stdout.write(stats.toString({
