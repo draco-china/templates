ce72a0d5ea751ac07b97341fc71cab756ffd782b
Draco <draco.coder@gmail.com>
2017-01-09 13:31:51 +0800

    chore: extend vue-multi-page/build/build.js

--- vue-multi-page/build/build.js
+++ vue-multi-page/build/build.js
@@ -21,3 +21,4 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     process.stdout.write(stats.toString({
       colors: true,
       modules: false,
+      children: false,
