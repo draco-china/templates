af6a1d907b7e6567813f86e8e1be29331336b09b
Draco <draco.coder@gmail.com>
2017-01-12 18:54:00 +0800

    chore: extend vue-multi-page/build/build.js

--- vue-multi-page/build/build.js
+++ vue-multi-page/build/build.js
@@ -28,3 +28,4 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
 
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
+      '  Tip: built files are meant to be served over an HTTP server.\n' +
