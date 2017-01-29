6c132f5bab24787ee53c385943b62fb21e176c6a
Draco <draco.coder@gmail.com>
2017-01-29 13:36:04 +0800

    chore: extend vue-multi-page/build/dev-client.js

--- vue-multi-page/build/dev-client.js
+++ vue-multi-page/build/dev-client.js
@@ -4,3 +4,4 @@ var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
 
 hotClient.subscribe(function (event) {
   if (event.action === 'reload') {
+    window.location.reload()
