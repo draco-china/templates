eb7b1929d2b0954604c6684eaf865a64a17469b9
Draco <draco.coder@gmail.com>
2017-01-31 11:14:16 +0800

    chore: extend vue-multi-page/build/dev-server.js

--- vue-multi-page/build/dev-server.js
+++ vue-multi-page/build/dev-server.js
@@ -3,3 +3,4 @@ require('./check-versions')()
 var config = require('../config')
 var configure = require('../config/configure')
 if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
