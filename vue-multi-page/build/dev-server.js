29ab1b9c7ea8d73b59a27016a77bf24544cd93f3
Draco <draco.coder@gmail.com>
2017-02-07 13:45:20 +0800

    chore: extend vue-multi-page/build/dev-server.js

--- vue-multi-page/build/dev-server.js
+++ vue-multi-page/build/dev-server.js
@@ -37,3 +37,4 @@ compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
     hotMiddleware.publish({ action: 'reload' })
     cb()
+  })
