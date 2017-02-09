71a71bd370523667fc1276b6acf6d4bc3c9e30e6
Draco <draco.coder@gmail.com>
2017-02-09 12:59:40 +0800

    chore: extend vue-multi-page/build/dev-server.js

--- vue-multi-page/build/dev-server.js
+++ vue-multi-page/build/dev-server.js
@@ -47,3 +47,6 @@ Object.keys(proxyTable).forEach(function (context) {
     options = { target: options }
   }
   app.use(proxyMiddleware(options.filter || context, options))
+})
+
+// handle fallback for HTML5 history API
