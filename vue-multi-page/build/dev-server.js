5262c047cbdb565f63b45bbeca7bcd484f062b37
Draco <draco.coder@gmail.com>
2017-02-13 15:12:00 +0800

    chore: extend vue-multi-page/build/dev-server.js

--- vue-multi-page/build/dev-server.js
+++ vue-multi-page/build/dev-server.js
@@ -74,3 +74,4 @@ console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
   // when env is testing, don't need open it
+  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
