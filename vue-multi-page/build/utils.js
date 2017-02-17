1be0c5e830da006b06439bf9cbccb6122b5214fe
Draco <draco.coder@gmail.com>
2017-02-17 13:05:50 +0800

    chore: extend vue-multi-page/build/utils.js

--- vue-multi-page/build/utils.js
+++ vue-multi-page/build/utils.js
@@ -2,3 +2,5 @@ var path = require('path')
 var config = require('../config')
 var glob = require('glob');
 // 将样式提取到单独的css文件中，而不是打包到js文件或使用style标签插入在head标签中
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
