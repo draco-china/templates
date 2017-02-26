5352281cb257e4cab7d20744c4dedb9820c4a338
Draco <draco.coder@gmail.com>
2017-02-26 14:26:52 +0800

    chore: extend vue-multi-page/build/utils.js

--- vue-multi-page/build/utils.js
+++ vue-multi-page/build/utils.js
@@ -30,3 +30,4 @@ exports.cssLoaders = function (options) {
         loader: loader + '-loader',
         options: Object.assign({}, loaderOptions, {
           sourceMap: options.sourceMap
+        })
