5cd2048ff58ba5912d7c7aa3cd7c5aba754cfe8b
Draco <draco.coder@gmail.com>
2017-03-10 16:11:15 +0800

    chore: extend vue-multi-page/build/utils.js

--- vue-multi-page/build/utils.js
+++ vue-multi-page/build/utils.js
@@ -91,3 +91,4 @@ exports.getEntries = function (globPath) {
         basename = path.basename(entry, path.extname(entry));
         tmp = entry.split('/').splice(-3);
         pathname = tmp.splice(0, 1) + '/' + basename; // 正确输出js和html的路径
+        entries[pathname] = entry;
