abb48c14229c6470ac36df9361b234f72231fc6d
Draco <draco.coder@gmail.com>
2017-04-10 17:47:54 +0800

    chore: extend vue-multi-page/build/webpack.dev.conf.js

--- vue-multi-page/build/webpack.dev.conf.js
+++ vue-multi-page/build/webpack.dev.conf.js
@@ -8,3 +8,4 @@ var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
