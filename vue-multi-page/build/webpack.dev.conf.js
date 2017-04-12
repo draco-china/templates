495a2cea94cdec2d3e8ce51e79911f0fa32ba435
Draco <draco.coder@gmail.com>
2017-04-12 18:58:01 +0800

    chore: extend vue-multi-page/build/webpack.dev.conf.js

--- vue-multi-page/build/webpack.dev.conf.js
+++ vue-multi-page/build/webpack.dev.conf.js
@@ -18,3 +18,4 @@ module.exports = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
+    new webpack.DefinePlugin({
