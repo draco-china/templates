325abc82ff31892c67e61338e201905f8bc58e28
Draco <draco.coder@gmail.com>
2017-04-13 19:34:37 +0800

    chore: extend vue-multi-page/build/webpack.dev.conf.js

--- vue-multi-page/build/webpack.dev.conf.js
+++ vue-multi-page/build/webpack.dev.conf.js
@@ -20,3 +20,5 @@ module.exports = merge(baseWebpackConfig, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
