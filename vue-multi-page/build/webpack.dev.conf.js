613b30f766268f4c29a119d53f12e79f16272730
Draco <draco.coder@gmail.com>
2017-04-14 09:12:15 +0800

    chore: extend vue-multi-page/build/webpack.dev.conf.js

--- vue-multi-page/build/webpack.dev.conf.js
+++ vue-multi-page/build/webpack.dev.conf.js
@@ -24,3 +24,6 @@ module.exports = merge(baseWebpackConfig, {
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    /*new HtmlWebpackPlugin({
