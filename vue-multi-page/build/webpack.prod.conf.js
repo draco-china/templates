61667e8c9a097c4ac7dd3f941e9927f0036ce6f7
Draco <draco.coder@gmail.com>
2017-04-26 09:29:02 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -8,3 +8,4 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 // 将样式提取到单独的css文件中，而不是打包到js文件或使用style标签插入在head标签中
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
