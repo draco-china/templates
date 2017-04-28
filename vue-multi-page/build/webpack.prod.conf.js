e60ef625e3a507952c18a4d6898a31e9a1bf46c3
Draco <draco.coder@gmail.com>
2017-04-28 17:18:15 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -25,3 +25,5 @@ var webpackConfig = merge(baseWebpackConfig, {
     })
   },
   devtool: config.build.productionSourceMap ? '#source-map' : config.build.productionSourceMap,
+  output: {
+    path: config.build.assetsRoot,
