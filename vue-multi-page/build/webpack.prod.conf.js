732868df86af399c0adb462c1f8f347bcc77e898
Draco <draco.coder@gmail.com>
2017-04-29 19:10:52 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -34,3 +34,5 @@ var webpackConfig = merge(baseWebpackConfig, {
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
+    }),
+    new webpack.optimize.UglifyJsPlugin({
