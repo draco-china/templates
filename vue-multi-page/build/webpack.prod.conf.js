33588030eed5b91769d31e574813f394f29d5e7e
Draco <draco.coder@gmail.com>
2017-05-02 19:29:21 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -50,3 +50,5 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
