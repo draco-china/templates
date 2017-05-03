806fabc36a7251380217b168ceab06be06757ac8
Draco <draco.coder@gmail.com>
2017-05-03 15:29:40 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -60,3 +60,5 @@ var webpackConfig = merge(baseWebpackConfig, {
     // see https://github.com/ampedandwired/html-webpack-plugin
     /*new HtmlWebpackPlugin({
       filename: config.build.index,
+      template: 'index.html',
+      inject: true,
