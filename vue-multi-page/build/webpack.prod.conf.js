764d11c0660bbf81efbab78b0e5cf016ae6a81dc
Draco <draco.coder@gmail.com>
2017-05-04 18:23:24 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -71,3 +71,8 @@ var webpackConfig = merge(baseWebpackConfig, {
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
+    }),*/
+
+    // split vendor js into its own file
+    /*new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
