c15979328ded410080ed8d3d259d582879b52e09
Draco <draco.coder@gmail.com>
2017-05-09 13:58:29 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -95,3 +95,5 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),*/
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendors', // 公共模块的名称
+      chunks: chunks,  // chunks是需要提取的模块
+      minChunks: chunks.length
