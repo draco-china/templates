8dcf3f21960c2d1903534e4ca89a457ad375e7a6
Draco <draco.coder@gmail.com>
2017-05-12 15:28:15 +0800

    chore: extend vue-multi-page/build/webpack.prod.conf.js

--- vue-multi-page/build/webpack.prod.conf.js
+++ vue-multi-page/build/webpack.prod.conf.js
@@ -113,3 +113,5 @@ if (config.build.productionGzip) {
   var CompressionWebpackPlugin = require('compression-webpack-plugin')
 
   webpackConfig.plugins.push(
+    new CompressionWebpackPlugin({
+      asset: '[path].gz[query]',
