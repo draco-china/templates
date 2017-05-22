ad914a9cd924456f9b8cf0f4ef4e1bb4cafad60a
Draco <draco.coder@gmail.com>
2017-05-22 20:20:26 +0800

    chore: extend vue-multi-page/config/index.js

--- vue-multi-page/config/index.js
+++ vue-multi-page/config/index.js
@@ -8,3 +8,4 @@ module.exports = {
     //index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: configure.ProjectDirectory,
+    assetsPublicPath: '/',
