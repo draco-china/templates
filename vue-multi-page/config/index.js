7cda2b49ce43e483ce92b22ad006d7a0f0f74ce5
Draco <draco.coder@gmail.com>
2017-05-23 12:38:15 +0800

    chore: extend vue-multi-page/config/index.js

--- vue-multi-page/config/index.js
+++ vue-multi-page/config/index.js
@@ -9,3 +9,8 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: configure.ProjectDirectory,
     assetsPublicPath: '/',
+    productionSourceMap: false,//是否显示源码
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
