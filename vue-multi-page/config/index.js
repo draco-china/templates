23e4ec768f3d474c50257b6921e002a16f9f5545
Draco <draco.coder@gmail.com>
2017-05-23 12:47:25 +0800

    chore: extend vue-multi-page/config/index.js

--- vue-multi-page/config/index.js
+++ vue-multi-page/config/index.js
@@ -14,3 +14,4 @@ module.exports = {
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
