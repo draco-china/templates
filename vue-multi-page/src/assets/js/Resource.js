f42e8d10560a0a8ade99b19f0afc6b7a93b605e8
Draco <draco.coder@gmail.com>
2017-08-15 17:06:24 +0800

    chore: extend vue-multi-page/src/assets/js/Resource.js

--- vue-multi-page/src/assets/js/Resource.js
+++ vue-multi-page/src/assets/js/Resource.js
@@ -64,3 +64,4 @@ var Resource = function() {
         Vue.http.post(url,data,options).then(function(response){
             response = response.data;
             if(response.code == 0){
+                if(success) success(response);
