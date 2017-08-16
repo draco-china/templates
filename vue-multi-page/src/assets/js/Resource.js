382603bba958151eeccb26f320a6758ab02a2523
Draco <draco.coder@gmail.com>
2017-08-16 16:32:03 +0800

    chore: extend vue-multi-page/src/assets/js/Resource.js

--- vue-multi-page/src/assets/js/Resource.js
+++ vue-multi-page/src/assets/js/Resource.js
@@ -65,3 +65,4 @@ var Resource = function() {
             response = response.data;
             if(response.code == 0){
                 if(success) success(response);
+            }else if(response.code ==1){ //请求检测未登录处理
