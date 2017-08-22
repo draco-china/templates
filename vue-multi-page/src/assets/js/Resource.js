a6b1cf8e5885b55163b4c65c91e0f084958b85a8
Draco <draco.coder@gmail.com>
2017-08-22 09:21:16 +0800

    chore: extend vue-multi-page/src/assets/js/Resource.js

--- vue-multi-page/src/assets/js/Resource.js
+++ vue-multi-page/src/assets/js/Resource.js
@@ -81,3 +81,4 @@ var Resource = function() {
     // 获取地址栏参数
     var getUrlParam = function(name){
         var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
