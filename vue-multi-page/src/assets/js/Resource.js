f555168187bde0e5644059ef721202cc757e99ec
Draco <draco.coder@gmail.com>
2017-08-22 13:41:59 +0800

    chore: extend vue-multi-page/src/assets/js/Resource.js

--- vue-multi-page/src/assets/js/Resource.js
+++ vue-multi-page/src/assets/js/Resource.js
@@ -84,3 +84,4 @@ var Resource = function() {
         var r = window.location.search.substr(1).match(reg);  //匹配目标参数
         if (r!=null) return unescape(r[2]); return null; //返回参数值
     };
+    return {
