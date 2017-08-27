6c1c06e5786a697234da97f2a2631e3889c8d93d
Draco <draco.coder@gmail.com>
2017-08-27 19:31:05 +0800

    chore: extend vue-multi-page/src/assets/js/lib.js

--- vue-multi-page/src/assets/js/lib.js
+++ vue-multi-page/src/assets/js/lib.js
@@ -6,3 +6,6 @@ import vueResource from 'vue-resource'
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 import '@/assets/css/all.css'  //引入公共样式
+import '@/assets/css/icon.css' //字体图标样式
+
+// 注册组件
