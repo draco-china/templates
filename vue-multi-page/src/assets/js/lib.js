54214d0a506837d5d119b85023c4fa26acc32358
Draco <draco.coder@gmail.com>
2017-09-10 10:47:19 +0800

    chore: extend vue-multi-page/src/assets/js/lib.js

--- vue-multi-page/src/assets/js/lib.js
+++ vue-multi-page/src/assets/js/lib.js
@@ -59,3 +59,4 @@ export default{
             Vue.http.post(url,data,options).then(function(response){
                 response = response.data;
                 if(response.code == 0){
+                    if(success) success(response);
