46d109ed0e0cb119ed444b013d4f5d1fd4172815
Draco <draco.coder@gmail.com>
2017-09-16 18:31:52 +0800

    chore: add vue-multi-page/src/assets/js/proving.js

---
+++ vue-multi-page/src/assets/js/proving.js
@@ -0,0 +1,8 @@
+/**
+ * Created by gemini on 2017/3/15.
+ */
+var proving = function(){
+	//验证特殊字符
+	//返回false表示有特殊字符
+	function checkStr(str){
+		if(!!str.match(/^(([^\^\.<>%&',;=?$"':#@!~\]\[{}\\/`\|])*)$/)){ 
