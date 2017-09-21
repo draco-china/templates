446fb62f4a932a2f77fff6ac452303fd2e90a036
Draco <draco.coder@gmail.com>
2017-09-21 17:23:35 +0800

    chore: extend vue-multi-page/src/assets/js/proving.js

--- vue-multi-page/src/assets/js/proving.js
+++ vue-multi-page/src/assets/js/proving.js
@@ -91,3 +91,8 @@ var proving = function(){
 	    {  
 	        alert('您输入的身份证号码不正确,请重新输入');  
 	        document.getElementById('card_no').focus;  
+	        return false;  
+	    }  
+	    //校验生日  
+	    if(checkBirthday(card) === false)  
+	    {  
