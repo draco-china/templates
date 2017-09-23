2f94f454371ce2233e3dcfebe0744f641f658b6e
Draco <draco.coder@gmail.com>
2017-09-23 09:02:06 +0800

    chore: extend vue-multi-page/src/assets/js/proving.js

--- vue-multi-page/src/assets/js/proving.js
+++ vue-multi-page/src/assets/js/proving.js
@@ -107,3 +107,8 @@ var proving = function(){
 	        document.getElementById('card_no').focus();  
 	        return false;  
 	    }  
+	    return true;  
+		//检查号码是否符合规范，包括长度，类型  
+		isCardNo = function(card)  
+		{  
+		    //身份证号码为15位或者18位，15位时全为数字，18位前17位为数字，最后一位是校验位，可能为数字或字符X  
