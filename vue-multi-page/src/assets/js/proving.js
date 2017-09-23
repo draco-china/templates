dd4b910ef0ef2fe863e0f64bd61ad85db2a12640
Draco <draco.coder@gmail.com>
2017-09-23 11:14:07 +0800

    chore: extend vue-multi-page/src/assets/js/proving.js

--- vue-multi-page/src/assets/js/proving.js
+++ vue-multi-page/src/assets/js/proving.js
@@ -112,3 +112,6 @@ var proving = function(){
 		isCardNo = function(card)  
 		{  
 		    //身份证号码为15位或者18位，15位时全为数字，18位前17位为数字，最后一位是校验位，可能为数字或字符X  
+		    var reg = /(^\d{15}$)|(^\d{17}(\d|X)$)/;  
+		    if(reg.test(card) === false)  
+		    {  
