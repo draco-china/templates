6a87b3a4a3c4e6d151065687f8b4d303f247b3ba
Draco <draco.coder@gmail.com>
2017-09-25 12:25:09 +0800

    chore: extend vue-multi-page/src/assets/js/proving.js

--- vue-multi-page/src/assets/js/proving.js
+++ vue-multi-page/src/assets/js/proving.js
@@ -149,3 +149,6 @@ var proving = function(){
 		    }  
 		    //身份证18位时，次序为省（3位）市（3位）年（4位）月（2位）日（2位）校验位（4位），校验位末尾可能为X  
 		    if(len == '18')  
+		    {  
+		        var re_eighteen = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/;  
+		        var arr_data = card.match(re_eighteen);  
