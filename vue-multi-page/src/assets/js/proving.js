c2f153acbcdc8cccb2f4c55fd6fdb3e5017935c3
Draco <draco.coder@gmail.com>
2017-10-02 16:58:16 +0800

    chore: extend vue-multi-page/src/assets/js/proving.js

--- vue-multi-page/src/assets/js/proving.js
+++ vue-multi-page/src/assets/js/proving.js
@@ -231,3 +231,7 @@ var proving = function(){
     // 1.将未带校验位的 15（或18）位卡号从右依次编号 1 到 15（18），位于奇数位号上的数字乘以 2。
     // 2.将奇位乘积的个十位全部相加，再加上所有偶数位上的数字。
     // 3.将加法和加上校验位能被 10 整除。
+    function checkLuhm(bankno){
+		if (bankno.length < 16 || bankno.length > 19) {
+			//$("#banknoInfo").html("银行卡号长度必须在16到19之间");
+			return false;
