a4619e85a8cc514e7c13bfece2950ff895c6af7e
Draco <draco.coder@gmail.com>
2017-10-15 17:26:57 +0800

    chore: extend vue-multi-page/src/components/Hello/Hello.vue

--- vue-multi-page/src/components/Hello/Hello.vue
+++ vue-multi-page/src/components/Hello/Hello.vue
@@ -5,3 +5,4 @@
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
