47d83415752e76cc55452204de351bbc6495e3d9
Draco <draco.coder@gmail.com>
2017-10-16 15:35:29 +0800

    chore: extend vue-multi-page/src/components/Hello/Hello.vue

--- vue-multi-page/src/components/Hello/Hello.vue
+++ vue-multi-page/src/components/Hello/Hello.vue
@@ -8,3 +8,4 @@
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
