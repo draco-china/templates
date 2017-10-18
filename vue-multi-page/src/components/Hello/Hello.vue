62e9a4ceb7d5f5e559139647a957bc9de3ba4239
Draco <draco.coder@gmail.com>
2017-10-18 09:23:27 +0800

    chore: extend vue-multi-page/src/components/Hello/Hello.vue

--- vue-multi-page/src/components/Hello/Hello.vue
+++ vue-multi-page/src/components/Hello/Hello.vue
@@ -14,3 +14,4 @@
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
