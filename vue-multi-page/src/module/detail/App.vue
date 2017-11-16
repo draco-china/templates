4b9eed9e420b02b900698705a9f831391cee3fd8
Draco <draco.coder@gmail.com>
2017-11-16 18:06:06 +0800

    chore: extend vue-multi-page/src/module/detail/App.vue

--- vue-multi-page/src/module/detail/App.vue
+++ vue-multi-page/src/module/detail/App.vue
@@ -80,3 +80,4 @@
             lib.get("/user/alcohol/hasFillQuestionare",{sampleNumber:1},function (data) {
                 if(!data.data){
                     window.location.href="/h5/slightlydrunk/information.html?sampleNumber="+sampleNumber;
+                } else {
