b0e76df9b0bb956965e70a9d3d37f88160b55d53
Draco <draco.coder@gmail.com>
2017-12-01 17:09:17 +0800

    chore: extend vue-multi-page/src/module/index/App.vue

--- vue-multi-page/src/module/index/App.vue
+++ vue-multi-page/src/module/index/App.vue
@@ -2,3 +2,4 @@
   <div id="app">
     <mu-refresh-control :refreshing="refreshing" :trigger="trigger" @refresh="refresh"/>
     <mu-appbar title="首页">
+    <mu-icon-button icon="close" slot="left"/>
