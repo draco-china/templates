d9082df4a15a16d18004450305b556e306595729
Draco <draco.coder@gmail.com>
2017-12-02 15:52:07 +0800

    chore: extend vue-multi-page/src/module/index/App.vue

--- vue-multi-page/src/module/index/App.vue
+++ vue-multi-page/src/module/index/App.vue
@@ -6,3 +6,5 @@
     <mu-icon-menu icon="more_vert" slot="right">
       <mu-menu-item title="菜单 1"/>
       <mu-menu-item title="菜单 2"/>
+      <mu-menu-item title="菜单 3"/>
+      <mu-menu-item title="菜单 4"/>
