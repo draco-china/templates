063a310d38899d7578c7d3a9abf4e9445a0381e0
Draco <draco.coder@gmail.com>
2017-12-10 13:31:04 +0800

    chore: extend vue-multi-page/src/module/index/App.vue

--- vue-multi-page/src/module/index/App.vue
+++ vue-multi-page/src/module/index/App.vue
@@ -41,3 +41,5 @@
    <mu-paper :zDepth="5" style="position:fixed;bottom:0;left:0;width:100%">
       <mu-bottom-nav :value="bottomNav" @change="handleChange">
         <mu-bottom-nav-item value="recents" title="Recents" icon="restore"/>
+        <mu-bottom-nav-item value="favorites" title="Favorites" icon="favorite"/>
+        <mu-bottom-nav-item value="nearby" title="Nearby" icon="location_on"/>
