0cf1b54a6f7a550982e496c630ee216ef2cc8631
Draco <draco.coder@gmail.com>
2018-01-02 09:54:22 +0800

    chore: extend nuxt-express-template/.eslintrc.js

--- nuxt-express-template/.eslintrc.js
+++ nuxt-express-template/.eslintrc.js
@@ -8,3 +8,37 @@ module.exports = {
   },
   parserOptions: {
     sourceType: 'module'
+  },
+  //  https:// github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  // extends: 'standard',
+  //  required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  //  add your custom rules here
+  rules: {
+
+    /*Possible Errors*/
+
+    // 数组和对象键值对最后一个逗号，
+
+    // never参数：不能带末尾的逗号,
+
+    // always参数：必须带末尾的逗号，
+
+    // always-multiline：多行模式必须带逗号，单行模式不能带逗号
+    "comma-dangle": [0, "never"],
+
+    // 禁止在条件表达式中使用赋值语句
+    "no-cond-assign": 2,
+
+    // 禁止使用console
+    "no-console": 0,
+
+    // 禁止在条件中使用常量表达式 if(true) if(1)
+    "no-constant-condition": 2,
+
+    // 禁止在正则表达式中使用控制符
+    "no-control-regex": 2,
+
+    // 禁止使用debugger语句
