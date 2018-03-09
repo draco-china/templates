524a6f5498dbeb3ee77f34857040f4f690b31bd5
Draco <draco.coder@gmail.com>
2018-03-09 14:43:50 +0800

    chore: extend nuxt-express-template/filters/html-filter.js

--- nuxt-express-template/filters/html-filter.js
+++ nuxt-express-template/filters/html-filter.js
@@ -10,3 +10,4 @@
 export const textOverflow = (text, length) => {
   const _length = length || text.length
   const cansub = _length && text && text.length > _length
+  return cansub ? text.substr(0, _length) + '...' : text
