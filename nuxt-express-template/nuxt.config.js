2ef39ac786c8986afe106d1b493d3fb809701c23
Draco <draco.coder@gmail.com>
2018-04-10 20:40:25 +0800

    chore: extend nuxt-express-template/nuxt.config.js

--- nuxt-express-template/nuxt.config.js
+++ nuxt-express-template/nuxt.config.js
@@ -3,3 +3,8 @@ module.exports = {
     // 为JS和Vue文件定制babel配置。https://nuxtjs.org/api/configuration-build/#analyze
     babel: {
       presets: ['es2015', 'stage-2'],
+      plugins: [
+        'transform-async-to-generator',
+        'transform-runtime'
+      ],
+      comments: true
