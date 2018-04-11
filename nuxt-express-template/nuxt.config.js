2ee7ad2d558bbc929bf1dd1b2911c6322dbbd257
Draco <draco.coder@gmail.com>
2018-04-11 16:38:30 +0800

    chore: extend nuxt-express-template/nuxt.config.js

--- nuxt-express-template/nuxt.config.js
+++ nuxt-express-template/nuxt.config.js
@@ -22,3 +22,8 @@ module.exports = {
     extend (config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
