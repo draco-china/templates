c8a481b8bd179ba10deda78fd68adb8f9265a08c
Draco <draco.coder@gmail.com>
2018-04-18 19:02:46 +0800

    chore: extend nuxt-express-template/pages/_id.vue

--- nuxt-express-template/pages/_id.vue
+++ nuxt-express-template/pages/_id.vue
@@ -1,3 +1,7 @@
 <template>
   <section class="container">
     <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <h1 class="title">
+      User
+    </h1>
+    <h2 class="info">
