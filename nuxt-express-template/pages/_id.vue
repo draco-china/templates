ec2103fec8e593e84fab453238d612f68f83e19c
Draco <draco.coder@gmail.com>
2018-04-19 19:14:58 +0800

    chore: extend nuxt-express-template/pages/_id.vue

--- nuxt-express-template/pages/_id.vue
+++ nuxt-express-template/pages/_id.vue
@@ -18,3 +18,7 @@ import axios from '~/plugins/axios'
 
 export default {
   name: 'id',
+  asyncData ({ params, error }) {
+    return axios.get('/api/users/' + params.id)
+      .then((res) => {
+        return { user: res.data }
