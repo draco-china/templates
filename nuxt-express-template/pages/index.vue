34543c87bc36780f610c22afa5ed42350a51ac72
Draco <draco.coder@gmail.com>
2018-05-01 11:31:14 +0800

    chore: extend nuxt-express-template/pages/index.vue

--- nuxt-express-template/pages/index.vue
+++ nuxt-express-template/pages/index.vue
@@ -4,3 +4,7 @@
     <h1 class="title">
       USERS
     </h1>
+    <ul class="users">
+      <li v-for="(user, index) in users" :key="index" class="user">
+        <nuxt-link :to="{ name: 'id', params: { id: index }}">
+          {{ user.name }}
