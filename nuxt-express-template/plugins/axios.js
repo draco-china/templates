08be5f66713fcb33dbca912fb4bd30444e625533
Draco <draco.coder@gmail.com>
2018-05-06 15:02:41 +0800

    chore: extend nuxt-express-template/plugins/axios.js

--- nuxt-express-template/plugins/axios.js
+++ nuxt-express-template/plugins/axios.js
@@ -4,3 +4,10 @@ import qs from 'qs'
 
 let options = {}
 // The server-side needs a full url to works
+if (process.server) {
+  options.baseURL =  process.env.baseUrl
+}
+// 名称: 全替换函数
+// 功能: 把json中所有的undefined值替换成空字符串
+// 返回: 处理完成的json
+// 参数: json 指定的json
