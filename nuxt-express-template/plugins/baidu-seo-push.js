5c88a78298ec6e22bee3f9d502ebbd0d0e0ce683
Draco <draco.coder@gmail.com>
2018-05-13 13:01:04 +0800

    chore: extend nuxt-express-template/plugins/baidu-seo-push.js

--- nuxt-express-template/plugins/baidu-seo-push.js
+++ nuxt-express-template/plugins/baidu-seo-push.js
@@ -12,3 +12,4 @@ if (process.env.NODE_ENV === 'production') {
       let e = /([http|https]:\/\/[a-zA-Z0-9\_\.]+\.baidu\.com)/gi
       let r = href || window.location.href
       let t = document.referrer
+      if (!e.test(r)) {
