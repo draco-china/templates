374fd7c5c6daf53f7cb6e6e2e36705be6a01ce98
Draco <draco.coder@gmail.com>
2018-05-14 16:03:16 +0800

    chore: extend nuxt-express-template/plugins/baidu-seo-push.js

--- nuxt-express-template/plugins/baidu-seo-push.js
+++ nuxt-express-template/plugins/baidu-seo-push.js
@@ -16,3 +16,4 @@ if (process.env.NODE_ENV === 'production') {
         let o = "https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif"
         t ? (o += "?r=" + encodeURIComponent(document.referrer), r && (o += "&l=" + r)) : r && (o += "?l=" + r)
         let i = new Image
+        i.src = o
