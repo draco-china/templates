f87886a56600d706ca400f0eef98feb24c66c72f
Draco <draco.coder@gmail.com>
2018-05-17 18:44:29 +0800

    chore: extend nuxt-express-template/plugins/baidu-seo-push.js

--- nuxt-express-template/plugins/baidu-seo-push.js
+++ nuxt-express-template/plugins/baidu-seo-push.js
@@ -28,3 +28,4 @@ if (process.env.NODE_ENV === 'production') {
   */
   window.onNuxtReady((app) => {
     app.$nuxt.$on('routeChanged', (to, from) => {
+      baiduPush(window.location.origin + to.fullPath)
