0d34c3d092346bec7a96a3be29cdeebc520fa6bd
Draco <draco.coder@gmail.com>
2018-05-23 20:19:28 +0800

    chore: extend nuxt-express-template/plugins/copy-right.js

--- nuxt-express-template/plugins/copy-right.js
+++ nuxt-express-template/plugins/copy-right.js
@@ -31,3 +31,6 @@ if (process.env.NODE_ENV === 'production') {
     e.clipboardData.setData('text/plain', buildText(content))
     e.clipboardData.setData('text/html', buildHtml(content))
     e.preventDefault()
+  })
+
+}
