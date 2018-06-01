362b861f0cbedf7fb677ee83a622322584efd2b2
Draco <draco.coder@gmail.com>
2018-06-01 13:46:04 +0800

    chore: extend nuxt-express-template/plugins/offline.js

--- nuxt-express-template/plugins/offline.js
+++ nuxt-express-template/plugins/offline.js
@@ -3,3 +3,4 @@ if (process.env.NODE_ENV === 'production') {
   const OfflinePlugin = require('offline-plugin/runtime')
   window.onNuxtReady(() => {
     OfflinePlugin.install({
+      onInstalled: function () {
