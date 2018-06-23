dd70cd0de14a25d1c29142ff1ad78be315f63026
Draco <draco.coder@gmail.com>
2018-06-23 16:03:53 +0800

    chore: extend nuxt-express-template/store/index.js

--- nuxt-express-template/store/index.js
+++ nuxt-express-template/store/index.js
@@ -10,3 +10,4 @@ export const actions = {
   // 全局服务初始化
   nuxtServerInit(store, { params, route, isServer, req }) {
     // 检查设备类型
+    const userAgent = isServer ? req.headers['user-agent'] : navigator.userAgent
