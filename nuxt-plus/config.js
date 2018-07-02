62a8b79e685483f41d389df62044f483ba77360e
Draco <draco.coder@gmail.com>
2018-07-02 11:45:53 +0800

    chore: extend nuxt-plus/config.js

--- nuxt-plus/config.js
+++ nuxt-plus/config.js
@@ -4,3 +4,9 @@ var path = process.argv[2], // 请输入项目路径
     exec = require('child_process').exec,
     fs   = require('fs'),
     dir  = './file',
+    __dirname = path;
+
+function fileMerge(fileSource, exportFilePath) {
+
+  var readFiles = [];
+  var newFileSize = 0;
