86bd85b79e335db272b1e7b2b4e7d02984a66c78
Draco <draco.coder@gmail.com>
2018-07-15 16:16:39 +0800

    chore: extend nuxt-plus/file/.eslintrc.js

--- nuxt-plus/file/.eslintrc.js
+++ nuxt-plus/file/.eslintrc.js
@@ -8,3 +8,25 @@ module.exports = {
   },
   parserOptions: {
     sourceType: 'module'
+  },
+  //  https:// github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  // extends: 'standard',
+  //  required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  //  add your custom rules here
+  rules: {
+
+    /*Possible Errors*/
+
+    // 数组和对象键值对最后一个逗号，
+
+    // never参数：不能带末尾的逗号,
+
+    // always参数：必须带末尾的逗号，
+
+    // always-multiline：多行模式必须带逗号，单行模式不能带逗号
+    "comma-dangle": [0, "never"],
+
+    // 禁止在条件表达式中使用赋值语句
