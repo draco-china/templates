a6debe982cce5d38bd9a67d0f5603c8344a0f323
Draco <draco.coder@gmail.com>
2018-07-16 09:12:10 +0800

    chore: extend nuxt-plus/file/.eslintrc.js

--- nuxt-plus/file/.eslintrc.js
+++ nuxt-plus/file/.eslintrc.js
@@ -45,3 +45,21 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === 'production' ? 2 : 0,
 
     // 函数参数禁止重名
+    "no-dupe-args": 2,
+
+    // 在创建对象字面量时不允许键重复
+    "no-dupe-keys": 2,
+
+    // 在switch语句中禁止重复的case
+    "no-duplicate-case": 2,
+
+    // 代码块的内容不能为空，禁止空代码块
+    "no-empty": 2,
+
+    // 正则表达式的内容不能为空，禁止使用不匹配任何字符串的正则表达式
+    "no-empty-character-class": 2,
+
+    // 禁止对catch语句中的异常进行赋值
+    "no-ex-assign": 2,
+
+    // 禁止不必要的bool转换
