0b6524905d1a37a3dd7e15d8e916050710e5ed59
Draco <draco.coder@gmail.com>
2018-07-23 17:57:28 +0800

    chore: extend nuxt-plus/file/.eslintrc.js

--- nuxt-plus/file/.eslintrc.js
+++ nuxt-plus/file/.eslintrc.js
@@ -556,3 +556,18 @@ module.exports = {
     "no-lonely-if": 0,
 
     // 禁止混用tab和空格
+    "no-mixed-spaces-and-tabs": 2,
+
+    // 不要留超过规定数目的空白行
+    "no-multiple-empty-lines": [2, { "max": 3 }],
+
+    // 在if语句中使用了否定表达式，同时else语句又不为空，那么这样的if-else语句将被视为不合法，为什么不将其反过来这样代码更容易理解，该规则同样适用于三元操作符
+    "no-negated-condition": 0,
+
+    // 三元操作符禁止嵌套
+    "no-nested-ternary": 0,
+
+    // 禁止使用new Object()来构造对象
+    "no-new-object": 0,
+
+    // 禁止使用++，--
