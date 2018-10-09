fbe5e11de33fe97a52b5052b376aa2f61bf926e6
Draco <draco.coder@gmail.com>
2018-10-09 16:04:38 +0800

    chore: extend nuxt-plus/file/filters/html-filter.js

--- nuxt-plus/file/filters/html-filter.js
+++ nuxt-plus/file/filters/html-filter.js
@@ -11,3 +11,6 @@ export const textOverflow = (text, length) => {
   const _length = length || text.length
   const cansub = _length && text && text.length > _length
   return cansub ? text.substr(0, _length) + '...' : text
+}
+
+// 首字母大写
