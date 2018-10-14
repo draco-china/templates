7f2e599c98b179246b2af1edd55af5677f1c8887
Draco <draco.coder@gmail.com>
2018-10-14 11:00:24 +0800

    chore: extend nuxt-plus/file/filters/time-filter.js

--- nuxt-plus/file/filters/time-filter.js
+++ nuxt-plus/file/filters/time-filter.js
@@ -14,3 +14,4 @@ const pluralize = (time, label) => {
 // 相对时间过滤器，传入时间，返回距离今天有多久
 export const timeAgo = time => {
   time = time instanceof Date ? time : new Date(time)
+  const between = Date.now() / 1000 - Number(time) / 1000
