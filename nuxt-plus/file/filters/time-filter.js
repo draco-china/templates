8c2cae43c27f8a57d958a02de88af74c563df5e6
Draco <draco.coder@gmail.com>
2018-10-14 18:36:01 +0800

    chore: extend nuxt-plus/file/filters/time-filter.js

--- nuxt-plus/file/filters/time-filter.js
+++ nuxt-plus/file/filters/time-filter.js
@@ -16,3 +16,4 @@ export const timeAgo = time => {
   time = time instanceof Date ? time : new Date(time)
   const between = Date.now() / 1000 - Number(time) / 1000
   if (between < 3600) {
+    if (Object.is(~~(between / 60), 0)) {
