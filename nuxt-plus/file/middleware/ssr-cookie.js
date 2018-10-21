556f2905a7a31db45167010b97468088c8586023
Draco <draco.coder@gmail.com>
2018-10-21 15:21:35 +0800

    chore: extend nuxt-plus/file/middleware/ssr-cookie.js

--- nuxt-plus/file/middleware/ssr-cookie.js
+++ nuxt-plus/file/middleware/ssr-cookie.js
@@ -3,3 +3,4 @@ import axios from '~/plugins/axios'
 export default function ({ isServer, req, res }, next) {
   if (isServer) {
     if(req.headers.cookie != undefined) {
+      axios.defaults.headers.common.cookie = req.headers.cookie
