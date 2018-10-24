b7ec8bdaf1aecf1e5271465d91296731979fdf68
Draco <draco.coder@gmail.com>
2018-10-24 10:30:14 +0800

    chore: add nuxt-plus/file/nuxt.config.js

---
+++ nuxt-plus/file/nuxt.config.js
@@ -0,0 +1,10 @@
+
+
+/*---------- START ----------*/
+/********** DO NOT MODIFY **********/
+/**
+ * 将值插入数组中
+ * @param path // module.exports下的路径
+ * @param value // 需要push到数组的值或者对象
+ */
+function pushArr(path, value) {
