a3401b3d3417e94b33217ec9b367c62661b1997b
Draco <draco.coder@gmail.com>
2018-11-02 14:01:13 +0800

    chore: extend nuxt-plus/file/nuxt.config.js

--- nuxt-plus/file/nuxt.config.js
+++ nuxt-plus/file/nuxt.config.js
@@ -47,3 +47,4 @@ function pushMap(path, key, value) {
 }
 // 为JS和Vue文件定制babel配置。https://nuxtjs.org/api/configuration-build/#analyze
 pushMap('build.babel', 'presets', ['es2015', 'stage-2'])
+pushMap('build.babel', 'plugins', ['transform-async-to-generator', 'transform-runtime'])
