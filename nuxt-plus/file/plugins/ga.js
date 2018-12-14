6dc1fed0da73440786c12545f32cd3737b4d2036
Draco <draco.coder@gmail.com>
2018-12-14 19:53:03 +0800

    chore: extend nuxt-plus/file/plugins/ga.js

--- nuxt-plus/file/plugins/ga.js
+++ nuxt-plus/file/plugins/ga.js
@@ -6,3 +6,4 @@ if (process.env.NODE_ENV === 'production') {
   ** Google 统计分析脚本
   */
   (function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)}, i[r].l=1*new Date();a=s.createElement(o),
