da4f3a8d4b90b4571dec6539685d8109dc903e61
Draco <draco.coder@gmail.com>
2018-12-15 14:11:29 +0800

    chore: extend nuxt-plus/file/plugins/ga.js

--- nuxt-plus/file/plugins/ga.js
+++ nuxt-plus/file/plugins/ga.js
@@ -8,3 +8,8 @@ if (process.env.NODE_ENV === 'production') {
   (function(i, s, o, g, r, a, m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)}, i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+  /*
+  ** 当前页的访问统计
+  */
+  // 记得将 UA-XXXXXXXX-X 替换成你的 Google 统计分析服务的跟踪编号
