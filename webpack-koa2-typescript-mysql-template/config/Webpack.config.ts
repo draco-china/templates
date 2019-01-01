89ff0ee25945050eccd1699fa2db52ca326d023e
Draco <draco.coder@gmail.com>
2019-01-01 18:07:57 +0800

    chore: extend webpack-koa2-typescript-mysql-template/config/Webpack.config.ts

--- webpack-koa2-typescript-mysql-template/config/Webpack.config.ts
+++ webpack-koa2-typescript-mysql-template/config/Webpack.config.ts
@@ -2,3 +2,5 @@ import * as path from 'path';
 import * as StartServerPlugin from 'start-server-webpack-plugin';
 import * as webpack from 'webpack';
 import * as nodeExternals from 'webpack-node-externals';
+import { Configuration, ExternalsElement } from 'webpack';
+
