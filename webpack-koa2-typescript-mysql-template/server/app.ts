50c11f2fd0e46d9c28883acede7a35722af11c1c
Draco <draco.coder@gmail.com>
2019-02-19 17:11:27 +0800

    chore: extend webpack-koa2-typescript-mysql-template/server/app.ts

--- webpack-koa2-typescript-mysql-template/server/app.ts
+++ webpack-koa2-typescript-mysql-template/server/app.ts
@@ -2,3 +2,5 @@ import * as Koa from 'koa';
 import * as bodyparser from 'koa-bodyparser';
 import * as http from 'http';
 import { router } from './routers/index';
+import { error, cors } from './middleware';
+
