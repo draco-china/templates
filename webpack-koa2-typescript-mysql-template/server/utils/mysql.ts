7bbd69cd9e01ada2cd332c6207878948b6aecbc0
Draco <draco.coder@gmail.com>
2019-03-27 18:09:46 +0800

    chore: extend webpack-koa2-typescript-mysql-template/server/utils/mysql.ts

--- webpack-koa2-typescript-mysql-template/server/utils/mysql.ts
+++ webpack-koa2-typescript-mysql-template/server/utils/mysql.ts
@@ -5,3 +5,4 @@ export default (sql: string, params: Array<any>, callback: any) => {
   const connection = mysql.createConnection(CONF.DATABASE);
   connection.connect(err => {
     if (err) {
+      console.log('数据库连接失败');
