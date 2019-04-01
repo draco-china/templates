854e4c073571082222e75adf97a06ac2750b3c46
Draco <draco.coder@gmail.com>
2019-04-01 10:49:02 +0800

    chore: add nestjs-base-instance/src/app/app.config.ts

---
+++ nestjs-base-instance/src/app/app.config.ts
@@ -0,0 +1,10 @@
+/**
+ * @Description: 全局配置参数
+ * @Module: config
+ * @Author: Daker
+ * @Email: daker.zhou@gmail.com
+ * @Github: https://github.com/daker-china
+ * @Date: 2018-12-19 17:01:10
+ * @LastEditTime: 2018-12-29 18:30:43
+ */
+import { argv } from 'yargs';
