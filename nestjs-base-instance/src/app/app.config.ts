b4a1c67f27fdacfe6223dcc390ad67e936f8a7f0
Draco <draco.coder@gmail.com>
2019-04-10 12:41:33 +0800

    chore: extend nestjs-base-instance/src/app/app.config.ts

--- nestjs-base-instance/src/app/app.config.ts
+++ nestjs-base-instance/src/app/app.config.ts
@@ -32,3 +32,5 @@ export const MONGODB = {
   uri: `mongodb://127.0.0.1:${argv.db_port || 27017}/`,
   username: argv.db_username || 'DB_username',
   password: argv.db_password || 'DB_password'
+};
+
