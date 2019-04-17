0f5c7b4757eb271d85337be865c3bffb5fe626ce
Draco <draco.coder@gmail.com>
2019-04-17 11:12:25 +0800

    chore: extend nestjs-base-instance/src/app/app.config.ts

--- nestjs-base-instance/src/app/app.config.ts
+++ nestjs-base-instance/src/app/app.config.ts
@@ -44,3 +44,4 @@ export const INFO = {
   author: packages.author,
   site: packages.author.url,
   github: 'https://github.com/daker-china',
+  powered: ['Vue', 'Nuxt.js', 'ReactNative', 'Nodejs', 'MongoDB', 'Koa2', 'Nginx', 'TypeScript']
