670523c52599351b55b66ad4fd5cce0658d8823c
Draco <draco.coder@gmail.com>
2019-04-19 17:43:43 +0800

    chore: extend nestjs-base-instance/src/app/app.controller.spec.ts

--- nestjs-base-instance/src/app/app.controller.spec.ts
+++ nestjs-base-instance/src/app/app.controller.spec.ts
@@ -2,3 +2,4 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import * as APP_CONFIG from './app.config';
 
+describe('AppController', () => {
