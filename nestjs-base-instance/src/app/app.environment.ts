150f6ed4bc228548431b48d23ac022356a80f306
Draco <draco.coder@gmail.com>
2019-05-03 18:22:03 +0800

    chore: extend nestjs-base-instance/src/app/app.environment.ts

--- nestjs-base-instance/src/app/app.environment.ts
+++ nestjs-base-instance/src/app/app.environment.ts
@@ -4,3 +4,4 @@ export const isDevMode = Object.is(environment, 'development');
 export const isProdMode = Object.is(environment, 'production');
 export const isTestMode = Object.is(environment, 'test');
 
+export default {
