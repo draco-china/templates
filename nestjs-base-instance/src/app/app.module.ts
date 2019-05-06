9e939c67bb72c8b3d4f9c84c05863fd7cfdde0ad
Draco <draco.coder@gmail.com>
2019-05-06 15:52:09 +0800

    chore: extend nestjs-base-instance/src/app/app.module.ts

--- nestjs-base-instance/src/app/app.module.ts
+++ nestjs-base-instance/src/app/app.module.ts
@@ -1,3 +1,4 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from '@/app/app.controller';
+import { CorsMiddleware } from '@/common/middlewares/cors.middleware';
