e0fc8f894a8c212632c9c66dd7187681d6be4541
Draco <draco.coder@gmail.com>
2019-05-24 15:41:12 +0800

    chore: extend nestjs-base-instance/src/auth/auth.controller.ts

--- nestjs-base-instance/src/auth/auth.controller.ts
+++ nestjs-base-instance/src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
 import { Controller, UseGuards, HttpStatus, Response, Request, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiUseTags, ApiResponse } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
