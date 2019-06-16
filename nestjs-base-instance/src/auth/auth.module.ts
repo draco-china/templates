7e405e235ef3af9dbc0db2f80691a91ed39f41fd
Draco <draco.coder@gmail.com>
2019-06-16 11:43:06 +0800

    chore: extend nestjs-base-instance/src/auth/auth.module.ts

--- nestjs-base-instance/src/auth/auth.module.ts
+++ nestjs-base-instance/src/auth/auth.module.ts
@@ -2,3 +2,4 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
