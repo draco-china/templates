4cfc2046c60aa3d4f0629ad57752b9ec8da3b245
Draco <draco.coder@gmail.com>
2019-07-17 13:10:31 +0800

    chore: extend nestjs-base-instance/src/auth/jwt.strategy.ts

--- nestjs-base-instance/src/auth/jwt.strategy.ts
+++ nestjs-base-instance/src/auth/jwt.strategy.ts
@@ -1,3 +1,4 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { PassportStrategy } from '@nestjs/passport';
