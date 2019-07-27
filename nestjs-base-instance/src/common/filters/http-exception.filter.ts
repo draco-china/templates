97eb34d20c36dd5c84f5167fa8eb462140b6791a
Draco <draco.coder@gmail.com>
2019-07-27 11:35:45 +0800

    chore: extend nestjs-base-instance/src/common/filters/http-exception.filter.ts

--- nestjs-base-instance/src/common/filters/http-exception.filter.ts
+++ nestjs-base-instance/src/common/filters/http-exception.filter.ts
@@ -2,3 +2,4 @@ import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/co
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
