d0454007db03face9e67394e3d235dc67dd18244
Draco <draco.coder@gmail.com>
2019-07-29 16:28:35 +0800

    chore: extend nestjs-base-instance/src/common/filters/http-exception.filter.ts

--- nestjs-base-instance/src/common/filters/http-exception.filter.ts
+++ nestjs-base-instance/src/common/filters/http-exception.filter.ts
@@ -8,3 +8,4 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest();
     const statusCode = exception.getStatus();
     
+    response.status(statusCode).json({
