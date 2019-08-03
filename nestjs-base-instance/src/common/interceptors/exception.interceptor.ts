aa549a9cc909805044634f11cd165717c31fa2b1
Draco <draco.coder@gmail.com>
2019-08-03 13:26:39 +0800

    chore: extend nestjs-base-instance/src/common/interceptors/exception.interceptor.ts

--- nestjs-base-instance/src/common/interceptors/exception.interceptor.ts
+++ nestjs-base-instance/src/common/interceptors/exception.interceptor.ts
@@ -1,2 +1,3 @@
 import { Injectable, NestInterceptor, ExecutionContext, HttpStatus } from '@nestjs/common';
 import { HttpException } from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
