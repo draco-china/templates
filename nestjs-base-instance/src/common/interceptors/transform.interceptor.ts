85fc2864cb00a43086e28fb77b059541846b7ad8
Draco <draco.coder@gmail.com>
2019-08-23 12:01:38 +0800

    chore: extend nestjs-base-instance/src/common/interceptors/transform.interceptor.ts

--- nestjs-base-instance/src/common/interceptors/transform.interceptor.ts
+++ nestjs-base-instance/src/common/interceptors/transform.interceptor.ts
@@ -2,3 +2,4 @@ import { Injectable, NestInterceptor, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+export interface Response<T> {
