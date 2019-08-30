969a307ae39a37b5aeeb030790f46997047c128f
Draco <draco.coder@gmail.com>
2019-08-30 12:53:29 +0800

    chore: add nestjs-base-instance/src/common/middlewares/cors.middleware.ts

---
+++ nestjs-base-instance/src/common/middlewares/cors.middleware.ts
@@ -0,0 +1,11 @@
+/**
+ * @Description: 跨域资源共享
+ * @Module: midddlrewares.cors
+ * @Author: Daker
+ * @Email: daker.zhou@gmail.com
+ * @Github: https://github.com/daker-china
+ * @Date: 2018-12-28 11:42:54
+ * @LastEditTime: 2019-01-08 16:22:41
+ */
+
+import { Injectable, NestMiddleware, MiddlewareFunction, HttpStatus, RequestMethod } from '@nestjs/common';
