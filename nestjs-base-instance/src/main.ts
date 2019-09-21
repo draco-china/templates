0f5ac014c89385c54ccbc3c8c532073239e0e3c9
Draco <draco.coder@gmail.com>
2019-09-21 11:24:18 +0800

    chore: extend nestjs-base-instance/src/main.ts

--- nestjs-base-instance/src/main.ts
+++ nestjs-base-instance/src/main.ts
@@ -4,3 +4,4 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as Helmet from 'helmet';
 import * as Compression from 'compression';
 import * as BodyParser from 'body-parser';
+import { AppModule } from '@/app/app.module';
