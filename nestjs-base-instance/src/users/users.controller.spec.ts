586293495f062233c5f82b4d02dbe41a2198eb8d
Draco <draco.coder@gmail.com>
2019-11-12 20:48:39 +0800

    chore: extend nestjs-base-instance/src/users/users.controller.spec.ts

--- nestjs-base-instance/src/users/users.controller.spec.ts
+++ nestjs-base-instance/src/users/users.controller.spec.ts
@@ -11,3 +11,4 @@ describe('Users Controller', () => {
   });
   it('should be defined', () => {
     const controller: UsersController = module.get<UsersController>(UsersController);
+    expect(controller).toBeDefined();
