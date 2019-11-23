f1f8427377bb43c38192067c34e1eca770c525e4
Draco <draco.coder@gmail.com>
2019-11-23 11:28:18 +0800

    chore: extend nestjs-base-instance/src/users/users.module.ts

--- nestjs-base-instance/src/users/users.module.ts
+++ nestjs-base-instance/src/users/users.module.ts
@@ -1,3 +1,4 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
