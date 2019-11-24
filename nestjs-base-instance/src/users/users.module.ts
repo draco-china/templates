fa8c497cb0b90697270c5df3eb674480039d3024
Draco <draco.coder@gmail.com>
2019-11-24 15:27:28 +0800

    chore: extend nestjs-base-instance/src/users/users.module.ts

--- nestjs-base-instance/src/users/users.module.ts
+++ nestjs-base-instance/src/users/users.module.ts
@@ -5,3 +5,4 @@ import { UsersService } from './users.service';
 import { UserSchema } from './schemas/user.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
