dfcec2f3720aad71fab62b0bb18301045f304b5b
Draco <draco.coder@gmail.com>
2019-12-05 12:44:41 +0800

    chore: extend nestjs-base-instance/src/users/users.service.ts

--- nestjs-base-instance/src/users/users.service.ts
+++ nestjs-base-instance/src/users/users.service.ts
@@ -2,3 +2,4 @@ import { Model, PassportLocalModel } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { debug } from 'console';
+import { User } from './interface/user.interface';
