3f5ba6a4b6ea5d157472077b182b26682ac923d4
Draco <draco.coder@gmail.com>
2019-12-06 11:13:04 +0800

    chore: extend nestjs-base-instance/src/users/users.service.ts

--- nestjs-base-instance/src/users/users.service.ts
+++ nestjs-base-instance/src/users/users.service.ts
@@ -3,3 +3,5 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { debug } from 'console';
 import { User } from './interface/user.interface';
+import { CreateUserDto } from './dto/createUser.dto';
+
