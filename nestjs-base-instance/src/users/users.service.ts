32291014b8a30a0532b977f07c009fb7d7cba40e
Draco <draco.coder@gmail.com>
2019-12-09 16:13:05 +0800

    chore: extend nestjs-base-instance/src/users/users.service.ts

--- nestjs-base-instance/src/users/users.service.ts
+++ nestjs-base-instance/src/users/users.service.ts
@@ -8,3 +8,4 @@ import { CreateUserDto } from './dto/createUser.dto';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel('User') private readonly userModel: PassportLocalModel<User>) { }
+  async findAll(): Promise<User[]> {
