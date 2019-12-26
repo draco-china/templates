40b6da278597e708c4ba0933f883bea23c77fe25
Draco <draco.coder@gmail.com>
2019-12-26 09:16:13 +0800

    chore: extend nestjs-base-instance/test/app.e2e-spec.ts

--- nestjs-base-instance/test/app.e2e-spec.ts
+++ nestjs-base-instance/test/app.e2e-spec.ts
@@ -2,3 +2,5 @@ import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app/app.module';
+import * as APP_CONFIG from './../src/app/app.config';
+
