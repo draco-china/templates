ba6afa231b5d36addfeba238f40c036207b558d0
Draco <draco.coder@gmail.com>
2020-01-10 17:38:54 +0800

    chore: extend antd-pro-umi-qiankun/app1-umi2/src/layouts/__tests__/index.test.tsx

--- antd-pro-umi-qiankun/app1-umi2/src/layouts/__tests__/index.test.tsx
+++ antd-pro-umi-qiankun/app1-umi2/src/layouts/__tests__/index.test.tsx
@@ -4,3 +4,4 @@ import React from 'react';
 import renderer, { ReactTestInstance, ReactTestRenderer } from 'react-test-renderer';
 
 describe('Layout: BasicLayout', () => {
+  it('Render correctly', () => {
