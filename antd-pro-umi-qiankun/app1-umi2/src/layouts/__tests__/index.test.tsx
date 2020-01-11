2134015def3986eb315c71a0603bf4d6ee5ee7f4
Draco <draco.coder@gmail.com>
2020-01-11 14:15:12 +0800

    chore: extend antd-pro-umi-qiankun/app1-umi2/src/layouts/__tests__/index.test.tsx

--- antd-pro-umi-qiankun/app1-umi2/src/layouts/__tests__/index.test.tsx
+++ antd-pro-umi-qiankun/app1-umi2/src/layouts/__tests__/index.test.tsx
@@ -7,3 +7,5 @@ describe('Layout: BasicLayout', () => {
   it('Render correctly', () => {
     const wrapper: ReactTestRenderer = renderer.create(<BasicLayout />);
     expect(wrapper.root.children.length).toBe(1);
+    const outerLayer = wrapper.root.children[0] as ReactTestInstance;
+    expect(outerLayer.type).toBe('div');
