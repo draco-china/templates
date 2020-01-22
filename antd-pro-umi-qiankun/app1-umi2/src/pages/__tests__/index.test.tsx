f71298b4dc2e715be3c414e032e10ce948d195a9
Draco <draco.coder@gmail.com>
2020-01-22 13:05:11 +0800

    chore: extend antd-pro-umi-qiankun/app1-umi2/src/pages/__tests__/index.test.tsx

--- antd-pro-umi-qiankun/app1-umi2/src/pages/__tests__/index.test.tsx
+++ antd-pro-umi-qiankun/app1-umi2/src/pages/__tests__/index.test.tsx
@@ -3,3 +3,6 @@ import Index from '..';
 import React from 'react';
 import renderer, { ReactTestInstance, ReactTestRenderer } from 'react-test-renderer';
 
+jest.mock('umi-plugin-locale');
+
+describe('Page: index', () => {
