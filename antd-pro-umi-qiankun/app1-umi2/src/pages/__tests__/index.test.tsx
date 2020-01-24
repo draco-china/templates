a717187d114e2adfbd3ac05f79c340bfce75c9fb
Draco <draco.coder@gmail.com>
2020-01-24 15:38:04 +0800

    chore: extend antd-pro-umi-qiankun/app1-umi2/src/pages/__tests__/index.test.tsx

--- antd-pro-umi-qiankun/app1-umi2/src/pages/__tests__/index.test.tsx
+++ antd-pro-umi-qiankun/app1-umi2/src/pages/__tests__/index.test.tsx
@@ -11,3 +11,4 @@ describe('Page: index', () => {
     expect(wrapper.root.children.length).toBe(1);
     const outerLayer = wrapper.root.children[0] as ReactTestInstance;
     expect(outerLayer.type).toBe('div');
+    expect(outerLayer.children.length).toBe(2);
