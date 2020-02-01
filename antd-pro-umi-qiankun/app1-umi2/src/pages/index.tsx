cb6794f4a369ab4d433fd180b2c722f1322661d5
Draco <draco.coder@gmail.com>
2020-02-01 09:49:03 +0800

    chore: extend antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx

--- antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx
+++ antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx
@@ -2,3 +2,5 @@ import React from 'react';
 import styles from './index.css';
 import { formatMessage } from 'umi-plugin-locale';
 export default function() {
+  return (
+    <div className={styles.normal}>
