81cd2c0caedd983a3d6f290ad737598a0c8dfded
Draco <draco.coder@gmail.com>
2020-02-01 11:09:35 +0800

    chore: extend antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx

--- antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx
+++ antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx
@@ -5,3 +5,5 @@ export default function() {
   return (
     <div className={styles.normal}>
       <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
