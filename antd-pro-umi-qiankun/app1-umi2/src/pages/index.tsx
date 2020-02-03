65a1ee77561468d8f7fa22614b53c255afe79a77
Draco <draco.coder@gmail.com>
2020-02-03 14:09:54 +0800

    chore: extend antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx

--- antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx
+++ antd-pro-umi-qiankun/app1-umi2/src/pages/index.tsx
@@ -8,3 +8,5 @@ export default function() {
       <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
         <li>
+          <a href="https://umijs.org/guide/getting-started.html">
+            {formatMessage({ id: 'index.start' })}
