14dc467095d886e39a95723178e79f65b7b563dd
Draco <draco.coder@gmail.com>
2020-02-15 18:00:26 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts
@@ -31,3 +31,10 @@ export interface DefaultSettings {
    * sticky siderbar
    */
   fixSiderbar: boolean;
+  menu: { locale: boolean };
+  title: string;
+  pwa: boolean;
+  // Your custom iconfont Symbol script Url
+  // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
+  // 注意：如果需要图标多色，Iconfont 图标项目里要进行批量去色处理
+  // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
