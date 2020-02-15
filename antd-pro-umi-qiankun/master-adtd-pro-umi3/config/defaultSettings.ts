ea32a95808c8307ff3dee144f38ece263a744205
Draco <draco.coder@gmail.com>
2020-02-15 09:32:12 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts
@@ -14,3 +14,15 @@ export interface DefaultSettings {
   /**
    * nav menu position: `sidemenu` or `topmenu`
    */
+  layout: 'sidemenu' | 'topmenu';
+  /**
+   * layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
+   */
+  contentWidth: ContentWidth;
+  /**
+   * sticky header
+   */
+  fixedHeader: boolean;
+  /**
+   * auto hide header
+   */
