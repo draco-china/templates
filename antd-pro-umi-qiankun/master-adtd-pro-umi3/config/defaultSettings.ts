4bd0016ff545bffcbe89e8777dd2670f26743666
Draco <draco.coder@gmail.com>
2020-02-15 09:00:42 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/config/defaultSettings.ts
@@ -2,3 +2,15 @@ import { MenuTheme } from 'antd/es/menu/MenuContext';
 
 export type ContentWidth = 'Fluid' | 'Fixed';
 
+export interface DefaultSettings {
+  /**
+   * theme for nav menu
+   */
+  navTheme: MenuTheme;
+  /**
+   * primary color of ant design
+   */
+  primaryColor: string;
+  /**
+   * nav menu position: `sidemenu` or `topmenu`
+   */
