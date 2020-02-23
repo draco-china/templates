2c947d3b75a1ec4a4e0b60b2eee626bf17ba82c0
Draco <draco.coder@gmail.com>
2020-02-23 12:06:49 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/config/themePluginConfig.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/config/themePluginConfig.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/config/themePluginConfig.ts
@@ -7,3 +7,13 @@ export default {
     },
     {
       key: 'dust',
+      fileName: 'dust.css',
+      modifyVars: {
+        '@primary-color': '#F5222D',
+      },
+    },
+    {
+      key: 'volcano',
+      fileName: 'volcano.css',
+      modifyVars: {
+        '@primary-color': '#FA541C',
