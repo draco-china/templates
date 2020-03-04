f3b48a751b417047ff7814f76b0d5874a2e6c9ca
Draco <draco.coder@gmail.com>
2020-03-04 14:51:12 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+const getNotices = (req: Request, res: Response) => {
+  res.json([
+    {
+      id: '000000001',
+      avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
+      title: '你收到了 14 份新周报',
+      datetime: '2017-08-09',
