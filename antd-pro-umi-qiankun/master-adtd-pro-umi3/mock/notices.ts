1061761d48326cf80f705ac0a681a605a6f9afab
Draco <draco.coder@gmail.com>
2020-03-06 11:39:30 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts
@@ -41,3 +41,12 @@ const getNotices = (req: Request, res: Response) => {
     {
       id: '000000006',
       avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
+      title: '曲丽丽 评论了你',
+      description: '描述信息描述信息描述信息',
+      datetime: '2017-08-07',
+      type: 'message',
+      clickClose: true,
+    },
+    {
+      id: '000000007',
+      avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
