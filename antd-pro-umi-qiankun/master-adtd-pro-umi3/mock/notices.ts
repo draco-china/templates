5d13f426fd9679d0cf420ba142050d840238d356
Draco <draco.coder@gmail.com>
2020-03-07 15:20:39 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/notices.ts
@@ -84,3 +84,12 @@ const getNotices = (req: Request, res: Response) => {
     {
       id: '000000011',
       title: '信息安全考试',
+      description: '指派竹尔于 2017-01-09 前完成更新并发布',
+      extra: '已耗时 8 天',
+      status: 'doing',
+      type: 'event',
+    },
+    {
+      id: '000000012',
+      title: 'ABCD 版本发布',
+      description: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务',
