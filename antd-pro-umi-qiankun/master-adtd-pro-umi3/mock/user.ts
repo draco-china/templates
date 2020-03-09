6ba1676829d100423d0a7bc2f9cd172975cd5773
Draco <draco.coder@gmail.com>
2020-03-09 18:58:49 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts
@@ -13,3 +13,15 @@ export default {
     email: 'antdesign@alipay.com',
     signature: '海纳百川，有容乃大',
     title: '交互专家',
+    group: '蚂蚁金服－某某某事业群－某某平台部－某某技术部－UED',
+    tags: [
+      {
+        key: '0',
+        label: '很有想法的',
+      },
+      {
+        key: '1',
+        label: '专注设计',
+      },
+      {
+        key: '2',
