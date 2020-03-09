31e5e30e4f5774a2fa66e3adb7542dec67a86fe8
Draco <draco.coder@gmail.com>
2020-03-09 17:33:51 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+function getFakeCaptcha(req: Request, res: Response) {
+  return res.json('captcha-xxx');
+}
+// 代码中会兼容本地 service mock 以及部署站点的静态数据
+export default {
+  // 支持值为 Object 和 Array
+  'GET /api/currentUser': {
+    name: 'Serati Ma',
+    avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+    userid: '00000001',
+    email: 'antdesign@alipay.com',
+    signature: '海纳百川，有容乃大',
+    title: '交互专家',
