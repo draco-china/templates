f1f554e0bffcfb04d0d4683f8e1f08c2f5d7471d
Draco <draco.coder@gmail.com>
2020-03-12 13:36:08 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/mock/user.ts
@@ -126,3 +126,15 @@ export default {
     res.status(404).send({
       timestamp: 1513932643431,
       status: 404,
+      error: 'Not Found',
+      message: 'No message available',
+      path: '/base/category/list/2121212',
+    });
+  },
+  'GET /api/403': (req: Request, res: Response) => {
+    res.status(403).send({
+      timestamp: 1513932555104,
+      status: 403,
+      error: 'Unauthorized',
+      message: 'Unauthorized',
+      path: '/base/category/list',
