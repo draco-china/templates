e306b6258e96b60d57bbb47a222cf71016f446e3
Draco <draco.coder@gmail.com>
2020-03-15 15:07:05 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Authorized.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Authorized.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Authorized.tsx
@@ -1,2 +1,7 @@
 import React from 'react';
 import { Result } from 'antd';
+import check, { IAuthorityType } from './CheckPermissions';
+
+import AuthorizedRoute from './AuthorizedRoute';
+import Secured from './Secured';
+
