d48500e45b44fb91658fe37817a42977dfd5045e
Draco <draco.coder@gmail.com>
2020-03-18 14:28:05 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Authorized.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Authorized.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Authorized.tsx
@@ -27,3 +27,5 @@ const Authorized: React.FunctionComponent<AuthorizedProps> = ({
     />
   ),
 }) => {
+  const childrenRender: React.ReactNode = typeof children === 'undefined' ? null : children;
+  const dom = check(authority, childrenRender, noMatch);
