0fcd93800d77ef516959dd3cc46003a77aea691b
Draco <draco.coder@gmail.com>
2020-03-19 15:40:36 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/AuthorizedRoute.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/AuthorizedRoute.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/AuthorizedRoute.tsx
@@ -4,3 +4,6 @@ import React from 'react';
 import Authorized from './Authorized';
 import { IAuthorityType } from './CheckPermissions';
 
+interface AuthorizedRoutePops {
+  currentAuthority: string;
+  component: React.ComponentClass<any, any>;
