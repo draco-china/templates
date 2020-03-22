d166739392f3874188132a8bc813c5cbcbfa3837
Draco <draco.coder@gmail.com>
2020-03-22 13:03:20 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { CURRENT } from './renderAuthorize';
+// eslint-disable-next-line import/no-cycle
+import PromiseRender from './PromiseRender';
+
+export type IAuthorityType =
+  | undefined
