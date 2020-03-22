b2c395367c454a1d50ccd8fc01db8cf5a94b6041
Draco <draco.coder@gmail.com>
2020-03-22 15:25:21 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx
@@ -5,3 +5,17 @@ import PromiseRender from './PromiseRender';
 
 export type IAuthorityType =
   | undefined
+  | string
+  | string[]
+  | Promise<boolean>
+  | ((currentAuthority: string | string[]) => IAuthorityType);
+
+/**
+ * 通用权限检查方法
+ * Common check permissions method
+ * @param { 权限判定 | Permission judgment } authority
+ * @param { 你的权限 | Your permission description } currentAuthority
+ * @param { 通过的组件 | Passing components } target
+ * @param { 未通过的组件 | no pass components } Exception
+ */
+const checkPermissions = <T, K>(
