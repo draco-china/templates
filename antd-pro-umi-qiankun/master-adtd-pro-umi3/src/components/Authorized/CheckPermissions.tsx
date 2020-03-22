39fa21e32e55bdc5b5b9d6bd530b2a4f6bf55a5f
Draco <draco.coder@gmail.com>
2020-03-22 20:52:50 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx
@@ -19,3 +19,10 @@ export type IAuthorityType =
  * @param { 未通过的组件 | no pass components } Exception
  */
 const checkPermissions = <T, K>(
+  authority: IAuthorityType,
+  currentAuthority: string | string[],
+  target: T,
+  Exception: K,
+): T | K | React.ReactNode => {
+  // 没有判定权限.默认查看所有
+  // Retirement authority, return target;
