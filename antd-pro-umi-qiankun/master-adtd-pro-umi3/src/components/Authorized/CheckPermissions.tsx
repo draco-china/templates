abd8989aa05dbccc6534fbb9002fd2f8c0a4c420
Draco <draco.coder@gmail.com>
2020-03-23 16:25:51 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/CheckPermissions.tsx
@@ -32,3 +32,8 @@ const checkPermissions = <T, K>(
   // 数组处理
   if (Array.isArray(authority)) {
     if (Array.isArray(currentAuthority)) {
+      if (currentAuthority.some(item => authority.includes(item))) {
+        return target;
+      }
+    } else if (authority.includes(currentAuthority)) {
+      return target;
