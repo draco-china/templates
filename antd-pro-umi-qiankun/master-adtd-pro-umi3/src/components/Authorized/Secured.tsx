746c7716ae494c226cf6e844b79ad7bee84800fb
Draco <draco.coder@gmail.com>
2020-03-29 19:10:47 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Secured.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Secured.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Secured.tsx
@@ -5,3 +5,7 @@ import CheckPermissions from './CheckPermissions';
  * 默认不能访问任何页面
  * default is "NULL"
  */
+const Exception403 = () => 403;
+
+export const isComponentClass = (component: React.ComponentClass | React.ReactNode): boolean => {
+  if (!component) return false;
