62ea09b6e3a212b21df4a4906e3579b3458472ac
Draco <draco.coder@gmail.com>
2020-03-30 11:36:15 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Secured.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Secured.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/Secured.tsx
@@ -9,3 +9,6 @@ const Exception403 = () => 403;
 
 export const isComponentClass = (component: React.ComponentClass | React.ReactNode): boolean => {
   if (!component) return false;
+  const proto = Object.getPrototypeOf(component);
+  if (proto === React.Component || proto === Function.prototype) return true;
+  return isComponentClass(proto);
