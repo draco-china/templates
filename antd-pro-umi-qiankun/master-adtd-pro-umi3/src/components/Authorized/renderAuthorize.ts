5ff592a399783b282a6504a4d1443453c13d2ad0
Draco <draco.coder@gmail.com>
2020-04-05 17:32:08 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/renderAuthorize.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/renderAuthorize.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/Authorized/renderAuthorize.ts
@@ -2,3 +2,9 @@
 /* eslint-disable import/no-mutable-exports */
 let CURRENT: string | string[] = 'NULL';
 
+type CurrentAuthorityType = string | string[] | (() => typeof CURRENT);
+/**
+ * use  authority or getAuthority
+ * @param {string|()=>String} currentAuthority
+ */
+const renderAuthorize = <T>(Authorized: T): ((currentAuthority: CurrentAuthorityType) => T) => (
