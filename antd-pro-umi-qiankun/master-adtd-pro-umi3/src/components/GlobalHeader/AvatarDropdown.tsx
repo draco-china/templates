b5891b54a6ada8bb2c46bbc5bb05d9fe20555d23
Draco <draco.coder@gmail.com>
2020-04-09 10:43:09 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/AvatarDropdown.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/AvatarDropdown.tsx
@@ -0,0 +1,6 @@
+import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Menu, Spin } from 'antd';
+import { ClickParam } from 'antd/es/menu';
+import React from 'react';
+import { connect, history } from 'umi';
+import { ConnectProps, ConnectState } from '@/models/connect';
