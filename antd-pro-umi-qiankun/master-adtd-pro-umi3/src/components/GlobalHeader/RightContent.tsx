dac9954075352fde074b0f24e54ed4fc387a0a36
Draco <draco.coder@gmail.com>
2020-04-19 18:37:59 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx
@@ -3,3 +3,10 @@ import { QuestionCircleOutlined } from '@ant-design/icons';
 import React from 'react';
 import { connect } from 'umi';
 import { ConnectProps, ConnectState } from '@/models/connect';
+import Avatar from './AvatarDropdown';
+import HeaderSearch from '../HeaderSearch';
+import SelectLang from '../SelectLang';
+import styles from './index.less';
+
+export type SiderTheme = 'light' | 'dark';
+export interface GlobalHeaderRightProps extends ConnectProps {
