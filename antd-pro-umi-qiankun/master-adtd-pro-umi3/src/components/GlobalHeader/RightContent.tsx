6027abcbd26e579f9e929aca6266615a6eb8e7ac
Draco <draco.coder@gmail.com>
2020-04-19 12:04:47 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx
@@ -0,0 +1,5 @@
+import { Tooltip, Tag } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import React from 'react';
+import { connect } from 'umi';
+import { ConnectProps, ConnectState } from '@/models/connect';
