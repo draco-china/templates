eef5aeb433bc52bc1afa9efe755c33a609c5816a
Draco <draco.coder@gmail.com>
2020-04-21 13:49:13 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx
@@ -38,3 +38,9 @@ const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
           { label: <a href="https://umijs.org/zh/guide/umi-ui.html">umi ui</a>, value: 'umi ui' },
           {
             label: <a href="next.ant.design">Ant Design</a>,
+            value: 'Ant Design',
+          },
+          {
+            label: <a href="https://protable.ant.design/">Pro Table</a>,
+            value: 'Pro Table',
+          },
