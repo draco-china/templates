5885f402504660c706906e2b8d19ef342060ef25
Draco <draco.coder@gmail.com>
2020-04-22 13:07:02 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/GlobalHeader/RightContent.tsx
@@ -59,3 +59,9 @@ const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
           href="https://pro.ant.design/docs/getting-started"
           rel="noopener noreferrer"
           className={styles.action}
+        >
+          <QuestionCircleOutlined />
+        </a>
+      </Tooltip>
+      <Avatar />
+      {REACT_APP_ENV && (
