168bec11670382dfaa46bedbc1e7756ec0931c18
Draco <draco.coder@gmail.com>
2020-04-24 20:47:59 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/HeaderDropdown/index.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/HeaderDropdown/index.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/HeaderDropdown/index.tsx
@@ -8,3 +8,5 @@ declare type OverlayFunc = () => React.ReactNode;
 
 export interface HeaderDropdownProps extends Omit<DropDownProps, 'overlay'> {
   overlayClassName?: string;
+  overlay: React.ReactNode | OverlayFunc | any;
+  placement?: 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topCenter' | 'topRight' | 'bottomCenter';
