d375d0f720b94fe9aed4f133d2a83d0274f1f56c
Draco <draco.coder@gmail.com>
2020-05-01 12:09:57 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/NoticeIcon/NoticeList.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/NoticeIcon/NoticeList.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/NoticeIcon/NoticeList.tsx
@@ -9,3 +9,12 @@ export interface NoticeIconTabProps {
   count?: number;
   name?: string;
   showClear?: boolean;
+  showViewMore?: boolean;
+  style?: React.CSSProperties;
+  title: string;
+  tabKey: string;
+  data?: NoticeIconData[];
+  onClick?: (item: NoticeIconData) => void;
+  onClear?: () => void;
+  emptyText?: string;
+  clearText?: string;
