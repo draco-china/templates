fe29b978a2c57fb469527df59d1979aea8609a72
Draco <draco.coder@gmail.com>
2020-05-09 09:47:51 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/PageLoading/index.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/PageLoading/index.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/PageLoading/index.tsx
@@ -2,3 +2,4 @@ import { PageLoading } from '@ant-design/pro-layout';
 
 // loading components from code split
 // https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
+export default PageLoading;
