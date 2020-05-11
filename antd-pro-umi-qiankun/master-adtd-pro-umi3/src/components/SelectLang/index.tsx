cde9e1367a790bc1d3dd841ac5936eca79111493
Draco <draco.coder@gmail.com>
2020-05-11 20:09:56 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/SelectLang/index.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/SelectLang/index.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/components/SelectLang/index.tsx
@@ -34,3 +34,7 @@ const SelectLang: React.FC<SelectLangProps> = props => {
     <Menu className={styles.menu} selectedKeys={[selectedLang]} onClick={changeLang}>
       {locales.map(locale => (
         <Menu.Item key={locale}>
+          <span role="img" aria-label={languageLabels[locale]}>
+            {languageIcons[locale]}
+          </span>{' '}
+          {languageLabels[locale]}
