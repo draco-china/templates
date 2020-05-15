a2b50b2435c31fa1b2b97d465b9a30c5728ce41d
Draco <draco.coder@gmail.com>
2020-05-15 14:51:18 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/e2e/baseLayout.e2e.js

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/e2e/baseLayout.e2e.js
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/e2e/baseLayout.e2e.js
@@ -3,3 +3,6 @@ const RouterConfig = require('../../config/config').default.routes;
 
 const BASE_URL = `http://localhost:${process.env.PORT || 8000}`;
 
+function formatter(routes, parentPath = '') {
+  const fixedParentPath = parentPath.replace(/\/{1,}/g, '/');
+  let result = [];
