9e577d991c33a765650d63ed4d8177dc093f142e
Draco <draco.coder@gmail.com>
2020-05-19 13:14:06 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/e2e/topMenu.e2e.js

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/e2e/topMenu.e2e.js
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/e2e/topMenu.e2e.js
@@ -2,3 +2,4 @@ const BASE_URL = `http://localhost:${process.env.PORT || 8000}`;
 
 describe('Homepage', () => {
   it('topmenu should have footer', async () => {
+    const params = '?navTheme=light&layout=topmenu';
