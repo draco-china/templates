da1fde60dc65c25aa6f0f310ccc9f43f9f474b19
Draco <draco.coder@gmail.com>
2020-05-27 19:16:49 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/BasicLayout.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/BasicLayout.tsx
@@ -0,0 +1,22 @@
+/**
+ * Ant Design Pro v4 use `@ant-design/pro-layout` to handle Layout.
+ * You can view component api by:
+ * https://github.com/ant-design/ant-design-pro-layout
+ */
+import ProLayout, {
+  MenuDataItem,
+  BasicLayoutProps as ProLayoutProps,
+  Settings,
+  DefaultFooter,
+} from '@ant-design/pro-layout';
+import React, { useEffect } from 'react';
+import { Dispatch } from 'redux';
+import { Link, connect, formatMessage } from 'umi';
+import { GithubOutlined } from '@ant-design/icons';
+import { Result, Button } from 'antd';
+import Authorized from '@/utils/Authorized';
+import RightContent from '@/components/GlobalHeader/RightContent';
+import { ConnectState } from '@/models/connect';
+import {
+  // isAntDesignPro,
+  getAuthorityFromRouter,
