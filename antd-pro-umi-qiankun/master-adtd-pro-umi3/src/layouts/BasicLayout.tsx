e0a100d4283cae57dbe7d8ebd1f95f506cc1b5e2
Draco <draco.coder@gmail.com>
2020-05-30 15:40:03 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/BasicLayout.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/BasicLayout.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/BasicLayout.tsx
@@ -98,3 +98,20 @@ const footerRender: BasicLayoutProps['footerRender'] = () => {
       <div
         style={{
           padding: '0px 24px 24px',
+          textAlign: 'center',
+        }}
+      >
+        <a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg"
+            width="82px"
+            alt="netlify logo"
+          />
+        </a>
+      </div>
+    </>
+  );
+};
+
+const BasicLayout: React.FC<BasicLayoutProps> = props => {
+  const {
