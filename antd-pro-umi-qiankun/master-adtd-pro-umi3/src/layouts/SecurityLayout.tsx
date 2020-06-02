acdad816cb15702babdd8be64a0d24cd36e1463f
Draco <draco.coder@gmail.com>
2020-06-02 09:44:00 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/SecurityLayout.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/SecurityLayout.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/SecurityLayout.tsx
@@ -2,3 +2,8 @@ import React from 'react';
 import { PageLoading } from '@ant-design/pro-layout';
 import { connect, Redirect } from 'umi';
 import { stringify } from 'querystring';
+import { ConnectState, ConnectProps } from '@/models/connect';
+import { CurrentUser } from '@/models/user';
+
+interface SecurityLayoutProps extends ConnectProps {
+  loading?: boolean;
