5e7a9d51f8e67d975274d7dcfa8da7d923d48590
Draco <draco.coder@gmail.com>
2020-06-06 10:55:51 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/UserLayout.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/layouts/UserLayout.tsx
@@ -0,0 +1,5 @@
+import { DefaultFooter, MenuDataItem, getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { Helmet } from 'react-helmet';
+import { Link, connect, formatMessage } from 'umi';
+import React from 'react';
+import SelectLang from '@/components/SelectLang';
