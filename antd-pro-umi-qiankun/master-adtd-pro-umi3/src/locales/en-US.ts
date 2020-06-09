fa7f20aac91225b1ab7c80674d1422b98a52d974
Draco <draco.coder@gmail.com>
2020-06-09 19:57:18 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US.ts
@@ -1,3 +1,5 @@
 import component from './en-US/component';
 import globalHeader from './en-US/globalHeader';
 import menu from './en-US/menu';
+import pwa from './en-US/pwa';
+import settingDrawer from './en-US/settingDrawer';
