2ffb82c4f1d0ec588d9ce0875418b3803698b7a0
Draco <draco.coder@gmail.com>
2020-06-12 12:16:28 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US.ts
@@ -9,3 +9,5 @@ export default {
   'navBar.lang': 'Languages',
   'layout.user.link.help': 'Help',
   'layout.user.link.privacy': 'Privacy',
+  'layout.user.link.terms': 'Terms',
+  'app.preview.down.block': 'Download this page to your local project',
