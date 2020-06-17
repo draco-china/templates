14ece80f0bccb1e0cef72abd4e49e251728ebe60
Draco <draco.coder@gmail.com>
2020-06-17 11:30:26 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/globalHeader.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/globalHeader.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/globalHeader.ts
@@ -3,3 +3,5 @@ export default {
   'component.globalHeader.search.example1': 'Search example 1',
   'component.globalHeader.search.example2': 'Search example 2',
   'component.globalHeader.search.example3': 'Search example 3',
+  'component.globalHeader.help': 'Help',
+  'component.globalHeader.notification': 'Notification',
