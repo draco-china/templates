c089526c559667246ce29cb5295aeec7b1f58fd9
Draco <draco.coder@gmail.com>
2020-06-19 12:51:49 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/globalHeader.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/globalHeader.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/globalHeader.ts
@@ -9,3 +9,4 @@ export default {
   'component.globalHeader.message': 'Message',
   'component.globalHeader.message.empty': 'You have viewed all messsages.',
   'component.globalHeader.event': 'Event',
+  'component.globalHeader.event.empty': 'You have viewed all events.',
