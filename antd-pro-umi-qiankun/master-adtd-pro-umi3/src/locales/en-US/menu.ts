a8d5bd93799f3321cf27f7abcabf9f195ffec724
Draco <draco.coder@gmail.com>
2020-06-21 11:06:15 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/menu.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/menu.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/menu.ts
@@ -19,3 +19,8 @@ export default {
   'menu.form.step-form': 'Step Form',
   'menu.form.step-form.info': 'Step Form(write transfer information)',
   'menu.form.step-form.confirm': 'Step Form(confirm transfer information)',
+  'menu.form.step-form.result': 'Step Form(finished)',
+  'menu.form.advanced-form': 'Advanced Form',
+  'menu.list': 'List',
+  'menu.list.table-list': 'Search Table',
+  'menu.list.basic-list': 'Basic List',
