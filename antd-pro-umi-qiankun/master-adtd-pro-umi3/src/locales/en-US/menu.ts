0e12b40f279d5ee16a1e3c6ac5f56d29815797d3
Draco <draco.coder@gmail.com>
2020-06-22 19:19:40 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/menu.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/menu.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/menu.ts
@@ -37,3 +37,7 @@ export default {
   'menu.result.fail': 'Fail',
   'menu.exception': 'Exception',
   'menu.exception.not-permission': '403',
+  'menu.exception.not-find': '404',
+  'menu.exception.server-error': '500',
+  'menu.exception.trigger': 'Trigger',
+  'menu.account': 'Account',
