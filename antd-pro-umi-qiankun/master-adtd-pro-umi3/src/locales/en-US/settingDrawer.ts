2397756003f84457bdc5381270a5fd33b3c466d1
Draco <draco.coder@gmail.com>
2020-06-27 16:40:48 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settingDrawer.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settingDrawer.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settingDrawer.ts
@@ -10,3 +10,6 @@ export default {
   'app.setting.themecolor.volcano': 'Volcano',
   'app.setting.themecolor.sunset': 'Sunset Orange',
   'app.setting.themecolor.cyan': 'Cyan',
+  'app.setting.themecolor.green': 'Polar Green',
+  'app.setting.themecolor.daybreak': 'Daybreak Blue (default)',
+  'app.setting.themecolor.geekblue': 'Geek Glue',
