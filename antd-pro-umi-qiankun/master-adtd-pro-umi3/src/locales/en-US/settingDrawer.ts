e07bd013429b160392cae1e92a56e45dfc95ead6
Draco <draco.coder@gmail.com>
2020-06-28 14:54:57 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settingDrawer.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settingDrawer.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settingDrawer.ts
@@ -21,3 +21,5 @@ export default {
   'app.setting.fixedsidebar': 'Fixed Sidebar',
   'app.setting.fixedsidebar.hint': 'Works on Side Menu Layout',
   'app.setting.hideheader': 'Hidden Header when scrolling',
+  'app.setting.hideheader.hint': 'Works when Hidden Header is enabled',
+  'app.setting.othersettings': 'Other Settings',
