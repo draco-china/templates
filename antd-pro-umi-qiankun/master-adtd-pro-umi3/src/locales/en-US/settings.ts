33bebd50195fb9d45167dd8d4183b99cb6da2db4
Draco <draco.coder@gmail.com>
2020-07-02 16:02:36 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settings.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settings.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/en-US/settings.ts
@@ -43,3 +43,8 @@ export default {
   'app.settings.binding.taobao-description': 'Currently unbound Taobao account',
   'app.settings.binding.alipay': 'Binding Alipay',
   'app.settings.binding.alipay-description': 'Currently unbound Alipay account',
+  'app.settings.binding.dingding': 'Binding DingTalk',
+  'app.settings.binding.dingding-description': 'Currently unbound DingTalk account',
+  'app.settings.binding.bind': 'Bind',
+  'app.settings.notification.password': 'Account Password',
+  'app.settings.notification.password-description':
