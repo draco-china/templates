569f55950d836cedb22f7efb0e3593b00ec400f3
Draco <draco.coder@gmail.com>
2020-07-16 19:56:07 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/menu.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/menu.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/menu.ts
@@ -12,3 +12,7 @@ export default {
   'menu.dashboard.monitor': 'Monitor',
   'menu.dashboard.workplace': 'Ambiente de Trabalho',
   'menu.exception.403': '403',
+  'menu.exception.404': '404',
+  'menu.exception.500': '500',
+  'menu.form': 'Formulário',
+  'menu.form.basic-form': 'Formulário Básico',
