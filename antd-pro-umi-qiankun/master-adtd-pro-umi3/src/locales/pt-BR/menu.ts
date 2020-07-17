662b458236f93dade5835caab50a2981e0b1a362
Draco <draco.coder@gmail.com>
2020-07-17 13:44:50 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/menu.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/menu.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/menu.ts
@@ -21,3 +21,8 @@ export default {
   'menu.form.step-form.confirm': 'Formulário Assistido(confirmar informações de transferência)',
   'menu.form.step-form.result': 'Formulário Assistido(finalizado)',
   'menu.form.advanced-form': 'Formulário Avançado',
+  'menu.list': 'Lista',
+  'menu.list.table-list': 'Tabela de Busca',
+  'menu.list.basic-list': 'Lista Básica',
+  'menu.list.card-list': 'Lista de Card',
+  'menu.list.search-list': 'Lista de Busca',
