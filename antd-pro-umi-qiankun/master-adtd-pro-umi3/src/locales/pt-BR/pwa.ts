0fd80af776920a235160d8dc6bded8e29adb8f6b
Draco <draco.coder@gmail.com>
2020-07-21 18:22:09 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/pwa.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/pwa.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/pwa.ts
@@ -2,3 +2,4 @@ export default {
   'app.pwa.offline': 'Você está offline agora',
   'app.pwa.serviceworker.updated': 'Novo conteúdo está disponível',
   'app.pwa.serviceworker.updated.hint':
+    'Por favor, pressione o botão "Atualizar" para recarregar a página atual',
