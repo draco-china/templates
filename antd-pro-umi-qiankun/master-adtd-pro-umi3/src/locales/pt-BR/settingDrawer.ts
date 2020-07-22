dbfd03ae5ea7bfc1463aa81af03a9237ea21a87e
Draco <draco.coder@gmail.com>
2020-07-22 11:21:49 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settingDrawer.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settingDrawer.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settingDrawer.ts
@@ -1,2 +1,5 @@
 export default {
   'app.setting.pagestyle': 'Configuração de estilo da página',
+  'app.setting.pagestyle.dark': 'Dark style',
+  'app.setting.pagestyle.light': 'Light style',
+  'app.setting.content-width': 'Largura do conteúdo',
