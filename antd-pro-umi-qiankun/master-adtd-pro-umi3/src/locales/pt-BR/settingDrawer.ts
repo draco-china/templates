1623e847fa7b376afccdc8f89df75ddc8bc29597
Draco <draco.coder@gmail.com>
2020-07-25 12:15:11 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settingDrawer.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settingDrawer.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settingDrawer.ts
@@ -27,3 +27,6 @@ export default {
   'app.setting.copy': 'Copiar Configuração',
   'app.setting.copyinfo':
     'copiado com sucesso，por favor trocar o defaultSettings em src/models/setting.js',
+  'app.setting.production.hint':
+    'O painel de configuração apenas é exibido no ambiente de desenvolvimento, por favor modifique manualmente o',
+};
