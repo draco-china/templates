5d51eea4a6d806e8735c4f0bff874cae1074c849
Draco <draco.coder@gmail.com>
2020-07-25 13:39:36 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settings.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/pt-BR/settings.ts
@@ -0,0 +1,5 @@
+export default {
+  'app.settings.menuMap.basic': 'Configurações Básicas',
+  'app.settings.menuMap.security': 'Configurações de Segurança',
+  'app.settings.menuMap.binding': 'Vinculação de Conta',
+  'app.settings.menuMap.notification': 'Mensagens de Notificação',
