a3d054bcbb929c24c5c1b0a0e2083d4e129ef644
Draco <draco.coder@gmail.com>
2020-07-30 12:18:34 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts
@@ -3,3 +3,6 @@ import globalHeader from './zh-CN/globalHeader';
 import menu from './zh-CN/menu';
 import pwa from './zh-CN/pwa';
 import settingDrawer from './zh-CN/settingDrawer';
+import settings from './zh-CN/settings';
+
+export default {
