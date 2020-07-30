d4149b2a6f30f0464eeb392736269713633d4833
Draco <draco.coder@gmail.com>
2020-07-30 20:18:10 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts
@@ -10,3 +10,5 @@ export default {
   'layout.user.link.help': '帮助',
   'layout.user.link.privacy': '隐私',
   'layout.user.link.terms': '条款',
+  'app.preview.down.block': '下载此页面到本地项目',
+  'app.welcome.link.fetch-blocks': '获取全部区块',
