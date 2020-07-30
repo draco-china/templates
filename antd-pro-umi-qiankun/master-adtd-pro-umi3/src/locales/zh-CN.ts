fa707b236116bf47f57d179250bb75f0535d4bd7
Draco <draco.coder@gmail.com>
2020-07-30 16:28:37 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN.ts
@@ -8,3 +8,5 @@ import settings from './zh-CN/settings';
 export default {
   'navBar.lang': '语言',
   'layout.user.link.help': '帮助',
+  'layout.user.link.privacy': '隐私',
+  'layout.user.link.terms': '条款',
