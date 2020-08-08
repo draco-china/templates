1614712d0ee72d0ee89d6a4a6d32b8a9ed167357
Draco <draco.coder@gmail.com>
2020-08-08 15:26:38 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN/menu.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN/menu.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-CN/menu.ts
@@ -26,3 +26,8 @@ export default {
   'menu.list.basic-list': '标准列表',
   'menu.list.card-list': '卡片列表',
   'menu.list.search-list': '搜索列表',
+  'menu.list.search-list.articles': '搜索列表（文章）',
+  'menu.list.search-list.projects': '搜索列表（项目）',
+  'menu.list.search-list.applications': '搜索列表（应用）',
+  'menu.profile': '详情页',
+  'menu.profile.basic': '基础详情页',
