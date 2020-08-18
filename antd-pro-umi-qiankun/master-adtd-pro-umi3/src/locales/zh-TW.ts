cb52834b788a94249d4e30328537761caebb9503
Draco <draco.coder@gmail.com>
2020-08-18 16:40:30 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW.ts
@@ -1,3 +1,5 @@
 import component from './zh-TW/component';
 import globalHeader from './zh-TW/globalHeader';
 import menu from './zh-TW/menu';
+import pwa from './zh-TW/pwa';
+import settingDrawer from './zh-TW/settingDrawer';
