21d1346bdd214b858c1118a05071113026c6eb32
Draco <draco.coder@gmail.com>
2020-08-25 15:26:41 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts
@@ -2,3 +2,5 @@ export default {
   'component.globalHeader.search': '站內搜索',
   'component.globalHeader.search.example1': '搜索提示壹',
   'component.globalHeader.search.example2': '搜索提示二',
+  'component.globalHeader.search.example3': '搜索提示三',
+  'component.globalHeader.help': '使用手冊',
