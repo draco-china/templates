7720d8eaeea1c00901eb3d04ee49a259b251ad93
Draco <draco.coder@gmail.com>
2020-08-26 11:44:18 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts
@@ -7,3 +7,4 @@ export default {
   'component.globalHeader.notification': '通知',
   'component.globalHeader.notification.empty': '妳已查看所有通知',
   'component.globalHeader.message': '消息',
+  'component.globalHeader.message.empty': '您已讀完所有消息',
