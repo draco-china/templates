cd26559825582862f8603cbe0274e280cab8ed9c
Draco <draco.coder@gmail.com>
2020-08-26 17:05:27 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/globalHeader.ts
@@ -11,3 +11,5 @@ export default {
   'component.globalHeader.event': '待辦',
   'component.globalHeader.event.empty': '妳已完成所有待辦',
   'component.noticeIcon.clear': '清空',
+  'component.noticeIcon.cleared': '清空了',
+  'component.noticeIcon.empty': '暫無資料',
