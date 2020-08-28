ef7156c970bf2870d701e0b13d2b717d967f8209
Draco <draco.coder@gmail.com>
2020-08-28 14:41:41 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/menu.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/menu.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/menu.ts
@@ -7,3 +7,8 @@ export default {
   'menu.admin.sub-page': '二级管理页',
   'menu.exception.403': '403',
   'menu.exception.404': '404',
+  'menu.exception.500': '500',
+  'menu.register': '註冊',
+  'menu.register.result': '註冊結果',
+  'menu.dashboard': 'Dashboard',
+  'menu.dashboard.analysis': '分析頁',
