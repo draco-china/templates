6b2628904a765d4550dabcffa96ca5e018c462b0
Draco <draco.coder@gmail.com>
2020-09-04 19:07:09 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/settingDrawer.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/settingDrawer.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/settingDrawer.ts
@@ -3,3 +3,6 @@ export default {
   'app.setting.pagestyle.dark': '暗色菜單風格',
   'app.setting.pagestyle.light': '亮色菜單風格',
   'app.setting.content-width': '內容區域寬度',
+  'app.setting.content-width.fixed': '定寬',
+  'app.setting.content-width.fluid': '流式',
+  'app.setting.themecolor': '主題色',
