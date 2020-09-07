ad36750ee818131cae538bf1f0668adc7df8fe83
Draco <draco.coder@gmail.com>
2020-09-07 09:59:08 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/settings.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/locales/zh-TW/settings.ts
@@ -0,0 +1,5 @@
+export default {
+  'app.settings.menuMap.basic': '基本設置',
+  'app.settings.menuMap.security': '安全設置',
+  'app.settings.menuMap.binding': '賬號綁定',
+  'app.settings.menuMap.notification': '新消息通知',
