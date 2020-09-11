d1cbc905b6600ac4a8cfa4d03e9d9cdd9fdc4973
Draco <draco.coder@gmail.com>
2020-09-11 17:09:53 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/base.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/base.ts
@@ -0,0 +1,19 @@
+/*
+ * @Description: your description
+ * @Module: module.name
+ * @Author: Draco
+ * @Email: Draco.coder@gmail.com
+ * @Github: https://github.com/draco-china
+ * @Date: 2020-03-06 15:20:18
+ * @LastEditTime: 2020-03-06 15:20:18
+ */
+/* eslint-disable import/no-extraneous-dependencies */
+import { Reducer } from 'redux';
+import { Effect, IRoute, qiankunStart } from 'umi';
+import { query } from '@/services/base';
+
+// function sleep(ms: number) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+export interface App extends Partial<IRoute> {
