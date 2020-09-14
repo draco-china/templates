a95c31a1b4c2b54030cc4ca4d583576c268c6bda
Draco <draco.coder@gmail.com>
2020-09-14 19:27:01 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts
@@ -0,0 +1,13 @@
+import { Reducer } from 'redux';
+import { Subscription, Effect } from 'umi';
+import { NoticeIconData } from '@/components/NoticeIcon';
+import { queryNotices } from '@/services/user';
+import { ConnectState } from './connect.d';
+
+export interface NoticeItem extends NoticeIconData {
+  id: string;
+  type: string;
+  status: string;
+}
+
+export interface GlobalModelState {
