864a24886e82d63db7130b35134b0169430d6bcf
Draco <draco.coder@gmail.com>
2020-09-15 16:54:30 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts
@@ -36,3 +36,16 @@ const GlobalModel: GlobalModelType = {
 
   state: {
     collapsed: false,
+    notices: [],
+  },
+
+  effects: {
+    *fetchNotices(_, { call, put, select }) {
+      const data = yield call(queryNotices);
+      yield put({
+        type: 'saveNotices',
+        payload: data,
+      });
+      const unreadCount: number = yield select(
+        (state: ConnectState) => state.global.notices.filter(item => !item.read).length,
+      );
