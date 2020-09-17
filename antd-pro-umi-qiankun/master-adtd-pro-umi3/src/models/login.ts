428731f82a5153ffdc51f3f83cbb397921f6684e
Draco <draco.coder@gmail.com>
2020-09-17 11:11:01 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/login.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/login.ts
@@ -0,0 +1,17 @@
+/*
+ * @Description: your description
+ * @Module: module.name
+ * @Author: Draco
+ * @Email: Draco.coder@gmail.com
+ * @Github: https://github.com/draco-china
+ * @Date: 2020-03-12 11:56:25
+ * @LastEditTime: 2020-03-12 12:00:45
+ */
+import { Reducer } from 'redux';
+import { stringify } from 'querystring';
+import { Effect, history } from 'umi';
+
+import { fakeAccountLogin } from '@/services/login';
+import { setAuthority } from '@/utils/authority';
+import { getPageQuery } from '@/utils/utils';
+
