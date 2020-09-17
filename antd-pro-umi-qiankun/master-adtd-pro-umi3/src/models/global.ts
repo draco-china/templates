2c954521f791498ace8683a705eed0baae60f06a
Draco <draco.coder@gmail.com>
2020-09-17 10:08:04 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/global.ts
@@ -121,3 +121,18 @@ const GlobalModel: GlobalModelType = {
         notices: state.notices.filter((item): boolean => item.type !== payload),
       };
     },
+  },
+
+  subscriptions: {
+    setup({ history }): void {
+      // Subscribe history(url) change, trigger `load` action if pathname is `/`
+      history.listen(({ pathname, search }): void => {
+        if (typeof window.ga !== 'undefined') {
+          window.ga('send', 'pageview', pathname + search);
+        }
+      });
+    },
+  },
+};
+
+export default GlobalModel;
