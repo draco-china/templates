8e50787f066593097426d60f45cad11fb68c99ae
Draco <draco.coder@gmail.com>
2020-09-19 11:31:11 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/login.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/login.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/login.ts
@@ -49,3 +49,10 @@ const Model: LoginModelType = {
       });
       // Login successfully
       if (response.status === 'ok') {
+        const urlParams = new URL(window.location.href);
+        const params = getPageQuery();
+        let { redirect } = params as { redirect: string };
+        if (redirect) {
+          const redirectUrlParams = new URL(redirect);
+          if (redirectUrlParams.origin === urlParams.origin) {
+            redirect = redirect.substr(urlParams.origin.length);
