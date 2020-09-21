84229b5637f6b83ff5ffc1eeef01db80af634173
Draco <draco.coder@gmail.com>
2020-09-21 11:59:36 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/setting.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/setting.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/setting.ts
@@ -2,3 +2,6 @@ import { Reducer } from 'redux';
 import defaultSettings, { DefaultSettings } from '../../config/defaultSettings';
 
 export interface SettingModelType {
+  namespace: 'settings';
+  state: DefaultSettings;
+  reducers: {
