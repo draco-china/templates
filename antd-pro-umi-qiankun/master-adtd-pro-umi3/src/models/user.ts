f9c84ed16c4bb0bf4958d0c8c9d246df7043c499
Draco <draco.coder@gmail.com>
2020-09-25 10:24:56 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/user.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/user.ts
@@ -0,0 +1,17 @@
+/*
+ * @Description: your description
+ * @Module: module.name
+ * @Author: Draco
+ * @Email: Draco.coder@gmail.com
+ * @Github: https://github.com/draco-china
+ * @Date: 2020-03-12 12:00:55
+ * @LastEditTime: 2020-03-12 12:00:55
+ */
+import { Effect } from 'umi';
+import { Reducer } from 'redux';
+
+import { queryCurrent, query as queryUsers } from '@/services/user';
+
+export interface CurrentUser {
+  avatar?: string;
+  name?: string;
