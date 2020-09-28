5094a2e0958a1308c64fd7eb3efb962d61fe5d95
Draco <draco.coder@gmail.com>
2020-09-28 13:11:45 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/user.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/user.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/models/user.ts
@@ -55,3 +55,11 @@ const UserModel: UserModelType = {
       const response = yield call(queryUsers);
       yield put({
         type: 'save',
+        payload: response,
+      });
+    },
+    *fetchCurrent(_, { call, put }) {
+      const response = yield call(queryCurrent);
+      yield put({
+        type: 'saveCurrentUser',
+        payload: response,
