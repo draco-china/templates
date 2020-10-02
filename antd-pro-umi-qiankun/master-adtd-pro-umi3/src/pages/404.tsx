ef8e7e4db10694e57d638938a43e0872e67e7625
Draco <draco.coder@gmail.com>
2020-10-02 11:35:08 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/404.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/404.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/404.tsx
@@ -9,3 +9,4 @@ const NoFoundPage: React.FC<{}> = () => (
     subTitle="Sorry, the page you visited does not exist."
     extra={
       <Button type="primary" onClick={() => history.push('/')}>
+        Back Home
