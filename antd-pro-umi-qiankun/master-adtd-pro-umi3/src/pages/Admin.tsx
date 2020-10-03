8e47c5d4b99e9ad5aa6349a850da44504a324a94
Draco <draco.coder@gmail.com>
2020-10-03 14:06:58 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Admin.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Admin.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Admin.tsx
@@ -4,3 +4,6 @@ import { Card, Typography, Alert } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 export default (): React.ReactNode => (
+  <PageHeaderWrapper content=" 这个页面只有 admin 权限才能查看">
+    <Card>
+      <Alert
