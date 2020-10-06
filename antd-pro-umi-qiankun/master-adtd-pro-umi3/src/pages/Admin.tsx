0a3cb495f609e73e2771453b2748b2250f5ecbf4
Draco <draco.coder@gmail.com>
2020-10-06 19:29:54 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Admin.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Admin.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Admin.tsx
@@ -23,3 +23,6 @@ export default (): React.ReactNode => (
     <p style={{ textAlign: 'center', marginTop: 24 }}>
       Want to add more pages? Please refer to{' '}
       <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+        use block
+      </a>
+      。
