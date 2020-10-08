a96bde38eb45ffee8897a4960bb22d8ddad5f7b1
Draco <draco.coder@gmail.com>
2020-10-08 20:04:27 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Authorized.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Authorized.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Authorized.tsx
@@ -5,3 +5,7 @@ import { getRouteAuthority } from '@/utils/utils';
 import { ConnectProps, ConnectState, UserModelState } from '@/models/connect';
 
 interface AuthComponentProps extends ConnectProps {
+  user: UserModelState;
+}
+
+const AuthComponent: React.FC<AuthComponentProps> = ({
