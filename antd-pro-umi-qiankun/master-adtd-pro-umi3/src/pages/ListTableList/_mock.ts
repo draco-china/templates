f33e820255784cb4d46313cc4f5061b96984953d
Draco <draco.coder@gmail.com>
2020-10-12 09:56:20 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/_mock.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/_mock.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { parse } from 'url';
+import { TableListItem, TableListParams } from './data.d';
+
+// mock tableListDataSource
+const genList = (current: number, pageSize: number) => {
+  const tableListDataSource: TableListItem[] = [];
+
+  for (let i = 0; i < pageSize; i += 1) {
+    const index = (current - 1) * 10 + i;
+    tableListDataSource.push({
+      key: index,
+      disabled: i % 6 === 0,
+      href: 'https://ant.design',
+      avatar: [
