2b8d3855b856400c0f7d5a49b54be82d5bb1afaf
Draco <draco.coder@gmail.com>
2020-10-20 11:08:18 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/components/UpdateForm.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/components/UpdateForm.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Form, Button, DatePicker, Input, Modal, Radio, Select, Steps } from 'antd';
+
+import { TableListItem } from '../data.d';
+
+export interface FormValueType extends Partial<TableListItem> {
+  target?: string;
+  template?: string;
+  type?: string;
+  time?: string;
+  frequency?: string;
+}
+
+export interface UpdateFormProps {
+  onCancel: (flag?: boolean, formVals?: FormValueType) => void;
+  onSubmit: (values: FormValueType) => void;
+  updateModalVisible: boolean;
+  values: Partial<TableListItem>;
+}
+const FormItem = Form.Item;
