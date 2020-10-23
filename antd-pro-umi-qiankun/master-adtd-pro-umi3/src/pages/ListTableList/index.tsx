5895c1e3966ac88a532814887b781f04053ef2ac
Draco <draco.coder@gmail.com>
2020-10-23 20:49:40 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/index.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/index.tsx
@@ -0,0 +1,25 @@
+import { DownOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, Divider, Dropdown, Menu, message } from 'antd';
+import React, { useState, useRef } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import ProTable, { ProColumns, ActionType } from '@ant-design/pro-table';
+import { SorterResult } from 'antd/es/table/interface';
+
+import CreateForm from './components/CreateForm';
+import UpdateForm, { FormValueType } from './components/UpdateForm';
+import { TableListItem } from './data.d';
+import { queryRule, updateRule, addRule, removeRule } from './service';
+
+/**
+ * 添加节点
+ * @param fields
+ */
+const handleAdd = async (fields: TableListItem) => {
+  const hide = message.loading('正在添加');
+  try {
+    await addRule({ ...fields });
+    hide();
+    message.success('添加成功');
+    return true;
+  } catch (error) {
+    hide();
