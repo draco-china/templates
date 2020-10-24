a0a88a2734a8388fc64949a64de92e195d6bd360
Draco <draco.coder@gmail.com>
2020-10-24 13:42:54 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/index.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/index.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/index.tsx
@@ -95,3 +95,23 @@ const TableList: React.FC<{}> = () => {
       valueType: 'textarea',
     },
     {
+      title: '服务调用次数',
+      dataIndex: 'callNo',
+      sorter: true,
+      hideInForm: true,
+      renderText: (val: string) => `${val} 万`,
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      hideInForm: true,
+      valueEnum: {
+        0: { text: '关闭', status: 'Default' },
+        1: { text: '运行中', status: 'Processing' },
+        2: { text: '已上线', status: 'Success' },
+        3: { text: '异常', status: 'Error' },
+      },
+    },
+    {
+      title: '上次调度时间',
+      dataIndex: 'updatedAt',
