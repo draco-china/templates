d7fab98f33eeb63da1eda893057b57b695f2092c
Draco <draco.coder@gmail.com>
2020-10-27 13:08:53 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/service.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/service.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/ListTableList/service.ts
@@ -2,3 +2,6 @@ import request from '@/utils/request';
 import { TableListParams, TableListItem } from './data.d';
 
 export async function queryRule(params?: TableListParams) {
+  return request('/api/rule', {
+    params,
+  });
