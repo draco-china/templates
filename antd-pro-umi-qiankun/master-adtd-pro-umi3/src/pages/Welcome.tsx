f544298881f505bab7ce944048edcad0a4cecc05
Draco <draco.coder@gmail.com>
2020-10-30 20:40:46 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx
@@ -4,3 +4,8 @@ import { Card, Typography, Alert } from 'antd';
 import styles from './Welcome.less';
 
 const CodePreview: React.FC<{}> = ({ children }) => (
+  <pre className={styles.pre}>
+    <code>
+      <Typography.Text copyable>{children}</Typography.Text>
+    </code>
+  </pre>
