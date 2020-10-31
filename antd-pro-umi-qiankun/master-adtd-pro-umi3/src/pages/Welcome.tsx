c7532d6e133854484277f9dc7d5bcc9fa451e541
Draco <draco.coder@gmail.com>
2020-10-31 18:17:11 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx
@@ -16,3 +16,8 @@ export default (): React.ReactNode => (
     <Card>
       <Alert
         message="umi ui 现已发布，点击右下角 umi 图标即可使用"
+        type="success"
+        showIcon
+        banner
+        style={{
+          margin: -12,
