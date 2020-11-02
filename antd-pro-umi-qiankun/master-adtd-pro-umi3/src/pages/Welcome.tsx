d16fe041662d47a6e38828d7f25a564c2fbe7d74
Draco <draco.coder@gmail.com>
2020-11-02 13:35:14 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/Welcome.tsx
@@ -54,3 +54,9 @@ export default (): React.ReactNode => (
     >
       Want to add more pages? Please refer to{' '}
       <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
+        use block
+      </a>
+      。
+    </p>
+  </PageHeaderWrapper>
+);
