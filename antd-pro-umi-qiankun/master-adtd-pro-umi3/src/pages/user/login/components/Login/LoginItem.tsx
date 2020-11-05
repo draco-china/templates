d382959ec861948ca9a1950b27c3ffd5db21e933
Draco <draco.coder@gmail.com>
2020-11-05 20:48:30 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginItem.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginItem.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginItem.tsx
@@ -59,3 +59,19 @@ const getFormItemOptions = ({
 
 const LoginItem: React.FC<LoginItemProps> = props => {
   const [count, setCount] = useState<number>(props.countDown || 0);
+  const [timing, setTiming] = useState(false);
+  // 这么写是为了防止restProps中 带入 onChange, defaultValue, rules props tabUtil
+  const {
+    onChange,
+    customProps,
+    defaultValue,
+    rules,
+    name,
+    getCaptchaButtonText,
+    getCaptchaSecondText,
+    updateActive,
+    type,
+    tabUtil,
+    ...restProps
+  } = props;
+
