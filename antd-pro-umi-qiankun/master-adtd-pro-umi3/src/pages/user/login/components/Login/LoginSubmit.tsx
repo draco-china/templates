3891d80a4dba4d1f6f6b95f442cb7d82b60d552e
Draco <draco.coder@gmail.com>
2020-11-12 09:05:55 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginSubmit.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginSubmit.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginSubmit.tsx
@@ -15,3 +15,4 @@ const LoginSubmit: React.FC<LoginSubmitProps> = ({ className, ...rest }) => {
   const clsString = classNames(styles.submit, className);
   return (
     <FormItem>
+      <Button size="large" className={clsString} type="primary" htmlType="submit" {...rest} />
