93704a0d9107fa62ddf650872220dab13fa1d9d8
Draco <draco.coder@gmail.com>
2020-11-12 20:23:58 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginTab.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginTab.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/LoginTab.tsx
@@ -1,3 +1,8 @@
 import React, { useEffect } from 'react';
 import { TabPaneProps } from 'antd/es/tabs';
 import { Tabs } from 'antd';
+import LoginContext, { LoginContextProps } from './LoginContext';
+
+const { TabPane } = Tabs;
+
+const generateId = (() => {
