484481a0c4d03354e9f2cae152bfced164710b01
Draco <draco.coder@gmail.com>
2020-11-14 18:00:13 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/index.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Tabs, Form } from 'antd';
+import React, { useState } from 'react';
+import useMergeValue from 'use-merge-value';
+import classNames from 'classnames';
+import { FormInstance } from 'antd/es/form';
+import { LoginParamsType } from '@/services/login';
+
+import LoginContext from './LoginContext';
+import LoginItem, { LoginItemProps } from './LoginItem';
+import LoginSubmit from './LoginSubmit';
+import LoginTab from './LoginTab';
