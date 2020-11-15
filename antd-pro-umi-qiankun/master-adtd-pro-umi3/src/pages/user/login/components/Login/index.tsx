84b76bae239d5a1895563a2457f9d66cda75cd22
Draco <draco.coder@gmail.com>
2020-11-15 11:18:05 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/index.tsx

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/index.tsx
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/components/Login/index.tsx
@@ -9,3 +9,15 @@ import LoginContext from './LoginContext';
 import LoginItem, { LoginItemProps } from './LoginItem';
 import LoginSubmit from './LoginSubmit';
 import LoginTab from './LoginTab';
+import styles from './index.less';
+
+export interface LoginProps {
+  activeKey?: string;
+  onTabChange?: (key: string) => void;
+  style?: React.CSSProperties;
+  onSubmit?: (values: LoginParamsType) => void;
+  className?: string;
+  from?: FormInstance;
+  children: React.ReactElement<typeof LoginTab>[];
+}
+
