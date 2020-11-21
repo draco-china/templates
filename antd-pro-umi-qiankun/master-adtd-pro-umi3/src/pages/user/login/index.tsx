d2391b5f548c05b633ffc7f6d4e375ee32d6e04e
Draco <draco.coder@gmail.com>
2020-11-21 14:11:16 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/index.tsx

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/pages/user/login/index.tsx
@@ -0,0 +1,13 @@
+import { AlipayCircleOutlined, TaobaoCircleOutlined, WeiboCircleOutlined } from '@ant-design/icons';
+import { Alert, Checkbox } from 'antd';
+import React, { useState } from 'react';
+import { Dispatch, AnyAction } from 'redux';
+import { connect, Link } from 'umi';
+import { StateType } from '@/models/login';
+import { LoginParamsType } from '@/services/login';
+import { ConnectState } from '@/models/connect';
+import LoginFrom from './components/Login';
+
+import styles from './style.less';
+
+const { Tab, UserName, Password, Mobile, Captcha, Submit } = LoginFrom;
