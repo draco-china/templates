b303264cf9df0437f691594ee1344efe566caac3
Draco <draco.coder@gmail.com>
2020-11-25 19:55:34 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/service-worker.js

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/service-worker.js
@@ -0,0 +1,7 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-underscore-dangle */
+/* globals workbox */
+workbox.core.setCacheNameDetails({
+  prefix: 'antd-pro',
+  suffix: 'v1',
