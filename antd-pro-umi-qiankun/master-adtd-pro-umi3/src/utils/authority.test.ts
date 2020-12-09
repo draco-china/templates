b812b689474f4589dcb8b16e3f3383e99517e601
Draco <draco.coder@gmail.com>
2020-12-09 17:37:17 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.test.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.test.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.test.ts
@@ -7,3 +7,4 @@ describe('getAuthority should be strong', () => {
   it('array with double quotes', () => {
     expect(getAuthority('"admin"')).toEqual(['admin']);
   });
+  it('array with single item', () => {
