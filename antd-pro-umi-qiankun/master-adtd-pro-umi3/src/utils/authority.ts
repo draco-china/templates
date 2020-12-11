ee15b2e0d7da7724759e9a58a4bec7a290b7a289
Draco <draco.coder@gmail.com>
2020-12-11 09:26:55 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.ts
@@ -5,3 +5,5 @@ export function getAuthority(str?: string): string | string[] {
   const authorityString =
     typeof str === 'undefined' && localStorage ? localStorage.getItem('antd-pro-authority') : str;
   // authorityString could be admin, "admin", ["admin"]
+  let authority;
+  try {
