7bafd481988f3093c56ba4a07cd785691e5e88a5
Draco <draco.coder@gmail.com>
2020-12-13 13:50:42 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts
@@ -6,3 +6,7 @@ import { extend } from 'umi-request';
 import { notification } from 'antd';
 
 const codeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
