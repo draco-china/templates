ab07aa49a8c5d4e52a681f0dc2ebc267c4c4aeea
Draco <draco.coder@gmail.com>
2020-12-13 13:23:49 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts
@@ -0,0 +1,8 @@
+/**
+ * request 网络请求工具
+ * 更详细的 api 文档: https://github.com/umijs/umi-request
+ */
+import { extend } from 'umi-request';
+import { notification } from 'antd';
+
+const codeMessage = {
