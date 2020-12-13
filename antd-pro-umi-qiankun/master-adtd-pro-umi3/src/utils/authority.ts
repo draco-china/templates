2a951ffe67d22595858ceb7be43e63881fec82fa
Draco <draco.coder@gmail.com>
2020-12-13 11:06:06 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/authority.ts
@@ -26,3 +26,7 @@ export function getAuthority(str?: string): string | string[] {
 
 export function setAuthority(authority: string | string[]): void {
   const proAuthority = typeof authority === 'string' ? [authority] : authority;
+  localStorage.setItem('antd-pro-authority', JSON.stringify(proAuthority));
+  // auto reload
+  reloadAuthorized();
+}
