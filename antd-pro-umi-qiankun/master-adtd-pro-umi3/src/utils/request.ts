96df872643c8fe7e96717729c817a7fee245eeb8
Draco <draco.coder@gmail.com>
2020-12-15 13:59:44 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/request.ts
@@ -21,3 +21,11 @@ const codeMessage = {
   502: '网关错误。',
   503: '服务不可用，服务器暂时过载或维护。',
   504: '网关超时。',
+};
+
+/**
+ * 异常处理程序
+ */
+const errorHandler = (error: { response: Response }): Response => {
+  const { response } = error;
+  if (response && response.status) {
