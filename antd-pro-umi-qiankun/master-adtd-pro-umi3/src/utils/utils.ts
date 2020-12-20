76c527efe19d58480125b7428cffda35047f8650
Draco <draco.coder@gmail.com>
2020-12-20 12:13:49 +0800

    chore: add antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/utils.ts

---
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/utils.ts
@@ -0,0 +1,5 @@
+import { parse } from 'querystring';
+import pathRegexp from 'path-to-regexp';
+import { Route } from '@/models/connect';
+
+/* eslint no-useless-escape:0 import/prefer-default-export:0 */
