0b90697a9667cf9a5cdd2f0ec21b064eb4999a9a
Draco <draco.coder@gmail.com>
2020-12-23 13:37:49 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/utils.ts

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/utils.ts
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/src/utils/utils.ts
@@ -56,3 +56,6 @@ export const getRouteAuthority = (path: string, routeData: Route[]) => {
         authorities = route.authority || authorities;
       }
       // get children authority recursively
+      if (route.routes) {
+        authorities = getRouteAuthority(path, route.routes) || authorities;
+      }
