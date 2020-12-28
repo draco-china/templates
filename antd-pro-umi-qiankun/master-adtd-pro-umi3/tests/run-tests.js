6d8447814ac4a17765680b2ff9394b4f83ecac87
Draco <draco.coder@gmail.com>
2020-12-28 09:10:25 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/tests/run-tests.js

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/tests/run-tests.js
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/tests/run-tests.js
@@ -39,3 +39,6 @@ startServer.stdout.on('data', data => {
     console.log('Development server is started, ready to run tests.');
     const testCmd = spawn(
       /^win/.test(process.platform) ? 'npm.cmd' : 'npm',
+      ['test', '--', '--maxWorkers=1', '--runInBand'],
+      {
+        stdio: 'inherit',
