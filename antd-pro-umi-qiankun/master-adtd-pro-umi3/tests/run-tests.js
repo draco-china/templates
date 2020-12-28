ad9b56aee69c993ccb925091243ee24424ce19c7
Draco <draco.coder@gmail.com>
2020-12-28 11:49:30 +0800

    chore: extend antd-pro-umi-qiankun/master-adtd-pro-umi3/tests/run-tests.js

--- antd-pro-umi-qiankun/master-adtd-pro-umi3/tests/run-tests.js
+++ antd-pro-umi-qiankun/master-adtd-pro-umi3/tests/run-tests.js
@@ -42,3 +42,7 @@ startServer.stdout.on('data', data => {
       ['test', '--', '--maxWorkers=1', '--runInBand'],
       {
         stdio: 'inherit',
+      },
+    );
+    testCmd.on('exit', code => {
+      startServer.kill();
