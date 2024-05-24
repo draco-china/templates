18acf841d9f1bd41842cb3cc59b16155441f123a
Draco <draco.coder@gmail.com>
2024-05-24 16:39:39 +0800

    chore: add next-ssr-template/.prettierrc.js

---
+++ next-ssr-template/.prettierrc.js
@@ -0,0 +1,28 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  // $schema: 'https://json.schemastore.org/prettierrc',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    // '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '^@/app/(.*)$',
+    // '',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+  jsxSingleQuote: true,
+  overrides: [
