953b56c98f149bb2f90940bd0f490027db9ad34f
Draco <draco.coder@gmail.com>
2024-05-25 10:15:59 +0800

    chore: extend next-ssr-template/.prettierrc.js

--- next-ssr-template/.prettierrc.js
+++ next-ssr-template/.prettierrc.js
@@ -26,3 +26,27 @@ module.exports = {
   importOrderTypeScriptVersion: '5.0.0',
   jsxSingleQuote: true,
   overrides: [
+    {
+      files: '*.md',
+      options: {
+        proseWrap: 'preserve',
+      },
+    },
+  ],
+  plugins: [
+    'prettier-plugin-sh',
+    'prettier-plugin-packagejson',
+    'prettier-plugin-sort-json',
+    'prettier-plugin-two-style-order',
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  printWidth: 100,
+  proseWrap: 'never',
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
