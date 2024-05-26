a54c0e1730ae2f026200d6f70510cd6bae8aeb77
Draco <draco.coder@gmail.com>
2024-05-26 19:09:21 +0800

    chore: extend next-ssr-template/.releaserc.js

--- next-ssr-template/.releaserc.js
+++ next-ssr-template/.releaserc.js
@@ -36,3 +36,39 @@ const options = {
       release: false,
       type: 'test',
     },
+    {
+      release: false,
+      type: 'ci',
+    },
+    {
+      release: false,
+      type: 'chore',
+    },
+    {
+      release: false,
+      type: 'wip',
+    },
+    {
+      release: 'major',
+      type: 'BREAKING CHANGE',
+    },
+    {
+      release: 'major',
+      scope: 'BREAKING CHANGE',
+    },
+    {
+      release: 'major',
+      subject: '*BREAKING CHANGE*',
+    },
+    { release: 'patch', subject: '*force release*' },
+    { release: 'patch', subject: '*force patch*' },
+    { release: 'minor', subject: '*force minor*' },
+    { release: 'major', subject: '*force major*' },
+    { release: false, subject: '*skip release*' },
+  ],
+};
+
+module.exports = {
+  $schema: 'https://json.schemastore.org/semantic-release',
+  ...createConfig(options),
+};
