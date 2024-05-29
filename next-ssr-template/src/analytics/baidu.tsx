56b4c9886b7c1300671d42edef1eff4ca53df862
Draco <draco.coder@gmail.com>
2024-05-29 15:56:31 +0800

    chore: extend next-ssr-template/src/analytics/baidu.tsx

--- next-ssr-template/src/analytics/baidu.tsx
+++ next-ssr-template/src/analytics/baidu.tsx
@@ -9,3 +9,13 @@ export default function BaiduAnalytics() {
             var _hmt = _hmt || [];
             (function() {
               var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?${BAIDU_ANALYTICS_ID}";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+            `,
+        }}
+      />
+    )
+  );
+}
