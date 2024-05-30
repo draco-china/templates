ac97a1594cb2bc74472d2b51b79919cd7bf402d0
Draco <draco.coder@gmail.com>
2024-05-30 18:08:01 +0800

    chore: add next-ssr-template/src/analytics/google.tsx

---
+++ next-ssr-template/src/analytics/google.tsx
@@ -0,0 +1,9 @@
+import {
+  GoogleAnalytics as Analytics,
+  GoogleTagManager as TagManager,
+} from '@next/third-parties/google';
+import { GOOGLE_ANALYTICS_ID, GOOGLE_TAG_MANAGER_ID } from '@/lib/constants';
+
+/**
+ * header script
+ */
