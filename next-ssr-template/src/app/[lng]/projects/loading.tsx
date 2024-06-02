18c78c137c7bf719563c89f6c0beb056570d0919
Draco <draco.coder@gmail.com>
2024-06-02 14:37:36 +0800

    chore: add next-ssr-template/src/app/[lng]/projects/loading.tsx

---
+++ next-ssr-template/src/app/[lng]/projects/loading.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function Loading() {
+  return (
+    <div className='container grid h-fit grid-cols-3 gap-6 py-6'>
+      {[...new Array(9).fill('')].map((item, index) => {
+        return (
+          <Skeleton key={index} className='rounded-xl *:p-6'>
+            <div className='flex flex-col space-y-1.5'>
+              <Skeleton className='h-4' />
+              <Skeleton className='h-5' />
