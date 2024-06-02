a17a6de180da90d33adbc5886080c179f1256004
Draco <draco.coder@gmail.com>
2024-06-02 18:07:15 +0800

    chore: add next-ssr-template/src/app/[lng]/projects/page.tsx

---
+++ next-ssr-template/src/app/[lng]/projects/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Link from 'next/link';
+import { Share1Icon, StarIcon } from '@radix-ui/react-icons';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import request from '@/lib/request';
+import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function Projects() {
+  const { data } = useSuspenseQuery({
+    queryKey: ['draco-china'],
+    queryFn: async () => {
+      const res = await request(
+        `https://api.github.com/users/draco-china/repos?type=&sort=&direction=&per_page=&page=`,
+      );
+      return res.data || [];
+    },
+  });
+  return (
+    <div className='container grid grid-cols-3 gap-6 py-6'>
+      {data?.map((item: any) => {
