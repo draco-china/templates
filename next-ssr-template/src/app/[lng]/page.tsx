e3b8f1fe6285a10f8eef322b5999f758f027d84c
Draco <draco.coder@gmail.com>
2024-06-02 12:30:21 +0800

    chore: extend next-ssr-template/src/app/[lng]/page.tsx

--- next-ssr-template/src/app/[lng]/page.tsx
+++ next-ssr-template/src/app/[lng]/page.tsx
@@ -43,3 +43,47 @@ export default async function Home({ params: { lng } }: { params: { lng: string 
           target='_blank'
           rel='noopener noreferrer'
         >
+          <h2 className='mb-3 text-2xl font-semibold'>
+            {t('learn')}
+            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+              -&gt;
+            </span>
+          </h2>
+          <p className='m-0 max-w-[30ch] text-sm opacity-50'>{t('learn.description')}</p>
+        </a>
+
+        <a
+          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2 className='mb-3 text-2xl font-semibold'>
+            {t('templates')}
+            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+              -&gt;
+            </span>
+          </h2>
+          <p className='m-0 max-w-[30ch] text-sm opacity-50'>{t('templates.description')}</p>
+        </a>
+
+        <a
+          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <h2 className='mb-3 text-2xl font-semibold'>
+            {t('deploy')}
+            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+              -&gt;
+            </span>
+          </h2>
+          <p className='m-0 max-w-[30ch] text-balance text-sm opacity-50'>
+            {t('deploy.description')}
+          </p>
+        </a>
+      </div>
+    </div>
+  );
+}
