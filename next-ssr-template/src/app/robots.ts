48776d6053584adecdd6eeaf5cef454d9b54464f
Draco <draco.coder@gmail.com>
2024-06-03 19:24:19 +0800

    chore: add next-ssr-template/src/app/robots.ts

---
+++ next-ssr-template/src/app/robots.ts
@@ -0,0 +1,8 @@
+import type { MetadataRoute } from 'next';
+import { SITE_URL } from '@/lib/constants';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
