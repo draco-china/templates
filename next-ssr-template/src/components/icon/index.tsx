f99001031ba574b80dfafbd87a34fa1c18d77ddd
Draco <draco.coder@gmail.com>
2024-06-05 13:48:53 +0800

    chore: extend next-ssr-template/src/components/icon/index.tsx

--- next-ssr-template/src/components/icon/index.tsx
+++ next-ssr-template/src/components/icon/index.tsx
@@ -4,3 +4,9 @@ import { icons as FlagPack } from '@iconify-json/flagpack';
 import { addCollection, Icon as Iconify, IconProps } from '@iconify/react';
 import Local from './local.json';
 
+addCollection(Local);
+addCollection(FlagPack);
+
+export default function Icon(props: IconProps) {
+  return <Iconify {...props} />;
+}
