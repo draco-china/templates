0657f26e78e99fdaab843ff7b565530c63d14294
Draco <draco.coder@gmail.com>
2024-06-05 13:30:53 +0800

    chore: add next-ssr-template/src/components/icon/index.tsx

---
+++ next-ssr-template/src/components/icon/index.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+import { icons as FlagPack } from '@iconify-json/flagpack';
+import { addCollection, Icon as Iconify, IconProps } from '@iconify/react';
+import Local from './local.json';
+
