d653709156629be0ff57b9df308ab68fe7f18fe1
Draco <draco.coder@gmail.com>
2024-07-03 09:06:00 +0800

    chore: add next-with-tauri/.i18nrc.js

---
+++ next-with-tauri/.i18nrc.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@lobehub/i18n-cli');
+
+module.exports = defineConfig({
+  entry: './src/i18n/locales/en-US',
+  entryLocale: 'en-US',
+  output: './src/i18n/locales',
+  outputLocales: [
+    'cs-CZ',
+    'de-DE',
+    'en-US',
+    'es-ES',
+    'es-MX',
+    'fi-FI',
