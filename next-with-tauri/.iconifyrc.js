354beb581b47cce2cb7bb386c776723b9ad8f869
Draco <draco.coder@gmail.com>
2024-07-05 13:22:25 +0800

    chore: extend next-with-tauri/.iconifyrc.js

--- next-with-tauri/.iconifyrc.js
+++ next-with-tauri/.iconifyrc.js
@@ -39,3 +39,27 @@ const savePath = path.join(__dirname, './src/components/icon', 'local.json');
       // If icon is not monotone, remove this code
       parseColors(svg, {
         defaultColor: 'currentColor',
+        callback: (attr, colorStr, color) => {
+          return !color || isEmptyColor(color) ? colorStr : 'currentColor';
+        },
+      });
+
+      // Optimise
+      runSVGO(svg);
+    } catch (err) {
+      // Invalid icon
+      console.error(`Error parsing ${name}:`, err);
+      iconSet.remove(name);
+      return;
+    }
+
+    // Update icon
+    iconSet.fromSVG(name, svg);
+  });
+
+  // Export as IconifyJSON
+  const exported = JSON.stringify(iconSet.export(), null, '\t') + '\n';
+
+  // Save to file
+  await fs.writeFile(savePath, exported, 'utf8');
+})();
