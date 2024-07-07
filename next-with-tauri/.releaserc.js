2e365d733f4be5d3279c3c13a24b2b1a3e7902b4
Draco <draco.coder@gmail.com>
2024-07-07 15:10:45 +0800

    chore: add next-with-tauri/.releaserc.js

---
+++ next-with-tauri/.releaserc.js
@@ -0,0 +1,26 @@
+const { createConfig } = require('semantic-release-config-gitmoji/lib/createConfig');
+
+const options = {
+  changelogTitle: `<a name="readme-top"></a>
+
+# Changelog`,
+  releaseRules: [
+    {
+      release: 'minor',
+      type: 'feat',
+    },
+    {
+      release: 'patch',
+      type: 'fix',
+    },
+    {
+      release: 'patch',
+      type: 'perf',
+    },
+    {
+      release: 'patch',
+      type: 'style',
+    },
+    {
+      release: 'patch',
+      type: 'refactor',
