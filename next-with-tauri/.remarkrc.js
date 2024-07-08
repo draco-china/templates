e2e402c28fb3dc00dc1e63e051aa56057a14b7e1
Draco <draco.coder@gmail.com>
2024-07-08 09:24:50 +0800

    chore: add next-with-tauri/.remarkrc.js

---
+++ next-with-tauri/.remarkrc.js
@@ -0,0 +1,26 @@
+const gfmHighlight = [
+  { from: 'Note', to: '[!NOTE]' },
+  { from: 'Tip', to: '[!TIP]' },
+  { from: 'Important', to: '[!IMPORTANT]' },
+  { from: 'Warning', to: '[!WARNING]' },
+  { from: 'Caution', to: '[!CAUTION]' },
+];
+
+function remarkGfmHighlight() {
+  return async (tree) => {
+    const { visit } = await import('unist-util-visit');
+    visit(tree, 'blockquote', (node) => {
+      visit(node.children[0], 'strong', (subnode) => {
+        if (subnode.position.start.column !== 3) return;
+        visit(subnode, 'text', (textnode) => {
+          if (!['Note', 'Tip', 'Important', 'Warning', 'Caution'].includes(textnode.value)) return;
+          for (const item of gfmHighlight) {
+            if (item.from !== textnode.value) continue;
+            subnode.type = 'text';
+            subnode.value = item.to;
+            return;
+          }
+        });
+      });
+    });
+  };
