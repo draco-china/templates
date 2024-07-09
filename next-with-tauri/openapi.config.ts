a294da69f3bdb2663628afc6c67d1064a10e23c2
Draco <draco.coder@gmail.com>
2024-07-09 09:05:34 +0800

    chore: extend next-with-tauri/openapi.config.ts

--- next-with-tauri/openapi.config.ts
+++ next-with-tauri/openapi.config.ts
@@ -1,3 +1,7 @@
 import { generateService } from '@umijs/openapi';
 
 generateService({
+  requestLibPath: "import request from '@/lib/request';",
+  schemaPath: 'https://generator.swagger.io/api/swagger.json',
+  // schemaPath: join(__dirname, 'openapi.json'),
+  // projectName: 'api',
