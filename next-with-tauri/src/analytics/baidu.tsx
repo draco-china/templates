ea74ef051c37a7e1ed67c314b9658886ae087aee
Draco <draco.coder@gmail.com>
2024-07-14 09:01:19 +0800

    chore: extend next-with-tauri/src/analytics/baidu.tsx

--- next-with-tauri/src/analytics/baidu.tsx
+++ next-with-tauri/src/analytics/baidu.tsx
@@ -5,3 +5,10 @@ export default function BaiduAnalytics() {
     BAIDU_ANALYTICS_ID && (
       <script
         dangerouslySetInnerHTML={{
+          __html: `
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?${BAIDU_ANALYTICS_ID}";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
