d27ffd85a589d564704bda436a1af03b846a4122
Draco <draco.coder@gmail.com>
2024-07-15 20:05:39 +0800

    chore: extend next-with-tauri/src/analytics/facebook.tsx

--- next-with-tauri/src/analytics/facebook.tsx
+++ next-with-tauri/src/analytics/facebook.tsx
@@ -9,3 +9,13 @@ export default function FacebookAnalytics() {
           async
           defer
           crossOrigin='anonymous'
+          src='https://connect.facebook.net/en_US/sdk.js'
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                appId: '${FACEBOOK_ANALYTICS_ID}',
+                xfbml: true,
+                version: 'v19.0'
