908a23064a0da34c26a076cbaef1a2be00450ff9
Draco <draco.coder@gmail.com>
2024-07-16 14:47:16 +0800

    chore: extend next-with-tauri/src/analytics/google.tsx

--- next-with-tauri/src/analytics/google.tsx
+++ next-with-tauri/src/analytics/google.tsx
@@ -1,3 +1,11 @@
 import {
   GoogleAnalytics as Analytics,
   GoogleTagManager as TagManager,
+} from '@next/third-parties/google';
+import { GOOGLE_ANALYTICS_ID, GOOGLE_TAG_MANAGER_ID } from '@/lib/constants';
+
+/**
+ * header script
+ */
+export function GoogleTagManager() {
+  return GOOGLE_TAG_MANAGER_ID && <TagManager gtmId={GOOGLE_TAG_MANAGER_ID} />;
