944d266e8d7638c2405dae901563c6c26a8fe368
Draco <draco.coder@gmail.com>
2024-07-23 19:51:19 +0800

    chore: extend next-with-tauri/src/app/page.tsx

--- next-with-tauri/src/app/page.tsx
+++ next-with-tauri/src/app/page.tsx
@@ -4,3 +4,7 @@ import { redirect } from 'next/navigation';
 import { useLanguage } from '@/i18n';
 
 export default function RootPage() {
+  const { lng } = useLanguage();
+
+  return redirect(`/${lng}`);
+}
