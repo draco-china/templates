482517fab49c0beed0501758ac8b1650e5038c31
Draco <draco.coder@gmail.com>
2024-07-23 20:56:48 +0800

    chore: extend next-with-tauri/src/app/robots.ts

--- next-with-tauri/src/app/robots.ts
+++ next-with-tauri/src/app/robots.ts
@@ -4,3 +4,8 @@ import { SITE_URL } from '@/lib/constants';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
