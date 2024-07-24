78ae2fe64037b6ac7304f021f12d143110811197
Draco <draco.coder@gmail.com>
2024-07-24 17:06:02 +0800

    chore: add next-with-tauri/src/app/sitemap.ts

---
+++ next-with-tauri/src/app/sitemap.ts
@@ -0,0 +1,7 @@
+import type { MetadataRoute } from 'next';
+import { languages } from '@/i18n';
+import { SITE_URL } from '@/lib/constants';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
