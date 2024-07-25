73964cb685177bb7bafc8a3c8dd24c54185d1e45
Draco <draco.coder@gmail.com>
2024-07-25 15:30:23 +0800

    chore: extend next-with-tauri/src/components/icon/index.tsx

--- next-with-tauri/src/components/icon/index.tsx
+++ next-with-tauri/src/components/icon/index.tsx
@@ -2,3 +2,8 @@
 
 import { icons as FlagPack } from '@iconify-json/flagpack';
 import { addCollection, Icon as Iconify, IconProps } from '@iconify/react';
+import Local from './local.json';
+
+addCollection(Local);
+addCollection(FlagPack);
+
