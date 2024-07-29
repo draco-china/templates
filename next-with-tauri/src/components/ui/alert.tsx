01f1d07cbbae8eb616977f78815773f029eecd0e
Draco <draco.coder@gmail.com>
2024-07-29 10:07:46 +0800

    chore: extend next-with-tauri/src/components/ui/alert.tsx

--- next-with-tauri/src/components/ui/alert.tsx
+++ next-with-tauri/src/components/ui/alert.tsx
@@ -37,3 +37,23 @@ const AlertTitle = React.forwardRef<
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
   <h5
+    ref={ref}
+    className={cn("mb-1 font-medium leading-none tracking-tight", className)}
+    {...props}
+  />
+))
+AlertTitle.displayName = "AlertTitle"
+
+const AlertDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
+    {...props}
+  />
+))
+AlertDescription.displayName = "AlertDescription"
+
+export { Alert, AlertTitle, AlertDescription }
