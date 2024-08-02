564109484bcdf056b2ee7fddffe06b773eb9a893
Draco <draco.coder@gmail.com>
2024-08-02 12:14:33 +0800

    chore: extend next-with-tauri/src/components/ui/badge.tsx

--- next-with-tauri/src/components/ui/badge.tsx
+++ next-with-tauri/src/components/ui/badge.tsx
@@ -20,3 +20,17 @@ const badgeVariants = cva(
     defaultVariants: {
       variant: "default",
     },
+  }
+)
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  )
+}
+
+export { Badge, badgeVariants }
