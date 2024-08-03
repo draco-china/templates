cd55d52722faa7f0b2245062e8a01d31db6e4b59
Draco <draco.coder@gmail.com>
2024-08-03 16:58:22 +0800

    chore: extend next-with-tauri/src/components/ui/breadcrumb.tsx

--- next-with-tauri/src/components/ui/breadcrumb.tsx
+++ next-with-tauri/src/components/ui/breadcrumb.tsx
@@ -76,3 +76,40 @@ const BreadcrumbSeparator = ({
   children,
   className,
   ...props
+}: React.ComponentProps<"li">) => (
+  <li
+    role="presentation"
+    aria-hidden="true"
+    className={cn("[&>svg]:size-3.5", className)}
+    {...props}
+  >
+    {children ?? <ChevronRightIcon />}
+  </li>
+)
+BreadcrumbSeparator.displayName = "BreadcrumbSeparator"
+
+const BreadcrumbEllipsis = ({
+  className,
+  ...props
+}: React.ComponentProps<"span">) => (
+  <span
+    role="presentation"
+    aria-hidden="true"
+    className={cn("flex h-9 w-9 items-center justify-center", className)}
+    {...props}
+  >
+    <DotsHorizontalIcon className="size-4" />
+    <span className="sr-only">More</span>
+  </span>
+)
+BreadcrumbEllipsis.displayName = "BreadcrumbElipssis"
+
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+}
