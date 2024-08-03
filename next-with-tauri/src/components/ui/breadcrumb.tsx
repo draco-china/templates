61b167f3b7c19a894883371f3dfb231d1431164c
Draco <draco.coder@gmail.com>
2024-08-03 15:02:33 +0800

    chore: extend next-with-tauri/src/components/ui/breadcrumb.tsx

--- next-with-tauri/src/components/ui/breadcrumb.tsx
+++ next-with-tauri/src/components/ui/breadcrumb.tsx
@@ -37,3 +37,42 @@ const BreadcrumbItem = React.forwardRef<
     {...props}
   />
 ))
+BreadcrumbItem.displayName = "BreadcrumbItem"
+
+const BreadcrumbLink = React.forwardRef<
+  HTMLAnchorElement,
+  React.ComponentPropsWithoutRef<"a"> & {
+    asChild?: boolean
+  }
+>(({ asChild, className, ...props }, ref) => {
+  const Comp = asChild ? Slot : "a"
+
+  return (
+    <Comp
+      ref={ref}
+      className={cn("transition-colors hover:text-foreground", className)}
+      {...props}
+    />
+  )
+})
+BreadcrumbLink.displayName = "BreadcrumbLink"
+
+const BreadcrumbPage = React.forwardRef<
+  HTMLSpanElement,
+  React.ComponentPropsWithoutRef<"span">
+>(({ className, ...props }, ref) => (
+  <span
+    ref={ref}
+    role="link"
+    aria-disabled="true"
+    aria-current="page"
+    className={cn("font-normal text-foreground", className)}
+    {...props}
+  />
+))
+BreadcrumbPage.displayName = "BreadcrumbPage"
+
+const BreadcrumbSeparator = ({
+  children,
+  className,
+  ...props
