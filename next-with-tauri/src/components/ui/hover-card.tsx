d537586d985af29f0260221acc5aefcef4f2bcc1
Draco <draco.coder@gmail.com>
2024-08-16 14:51:28 +0800

    chore: add next-with-tauri/src/components/ui/hover-card.tsx

---
+++ next-with-tauri/src/components/ui/hover-card.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import * as React from "react"
+import * as HoverCardPrimitive from "@radix-ui/react-hover-card"
+
+import { cn } from "@/lib/utils"
+
+const HoverCard = HoverCardPrimitive.Root
+
+const HoverCardTrigger = HoverCardPrimitive.Trigger
+
+const HoverCardContent = React.forwardRef<
