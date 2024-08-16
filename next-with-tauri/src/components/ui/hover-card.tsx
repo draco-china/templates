3f26411052d66ca3d9d9f331402c6b7dfa737556
Draco <draco.coder@gmail.com>
2024-08-16 15:24:47 +0800

    chore: extend next-with-tauri/src/components/ui/hover-card.tsx

--- next-with-tauri/src/components/ui/hover-card.tsx
+++ next-with-tauri/src/components/ui/hover-card.tsx
@@ -10,3 +10,11 @@ const HoverCard = HoverCardPrimitive.Root
 const HoverCardTrigger = HoverCardPrimitive.Trigger
 
 const HoverCardContent = React.forwardRef<
+  React.ElementRef<typeof HoverCardPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+  <HoverCardPrimitive.Content
+    ref={ref}
+    align={align}
+    sideOffset={sideOffset}
+    className={cn(
