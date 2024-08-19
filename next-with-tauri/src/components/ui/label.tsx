0c81632f529f49ce9491d41276e11920923545b2
Draco <draco.coder@gmail.com>
2024-08-19 17:44:22 +0800

    chore: extend next-with-tauri/src/components/ui/label.tsx

--- next-with-tauri/src/components/ui/label.tsx
+++ next-with-tauri/src/components/ui/label.tsx
@@ -16,3 +16,11 @@ const Label = React.forwardRef<
     VariantProps<typeof labelVariants>
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelVariants(), className)}
+    {...props}
+  />
+))
+Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label }
