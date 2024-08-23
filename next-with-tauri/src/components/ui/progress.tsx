c46b7ace9e28815405b3270e8fc2adfcbb8e3a0e
Draco <draco.coder@gmail.com>
2024-08-23 19:59:00 +0800

    chore: extend next-with-tauri/src/components/ui/progress.tsx

--- next-with-tauri/src/components/ui/progress.tsx
+++ next-with-tauri/src/components/ui/progress.tsx
@@ -9,3 +9,11 @@ const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
 >(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+      className
+    )}
+    {...props}
+  >
