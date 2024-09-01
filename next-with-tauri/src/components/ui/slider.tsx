7e0c968a20e63831dd3a054731570560b8d71eb2
Draco <draco.coder@gmail.com>
2024-09-01 12:10:37 +0800

    chore: extend next-with-tauri/src/components/ui/slider.tsx

--- next-with-tauri/src/components/ui/slider.tsx
+++ next-with-tauri/src/components/ui/slider.tsx
@@ -9,3 +9,11 @@ const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex w-full touch-none select-none items-center",
+      className
+    )}
+    {...props}
+  >
