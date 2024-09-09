8b4cd715b08fe913296c7d70f8cc0e64fc0d5d59
Draco <draco.coder@gmail.com>
2024-09-09 19:39:17 +0800

    chore: add next-with-tauri/src/components/ui/toggle-group.tsx

---
+++ next-with-tauri/src/components/ui/toggle-group.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import * as React from "react"
+import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group"
+import { VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+import { toggleVariants } from "@/components/ui/toggle"
+
+const ToggleGroupContext = React.createContext<
+  VariantProps<typeof toggleVariants>
+>({
+  size: "default",
+  variant: "default",
+})
+
+const ToggleGroup = React.forwardRef<
+  React.ElementRef<typeof ToggleGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> &
+    VariantProps<typeof toggleVariants>
+>(({ className, variant, size, children, ...props }, ref) => (
