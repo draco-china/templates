1b904130b18f59eabff4a02c55f087c5108a1a81
Draco <draco.coder@gmail.com>
2024-09-09 12:14:31 +0800

    chore: add next-with-tauri/src/components/ui/toaster.tsx

---
+++ next-with-tauri/src/components/ui/toaster.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
+
