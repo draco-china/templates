b45017fa5897f0c80e502938757a9a55d8eb519f
Draco <draco.coder@gmail.com>
2024-09-17 18:29:57 +0800

    chore: extend next-with-tauri/src/layout/footer.tsx

--- next-with-tauri/src/layout/footer.tsx
+++ next-with-tauri/src/layout/footer.tsx
@@ -22,3 +22,27 @@ import { SwitchMode } from './switch-mode';
 const Links = [
   {
     icon: <EnvelopeClosedIcon className='size-4' />,
+    href: `mailto:${EMAIL}`,
+  },
+  {
+    icon: <GitHubLogoIcon className='size-4' />,
+    href: GITHUB_LINK,
+  },
+  {
+    icon: <TwitterLogoIcon className='size-4' />,
+    href: TWITTER_LINK,
+  },
+  {
+    icon: <DiscordLogoIcon className='size-4' />,
+    href: DISCORD_LINK,
+  },
+  {
+    icon: <InstagramLogoIcon className='size-4' />,
+    href: INSTAGRAM_LINK,
+  },
+  {
+    icon: <LinkedInLogoIcon className='size-4' />,
+    href: LINKEDIN_LINK,
+  },
+];
+
