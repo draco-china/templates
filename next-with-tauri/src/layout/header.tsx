0c9ec948455f96db20f6d6505511a95759de5141
Draco <draco.coder@gmail.com>
2024-09-19 11:30:37 +0800

    chore: extend next-with-tauri/src/layout/header.tsx

--- next-with-tauri/src/layout/header.tsx
+++ next-with-tauri/src/layout/header.tsx
@@ -6,3 +6,11 @@ export function LayoutHeader({ lng }: { lng: string }) {
   return (
     <header className='sticky top-0 z-40 h-auto w-full border-b pt-[calc(env(safe-area-inset-top))] backdrop-blur-md'>
       <div className='container flex h-16 items-center justify-between'>
+        <div className='flex gap-6'>
+          <Link href={`/${lng}`} className='flex items-center space-x-2'>
+            <VercelLogoIcon className='size-6' />
+            <span className='inline-block font-bold'>Next Template</span>
+          </Link>
+        </div>
+
+        <div className='flex flex-1 items-center justify-end gap-4'>
