b0b856d608c53289f79667094371252dc9f142b6
Draco <draco.coder@gmail.com>
2024-09-20 16:14:53 +0800

    chore: extend next-with-tauri/src/layout/switch-language.tsx

--- next-with-tauri/src/layout/switch-language.tsx
+++ next-with-tauri/src/layout/switch-language.tsx
@@ -11,3 +11,16 @@ export default function SwitchLanguage() {
   const mounted = useMounted();
   const { lng, changeLanguage } = useLanguage();
   const { t } = useTranslation(lng, 'language');
+  const country = getCountry(lng);
+
+  if (!mounted) return <Skeleton className='h-8 w-14 rounded-full' />;
+
+  return (
+    <Select value={lng} onValueChange={changeLanguage}>
+      <SelectTrigger
+        aria-label='Select Language'
+        className='h-8 w-14 rounded-full px-2 shadow-none'
+      >
+        <Icon icon={`flagpack:${country?.alpha2.toLowerCase()}`} className='size-5' />
+      </SelectTrigger>
+      <SelectContent align='end'>
