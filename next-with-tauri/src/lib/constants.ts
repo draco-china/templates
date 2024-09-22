9def75192c3edc0ec91921237b4db1e8a1945fde
Draco <draco.coder@gmail.com>
2024-09-22 10:48:01 +0800

    chore: extend next-with-tauri/src/lib/constants.ts

--- next-with-tauri/src/lib/constants.ts
+++ next-with-tauri/src/lib/constants.ts
@@ -14,3 +14,10 @@ export const GITHUB_LINK = process.env.GITHUB_LINK;
 export const TWITTER_LINK = process.env.TWITTER_LINK;
 export const DISCORD_LINK = process.env.DISCORD_LINK;
 export const INSTAGRAM_LINK = process.env.INSTAGRAM_LINK;
+export const LINKEDIN_LINK = process.env.LINKEDIN_LINK;
+
+export const GOOGLE_TAG_MANAGER_ID = process.env.GOOGLE_TAG_MANAGER_ID;
+export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
+export const YANDEX_ANALYTICS_ID = process.env.YANDEX_ANALYTICS_ID;
+export const FACEBOOK_ANALYTICS_ID = process.env.FACEBOOK_ANALYTICS_ID;
+export const BAIDU_ANALYTICS_ID = process.env.BAIDU_ANALYTICS_ID;
