e2700bce4976f7dd77ec12e130d9c71f10170569
Draco <draco.coder@gmail.com>
2024-09-22 10:00:01 +0800

    chore: extend next-with-tauri/src/lib/constants.ts

--- next-with-tauri/src/lib/constants.ts
+++ next-with-tauri/src/lib/constants.ts
@@ -6,3 +6,11 @@ export const DEFAULT_SYSTEM_MODE = DEFAULT_MODE === 'system' ? 'light' : DEFAULT
 export const DEFAULT_THEME = process.env.DEFAULT_THEME || 'blue';
 
 export const SITE_URL = process.env.SITE_URL || 'http://localhost:3000';
+export const API_URL = process.env.API_URL || '';
+
+export const EMAIL = process.env.EMAIL;
+
+export const GITHUB_LINK = process.env.GITHUB_LINK;
+export const TWITTER_LINK = process.env.TWITTER_LINK;
+export const DISCORD_LINK = process.env.DISCORD_LINK;
+export const INSTAGRAM_LINK = process.env.INSTAGRAM_LINK;
