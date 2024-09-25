1c48aad7f1623d64ffccefaeee0ab441a001ca87
Draco <draco.coder@gmail.com>
2024-09-25 19:20:53 +0800

    chore: extend next-with-tauri/src/lib/site.ts

--- next-with-tauri/src/lib/site.ts
+++ next-with-tauri/src/lib/site.ts
@@ -19,3 +19,23 @@ export const metadata: Metadata = {
       },
       {
         url: '/favicon-32x32.png',
+        sizes: '32x32',
+      },
+      {
+        url: '/favicon.ico',
+        sizes: '48x48',
+      },
+    ],
+    apple: {
+      url: '/apple-touch-icon.png',
+    },
+  },
+  metadataBase: new URL(SITE_URL),
+  alternates: {
+    canonical: '/',
+    languages: Object.fromEntries(languages.map((language) => [language, `/${language}`])),
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
