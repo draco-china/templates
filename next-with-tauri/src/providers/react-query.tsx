c51477fe48d9b2cd547bf7404bd902849fe2a725
Draco <draco.coder@gmail.com>
2024-09-26 20:44:12 +0800

    chore: add next-with-tauri/src/providers/react-query.tsx

---
+++ next-with-tauri/src/providers/react-query.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { lazy, ReactNode, Suspense, useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import('@tanstack/react-query-devtools/build/modern/production.js').then((d) => ({
+    default: d.ReactQueryDevtools,
+  })),
+);
+
+export default function ReactQueryProvider(props: { children: ReactNode }) {
+  // Instead do this, which ensures each request has its own cache:
+  const [queryClient] = useState(
