f194968e1806e2792e43c00f557ac0d130ef312d
Draco <draco.coder@gmail.com>
2024-09-28 19:33:05 +0800

    chore: extend next-with-tauri/src/stores/global.ts

--- next-with-tauri/src/stores/global.ts
+++ next-with-tauri/src/stores/global.ts
@@ -56,3 +56,34 @@ export function setMode(mode: GlobalState['mode']) {
   el.style.colorScheme = globalState.currentMode;
   if (globalState.systemMode !== getSystemMode()) {
     globalState.systemMode = getSystemMode();
+    setCookie('systemMode', getSystemMode());
+  }
+}
+
+export function toggleMode(mode: GlobalState['mode'], coordinate?: { x: number; y: number }) {
+  const isDark = document.documentElement.getAttribute('data-mode') === 'dark';
+  if (!document.startViewTransition || checkMode(mode)) {
+    return setMode(mode);
+  }
+
+  const transition = document.startViewTransition(() => setMode(mode));
+
+  // 传入触发点坐标，从点击处开始扩散。否则，从🈶下角开始扩散
+  const x = coordinate?.x ?? window.innerWidth;
+  const y = coordinate?.y ?? window.innerHeight;
+
+  const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y));
+  transition.ready.then(() => {
+    const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`];
+    document.documentElement.animate(
+      {
+        clipPath: !isDark ? clipPath : [...clipPath].reverse(),
+      },
+      {
+        duration: 500,
+        easing: 'ease-in',
+        pseudoElement: !isDark ? '::view-transition-new(root)' : '::view-transition-old(root)',
+      },
+    );
+  });
+}
