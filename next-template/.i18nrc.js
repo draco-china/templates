d67f0347a971f7e9f31328b7904a5b76e8dd3e98
Draco <draco.coder@gmail.com>
2024-10-07 18:20:20 +0800

    chore: extend next-template/.i18nrc.js

--- next-template/.i18nrc.js
+++ next-template/.i18nrc.js
@@ -1,3 +1,6 @@
 const { defineConfig } = require('@lobehub/i18n-cli');
 
 module.exports = defineConfig({
+  entry: './src/i18n/locales/en-US',
+  entryLocale: 'en-US',
+  output: './src/i18n/locales',
