92936b062b5c201f6d38e4c458382139c26a57ee
Draco <draco.coder@gmail.com>
2024-10-19 13:51:31 +0800

    chore: add next-template/.releaserc.js

---
+++ next-template/.releaserc.js
@@ -0,0 +1,8 @@
+const { createConfig } = require('semantic-release-config-gitmoji/lib/createConfig');
+
+const options = {
+  changelogTitle: `<a name="readme-top"></a>
+
+# Changelog`,
+  releaseRules: [
+    {
