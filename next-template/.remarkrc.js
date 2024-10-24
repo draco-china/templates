fc76541a4e2632c09f7eb1dfd3583aa504312c6a
Draco <draco.coder@gmail.com>
2024-10-24 20:27:02 +0800

    chore: extend next-template/.remarkrc.js

--- next-template/.remarkrc.js
+++ next-template/.remarkrc.js
@@ -3,3 +3,10 @@ const gfmHighlight = [
   { from: 'Tip', to: '[!TIP]' },
   { from: 'Important', to: '[!IMPORTANT]' },
   { from: 'Warning', to: '[!WARNING]' },
+  { from: 'Caution', to: '[!CAUTION]' },
+];
+
+function remarkGfmHighlight() {
+  return async (tree) => {
+    const { visit } = await import('unist-util-visit');
+    visit(tree, 'blockquote', (node) => {
