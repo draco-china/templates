0b5334c91b6d98674cffd728d7732ea11542c26a
Draco <draco.coder@gmail.com>
2024-10-31 19:41:05 +0800

    chore: extend next-template/openapi.config.ts

--- next-template/openapi.config.ts
+++ next-template/openapi.config.ts
@@ -2,3 +2,6 @@ import { generateService } from '@umijs/openapi';
 
 generateService({
   requestLibPath: "import request from '@/lib/request';",
+  schemaPath: 'https://generator.swagger.io/api/swagger.json',
+  // schemaPath: join(__dirname, 'openapi.json'),
+  // projectName: 'api',
