862af9c5a24a89775942bf69807d7be4f7c17947
Draco <draco.coder@gmail.com>
2024-11-13 09:47:42 +0800

    chore: extend next-template/src/analytics/google.tsx

--- next-template/src/analytics/google.tsx
+++ next-template/src/analytics/google.tsx
@@ -2,3 +2,8 @@ import {
   GoogleAnalytics as Analytics,
   GoogleTagManager as TagManager,
 } from '@next/third-parties/google';
+import { GOOGLE_ANALYTICS_ID, GOOGLE_TAG_MANAGER_ID } from '@/lib/constants';
+
+/**
+ * header script
+ */
