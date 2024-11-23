df74f34e30ae19e5ee5e64e267b22ef3a60a6b5c
Draco <draco.coder@gmail.com>
2024-11-23 14:05:32 +0800

    chore: extend next-template/src/analytics/yandex.tsx

--- next-template/src/analytics/yandex.tsx
+++ next-template/src/analytics/yandex.tsx
@@ -7,3 +7,5 @@ export default function YandexAnalytics() {
         dangerouslySetInnerHTML={{
           __html: `
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
