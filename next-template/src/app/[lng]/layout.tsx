4ceb2f7294775c5999b1bc46a3dfb85d3e4bf1dc
Draco <draco.coder@gmail.com>
2024-11-26 11:33:30 +0800

    chore: add next-template/src/app/[lng]/layout.tsx

---
+++ next-template/src/app/[lng]/layout.tsx
@@ -0,0 +1,4 @@
+import { Noto_Sans as FontSans } from 'next/font/google';
+import { BodyAnalytics, HeadAnalytics } from '@/analytics';
+import { dir, languages } from '@/i18n';
+import LayoutFooter from '@/layout/footer';
