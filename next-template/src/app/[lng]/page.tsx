5b3ab7db1b8596acefd3ca561a1c22c9ba579e63
Draco <draco.coder@gmail.com>
2024-11-30 15:34:27 +0800

    chore: extend next-template/src/app/[lng]/page.tsx

--- next-template/src/app/[lng]/page.tsx
+++ next-template/src/app/[lng]/page.tsx
@@ -11,3 +11,11 @@ export default async function Home({ params: { lng } }: { params: { lng: string 
         <Image
           className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
           src='/next.svg'
+          alt='Next.js Logo'
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <Button className='my-2'>
