6101c91062dda80fbf0500e7ca518dbbfbf896b6
Draco <draco.coder@gmail.com>
2024-12-02 14:28:16 +0800

    chore: extend next-template/src/app/[lng]/page.tsx

--- next-template/src/app/[lng]/page.tsx
+++ next-template/src/app/[lng]/page.tsx
@@ -61,3 +61,10 @@ export default async function Home({ params: { lng } }: { params: { lng: string 
         >
           <h2 className='mb-3 text-2xl font-semibold'>
             {t('templates')}
+            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+              -&gt;
+            </span>
+          </h2>
+          <p className='m-0 max-w-[30ch] text-sm opacity-50'>{t('templates.description')}</p>
+        </a>
+
