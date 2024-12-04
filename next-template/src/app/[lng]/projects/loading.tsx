582ce23fa8e7731b4881eeba9161bafc252e01e4
Draco <draco.coder@gmail.com>
2024-12-04 16:07:50 +0800

    chore: extend next-template/src/app/[lng]/projects/loading.tsx

--- next-template/src/app/[lng]/projects/loading.tsx
+++ next-template/src/app/[lng]/projects/loading.tsx
@@ -3,3 +3,5 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function Loading() {
   return (
     <div className='container grid gap-6 py-6 lg:grid-cols-3'>
+      {[...new Array(9).fill('')].map((item, index) => {
+        return (
