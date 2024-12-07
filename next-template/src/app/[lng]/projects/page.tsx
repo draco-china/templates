a0ef286d22745459e8f05303e03ca6e967adcc18
Draco <draco.coder@gmail.com>
2024-12-07 12:15:17 +0800

    chore: extend next-template/src/app/[lng]/projects/page.tsx

--- next-template/src/app/[lng]/projects/page.tsx
+++ next-template/src/app/[lng]/projects/page.tsx
@@ -2,3 +2,8 @@
 
 import Link from 'next/link';
 import { Share1Icon, StarIcon } from '@radix-ui/react-icons';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import request from '@/lib/request';
+import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+
