b1de2f75cbe147f501d98f7a83a5bbd1d1297566
Draco <draco.coder@gmail.com>
2024-12-22 12:03:36 +0800

    chore: extend next-template/src/app/sitemap.ts

--- next-template/src/app/sitemap.ts
+++ next-template/src/app/sitemap.ts
@@ -2,3 +2,4 @@ import type { MetadataRoute } from 'next';
 import { languages } from '@/i18n';
 import { SITE_URL } from '@/lib/constants';
 
+export default function sitemap(): MetadataRoute.Sitemap {
