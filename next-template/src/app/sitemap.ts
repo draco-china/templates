42ce5e3b443f8759fa5008c058415875e6ca41c1
Draco <draco.coder@gmail.com>
2024-12-23 19:37:58 +0800

    chore: extend next-template/src/app/sitemap.ts

--- next-template/src/app/sitemap.ts
+++ next-template/src/app/sitemap.ts
@@ -12,3 +12,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
           (acc, lng) => {
             acc[lng] = `${SITE_URL}/${lng}`;
             return acc;
+          },
+          {} as Record<string, string>,
