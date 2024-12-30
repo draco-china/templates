5ec42fe6dcc146494539914893deabbd6fe79746
Draco <draco.coder@gmail.com>
2024-12-30 10:55:03 +0800

    chore: extend next-template/src/components/ui/accordion.tsx

--- next-template/src/components/ui/accordion.tsx
+++ next-template/src/components/ui/accordion.tsx
@@ -2,3 +2,10 @@
 
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import { ChevronDownIcon } from "@radix-ui/react-icons"
+
+import { cn } from "@/lib/utils"
+
+const Accordion = AccordionPrimitive.Root
+
+const AccordionItem = React.forwardRef<
