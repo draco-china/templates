7d0f8834acb3b2299a85eca0a7059a643cec2e05
Draco <draco.coder@gmail.com>
2024-12-30 18:36:53 +0800

    chore: extend next-template/src/components/ui/accordion.tsx

--- next-template/src/components/ui/accordion.tsx
+++ next-template/src/components/ui/accordion.tsx
@@ -13,3 +13,7 @@ const AccordionItem = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
+    ref={ref}
+    className={cn("border-b", className)}
+    {...props}
+  />
