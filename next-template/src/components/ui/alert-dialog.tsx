25a2f7e91580c95417d042f4c42dd4738eaa0874
Draco <draco.coder@gmail.com>
2025-01-03 16:48:02 +0800

    chore: extend next-template/src/components/ui/alert-dialog.tsx

--- next-template/src/components/ui/alert-dialog.tsx
+++ next-template/src/components/ui/alert-dialog.tsx
@@ -45,3 +45,12 @@ const AlertDialogContent = React.forwardRef<
 ))
 AlertDialogContent.displayName = AlertDialogPrimitive.Content.displayName
 
+const AlertDialogHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col space-y-2 text-center sm:text-left",
+      className
+    )}
