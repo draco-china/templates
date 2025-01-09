9a29872bb0e0075d6b58eaf816f5206ea8be7c31
Draco <draco.coder@gmail.com>
2025-01-09 17:13:37 +0800

    chore: extend next-template/src/components/ui/alert.tsx

--- next-template/src/components/ui/alert.tsx
+++ next-template/src/components/ui/alert.tsx
@@ -21,3 +21,7 @@ const alertVariants = cva(
 
 const Alert = React.forwardRef<
   HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+  <div
+    ref={ref}
