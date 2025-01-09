12f17b31cea3fceea53649e3162d9402ca1250bf
Draco <draco.coder@gmail.com>
2025-01-09 17:45:18 +0800

    chore: extend next-template/src/components/ui/alert.tsx

--- next-template/src/components/ui/alert.tsx
+++ next-template/src/components/ui/alert.tsx
@@ -25,3 +25,7 @@ const Alert = React.forwardRef<
 >(({ className, variant, ...props }, ref) => (
   <div
     ref={ref}
+    role="alert"
+    className={cn(alertVariants({ variant }), className)}
+    {...props}
+  />
