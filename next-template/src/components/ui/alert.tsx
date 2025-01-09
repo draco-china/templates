94deddbd05bb754ca5743a456d9a2d71d0119a98
Draco <draco.coder@gmail.com>
2025-01-09 20:51:33 +0800

    chore: extend next-template/src/components/ui/alert.tsx

--- next-template/src/components/ui/alert.tsx
+++ next-template/src/components/ui/alert.tsx
@@ -34,3 +34,7 @@ Alert.displayName = "Alert"
 
 const AlertTitle = React.forwardRef<
   HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h5
+    ref={ref}
