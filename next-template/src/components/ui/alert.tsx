66b037fd7634f1fb2147a45c94e8063a2cc0988e
Draco <draco.coder@gmail.com>
2025-01-11 16:08:12 +0800

    chore: extend next-template/src/components/ui/alert.tsx

--- next-template/src/components/ui/alert.tsx
+++ next-template/src/components/ui/alert.tsx
@@ -47,3 +47,7 @@ AlertTitle.displayName = "AlertTitle"
 const AlertDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
