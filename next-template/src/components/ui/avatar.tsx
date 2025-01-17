9e7724ebf4a5a52672cba3cd77e41a88d60a5499
Draco <draco.coder@gmail.com>
2025-01-17 15:48:02 +0800

    chore: extend next-template/src/components/ui/avatar.tsx

--- next-template/src/components/ui/avatar.tsx
+++ next-template/src/components/ui/avatar.tsx
@@ -36,3 +36,7 @@ const AvatarFallback = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Fallback>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
 >(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-full bg-muted",
