30d97798be4ab7e6e0a0eb4e6f9644578ca5910e
Draco <draco.coder@gmail.com>
2025-01-25 10:05:17 +0800

    chore: extend next-template/src/components/ui/breadcrumb.tsx

--- next-template/src/components/ui/breadcrumb.tsx
+++ next-template/src/components/ui/breadcrumb.tsx
@@ -61,3 +61,11 @@ const BreadcrumbPage = React.forwardRef<
   HTMLSpanElement,
   React.ComponentPropsWithoutRef<"span">
 >(({ className, ...props }, ref) => (
+  <span
+    ref={ref}
+    role="link"
+    aria-disabled="true"
+    aria-current="page"
+    className={cn("font-normal text-foreground", className)}
+    {...props}
+  />
