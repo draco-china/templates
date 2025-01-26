d714ddf0ebc5e0e8f07ce1e08541d4bfe6cd1c2a
Draco <draco.coder@gmail.com>
2025-01-26 11:44:04 +0800

    chore: extend next-template/src/components/ui/breadcrumb.tsx

--- next-template/src/components/ui/breadcrumb.tsx
+++ next-template/src/components/ui/breadcrumb.tsx
@@ -95,3 +95,12 @@ const BreadcrumbEllipsis = ({
   <span
     role="presentation"
     aria-hidden="true"
+    className={cn("flex h-9 w-9 items-center justify-center", className)}
+    {...props}
+  >
+    <DotsHorizontalIcon className="size-4" />
+    <span className="sr-only">More</span>
+  </span>
+)
+BreadcrumbEllipsis.displayName = "BreadcrumbElipssis"
+
