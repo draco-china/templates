ec1d84e9030016838a67ef4dde2bae35d0d777ca
Draco <draco.coder@gmail.com>
2025-01-29 17:21:53 +0800

    chore: extend next-template/src/components/ui/button.tsx

--- next-template/src/components/ui/button.tsx
+++ next-template/src/components/ui/button.tsx
@@ -37,3 +37,8 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
