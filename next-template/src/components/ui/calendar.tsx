19d487767cfddb0be9e027b9012b42b80fda3e24
Draco <draco.coder@gmail.com>
2025-02-01 09:10:01 +0800

    chore: extend next-template/src/components/ui/calendar.tsx

--- next-template/src/components/ui/calendar.tsx
+++ next-template/src/components/ui/calendar.tsx
@@ -5,3 +5,10 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+
+export type CalendarProps = React.ComponentProps<typeof DayPicker>
+
+function Calendar({
+  className,
+  classNames,
