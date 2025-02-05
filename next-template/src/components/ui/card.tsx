3c72069aca7db6645f7ea9bd9b4134ba0002621a
Draco <draco.coder@gmail.com>
2025-02-05 16:30:10 +0800

    chore: extend next-template/src/components/ui/card.tsx

--- next-template/src/components/ui/card.tsx
+++ next-template/src/components/ui/card.tsx
@@ -5,3 +5,8 @@ import { cn } from "@/lib/utils"
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-xl border bg-card text-card-foreground shadow",
