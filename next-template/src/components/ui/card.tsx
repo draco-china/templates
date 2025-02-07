7e23f8f90683a9f2c0a5eef6e75b45e5290452e9
Draco <draco.coder@gmail.com>
2025-02-07 13:06:48 +0800

    chore: extend next-template/src/components/ui/card.tsx

--- next-template/src/components/ui/card.tsx
+++ next-template/src/components/ui/card.tsx
@@ -44,3 +44,9 @@ CardTitle.displayName = "CardTitle"
 const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
