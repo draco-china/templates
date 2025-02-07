48accd39074422b801538fbaea0116129b1ebee4
Draco <draco.coder@gmail.com>
2025-02-07 20:08:39 +0800

    chore: extend next-template/src/components/ui/card.tsx

--- next-template/src/components/ui/card.tsx
+++ next-template/src/components/ui/card.tsx
@@ -62,3 +62,8 @@ const CardContent = React.forwardRef<
 CardContent.displayName = "CardContent"
 
 const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
