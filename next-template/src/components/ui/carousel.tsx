818bed90f83d54766d40e16f5e8d61c78950259e
Draco <draco.coder@gmail.com>
2025-02-13 15:00:47 +0800

    chore: extend next-template/src/components/ui/carousel.tsx

--- next-template/src/components/ui/carousel.tsx
+++ next-template/src/components/ui/carousel.tsx
@@ -203,3 +203,21 @@ const CarouselPrevious = React.forwardRef<
   return (
     <Button
       ref={ref}
+      variant={variant}
+      size={size}
+      className={cn(
+        "absolute  h-8 w-8 rounded-full",
+        orientation === "horizontal"
+          ? "-left-12 top-1/2 -translate-y-1/2"
+          : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
+        className
+      )}
+      disabled={!canScrollPrev}
+      onClick={scrollPrev}
+      {...props}
+    >
+      <ArrowLeftIcon className="size-4" />
+      <span className="sr-only">Previous slide</span>
+    </Button>
+  )
+})
