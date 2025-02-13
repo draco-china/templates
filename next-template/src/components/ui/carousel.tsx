bd19c0bfaeee971348409169c047ae5a818e16b1
Draco <draco.coder@gmail.com>
2025-02-13 20:20:58 +0800

    chore: extend next-template/src/components/ui/carousel.tsx

--- next-template/src/components/ui/carousel.tsx
+++ next-template/src/components/ui/carousel.tsx
@@ -221,3 +221,23 @@ const CarouselPrevious = React.forwardRef<
     </Button>
   )
 })
+CarouselPrevious.displayName = "CarouselPrevious"
+
+const CarouselNext = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<typeof Button>
+>(({ className, variant = "outline", size = "icon", ...props }, ref) => {
+  const { orientation, scrollNext, canScrollNext } = useCarousel()
+
+  return (
+    <Button
+      ref={ref}
+      variant={variant}
+      size={size}
+      className={cn(
+        "absolute h-8 w-8 rounded-full",
+        orientation === "horizontal"
+          ? "-right-12 top-1/2 -translate-y-1/2"
+          : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
+        className
+      )}
