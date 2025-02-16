683542179fa7dc7b7ccca46f1e4037124ebb9767
Draco <draco.coder@gmail.com>
2025-02-16 19:53:10 +0800

    chore: extend next-template/src/components/ui/checkbox.tsx

--- next-template/src/components/ui/checkbox.tsx
+++ next-template/src/components/ui/checkbox.tsx
@@ -7,3 +7,5 @@ import { CheckIcon } from "@radix-ui/react-icons"
 import { cn } from "@/lib/utils"
 
 const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
