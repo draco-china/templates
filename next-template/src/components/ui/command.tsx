12aad323ae12d820a5fb3d14e27dfa34e93bd8aa
Draco <draco.coder@gmail.com>
2025-02-21 18:03:23 +0800

    chore: extend next-template/src/components/ui/command.tsx

--- next-template/src/components/ui/command.tsx
+++ next-template/src/components/ui/command.tsx
@@ -11,3 +11,15 @@ import { Dialog, DialogContent } from "@/components/ui/dialog"
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive
+    ref={ref}
+    className={cn(
+      "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
+      className
+    )}
+    {...props}
+  />
+))
+Command.displayName = CommandPrimitive.displayName
+
