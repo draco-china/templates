c480219961e830f296f759328e2d9f3d24c737f0
Draco <draco.coder@gmail.com>
2025-02-24 18:24:59 +0800

    chore: extend next-template/src/components/ui/command.tsx

--- next-template/src/components/ui/command.tsx
+++ next-template/src/components/ui/command.tsx
@@ -94,3 +94,15 @@ const CommandGroup = React.forwardRef<
     )}
     {...props}
   />
+))
+
+CommandGroup.displayName = CommandPrimitive.Group.displayName
+
+const CommandSeparator = React.forwardRef<
+  React.ElementRef<typeof CommandPrimitive.Separator>,
+  React.ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>
+>(({ className, ...props }, ref) => (
+  <CommandPrimitive.Separator
+    ref={ref}
+    className={cn("-mx-1 h-px bg-border", className)}
+    {...props}
