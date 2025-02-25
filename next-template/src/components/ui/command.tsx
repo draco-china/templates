bc56dc9c1034d69ef74bb0b86c110d813b5bf15b
Draco <draco.coder@gmail.com>
2025-02-25 18:34:58 +0800

    chore: extend next-template/src/components/ui/command.tsx

--- next-template/src/components/ui/command.tsx
+++ next-template/src/components/ui/command.tsx
@@ -130,3 +130,15 @@ const CommandShortcut = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      className={cn(
+        "ml-auto text-xs tracking-widest text-muted-foreground",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+CommandShortcut.displayName = "CommandShortcut"
+
