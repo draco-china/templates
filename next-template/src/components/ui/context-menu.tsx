457a4ca8b65011a39359c6c1e3b66524520f0650
Draco <draco.coder@gmail.com>
2025-02-26 09:39:00 +0800

    chore: add next-template/src/components/ui/context-menu.tsx

---
+++ next-template/src/components/ui/context-menu.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import * as React from "react"
+import * as ContextMenuPrimitive from "@radix-ui/react-context-menu"
+import {
+  CheckIcon,
+  ChevronRightIcon,
+  DotFilledIcon,
+} from "@radix-ui/react-icons"
+
+import { cn } from "@/lib/utils"
+
+const ContextMenu = ContextMenuPrimitive.Root
+
+const ContextMenuTrigger = ContextMenuPrimitive.Trigger
+
+const ContextMenuGroup = ContextMenuPrimitive.Group
+
+const ContextMenuPortal = ContextMenuPrimitive.Portal
+
+const ContextMenuSub = ContextMenuPrimitive.Sub
+
