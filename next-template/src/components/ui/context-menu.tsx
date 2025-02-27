9685b6a39eecceb5ddf1d47fbf12c270854476bd
Draco <draco.coder@gmail.com>
2025-02-27 14:04:31 +0800

    chore: extend next-template/src/components/ui/context-menu.tsx

--- next-template/src/components/ui/context-menu.tsx
+++ next-template/src/components/ui/context-menu.tsx
@@ -81,3 +81,18 @@ const ContextMenuItem = React.forwardRef<
     inset?: boolean
   }
 >(({ className, inset, ...props }, ref) => (
+  <ContextMenuPrimitive.Item
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      inset && "pl-8",
+      className
+    )}
+    {...props}
+  />
+))
+ContextMenuItem.displayName = ContextMenuPrimitive.Item.displayName
+
+const ContextMenuCheckboxItem = React.forwardRef<
+  React.ElementRef<typeof ContextMenuPrimitive.CheckboxItem>,
+  React.ComponentPropsWithoutRef<typeof ContextMenuPrimitive.CheckboxItem>
