841b3fa5a0fd709414f9e986e0543d54db611687
Draco <draco.coder@gmail.com>
2025-03-01 20:30:09 +0800

    chore: extend next-template/src/components/ui/context-menu.tsx

--- next-template/src/components/ui/context-menu.tsx
+++ next-template/src/components/ui/context-menu.tsx
@@ -187,3 +187,18 @@ ContextMenuShortcut.displayName = "ContextMenuShortcut"
 
 export {
   ContextMenu,
+  ContextMenuTrigger,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuCheckboxItem,
+  ContextMenuRadioItem,
+  ContextMenuLabel,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuGroup,
+  ContextMenuPortal,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuRadioGroup,
+}
