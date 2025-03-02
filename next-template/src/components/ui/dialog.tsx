8b6b4479dc0adc2f2d3520c0396fdfb1763d6a18
Draco <draco.coder@gmail.com>
2025-03-02 11:36:16 +0800

    chore: add next-template/src/components/ui/dialog.tsx

---
+++ next-template/src/components/ui/dialog.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import * as React from "react"
+import * as DialogPrimitive from "@radix-ui/react-dialog"
+import { Cross2Icon } from "@radix-ui/react-icons"
+
+import { cn } from "@/lib/utils"
+
+const Dialog = DialogPrimitive.Root
+
+const DialogTrigger = DialogPrimitive.Trigger
+
+const DialogPortal = DialogPrimitive.Portal
+
