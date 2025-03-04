34e5ca34f39be83479aa0d3ecc4eb8d198691975
Draco <draco.coder@gmail.com>
2025-03-04 14:06:40 +0800

    chore: extend next-template/src/components/ui/dialog.tsx

--- next-template/src/components/ui/dialog.tsx
+++ next-template/src/components/ui/dialog.tsx
@@ -57,3 +57,11 @@ const DialogHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col space-y-1.5 text-center sm:text-left",
+      className
+    )}
+    {...props}
+  />
+)
