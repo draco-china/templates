6f3ca5b89a979ac37f18fe1e2add63202555cf67
Draco <draco.coder@gmail.com>
2025-03-05 14:59:41 +0800

    chore: extend next-template/src/components/ui/dialog.tsx

--- next-template/src/components/ui/dialog.tsx
+++ next-template/src/components/ui/dialog.tsx
@@ -102,3 +102,12 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+DialogDescription.displayName = DialogPrimitive.Description.displayName
+
+export {
+  Dialog,
+  DialogPortal,
