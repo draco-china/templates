d82d6a9608248ee6158422f26b893cd8bf0e7e5f
Draco <draco.coder@gmail.com>
2025-03-06 13:09:59 +0800

    chore: add next-template/src/components/ui/drawer.tsx

---
+++ next-template/src/components/ui/drawer.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import * as React from "react"
+import { Drawer as DrawerPrimitive } from "vaul"
+
+import { cn } from "@/lib/utils"
+
+const Drawer = ({
+  shouldScaleBackground = true,
+  ...props
+}: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
