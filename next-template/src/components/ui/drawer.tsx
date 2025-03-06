dd0abbb0d750ea25cd85ecd56331171d53a1c9ce
Draco <draco.coder@gmail.com>
2025-03-06 15:15:44 +0800

    chore: extend next-template/src/components/ui/drawer.tsx

--- next-template/src/components/ui/drawer.tsx
+++ next-template/src/components/ui/drawer.tsx
@@ -9,3 +9,14 @@ const Drawer = ({
   shouldScaleBackground = true,
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
+  <DrawerPrimitive.Root
+    shouldScaleBackground={shouldScaleBackground}
+    {...props}
+  />
+)
+Drawer.displayName = "Drawer"
+
+const DrawerTrigger = DrawerPrimitive.Trigger
+
+const DrawerPortal = DrawerPrimitive.Portal
+
