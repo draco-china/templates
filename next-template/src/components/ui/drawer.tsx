402cfa0f69bf2ba1cd8f56bd0c97d55e6b1f59c6
Draco <draco.coder@gmail.com>
2025-03-08 13:17:21 +0800

    chore: extend next-template/src/components/ui/drawer.tsx

--- next-template/src/components/ui/drawer.tsx
+++ next-template/src/components/ui/drawer.tsx
@@ -38,3 +38,11 @@ const DrawerContent = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
+  <DrawerPortal>
+    <DrawerOverlay />
+    <DrawerPrimitive.Content
+      ref={ref}
+      className={cn(
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        className
+      )}
