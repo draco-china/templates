8c41613ac47d49f67802604db319ffebec20d499
Draco <draco.coder@gmail.com>
2025-03-08 16:23:23 +0800

    chore: extend next-template/src/components/ui/drawer.tsx

--- next-template/src/components/ui/drawer.tsx
+++ next-template/src/components/ui/drawer.tsx
@@ -63,3 +63,12 @@ const DrawerHeader = ({
     className={cn("grid gap-1.5 p-4 text-center sm:text-left", className)}
     {...props}
   />
+)
+DrawerHeader.displayName = "DrawerHeader"
+
+const DrawerFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn("mt-auto flex flex-col gap-2 p-4", className)}
