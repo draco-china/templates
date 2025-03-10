9fe788964ca84505afebd7f94d00978bc7ea7fa0
Draco <draco.coder@gmail.com>
2025-03-10 09:43:37 +0800

    chore: extend next-template/src/components/ui/drawer.tsx

--- next-template/src/components/ui/drawer.tsx
+++ next-template/src/components/ui/drawer.tsx
@@ -98,3 +98,12 @@ const DrawerDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Description
     ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+DrawerDescription.displayName = DrawerPrimitive.Description.displayName
+
+export {
+  Drawer,
+  DrawerPortal,
