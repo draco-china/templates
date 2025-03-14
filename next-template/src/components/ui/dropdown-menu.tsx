9ec23daa13aa3a159f1561be1e7a7444486b580b
Draco <draco.coder@gmail.com>
2025-03-14 17:04:25 +0800

    chore: extend next-template/src/components/ui/dropdown-menu.tsx

--- next-template/src/components/ui/dropdown-menu.tsx
+++ next-template/src/components/ui/dropdown-menu.tsx
@@ -141,3 +141,18 @@ const DropdownMenuRadioItem = React.forwardRef<
     {children}
   </DropdownMenuPrimitive.RadioItem>
 ))
+DropdownMenuRadioItem.displayName = DropdownMenuPrimitive.RadioItem.displayName
+
+const DropdownMenuLabel = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Label>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
+    inset?: boolean
+  }
+>(({ className, inset, ...props }, ref) => (
+  <DropdownMenuPrimitive.Label
+    ref={ref}
+    className={cn(
+      "px-2 py-1.5 text-sm font-semibold",
+      inset && "pl-8",
+      className
+    )}
