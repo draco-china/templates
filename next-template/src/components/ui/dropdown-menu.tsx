1342f9efb6d8c20ab4b8b42b9009d36862477b08
Draco <draco.coder@gmail.com>
2025-03-14 17:05:01 +0800

    chore: extend next-template/src/components/ui/dropdown-menu.tsx

--- next-template/src/components/ui/dropdown-menu.tsx
+++ next-template/src/components/ui/dropdown-menu.tsx
@@ -156,3 +156,20 @@ const DropdownMenuLabel = React.forwardRef<
       inset && "pl-8",
       className
     )}
+    {...props}
+  />
+))
+DropdownMenuLabel.displayName = DropdownMenuPrimitive.Label.displayName
+
+const DropdownMenuSeparator = React.forwardRef<
+  React.ElementRef<typeof DropdownMenuPrimitive.Separator>,
+  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>
+>(({ className, ...props }, ref) => (
+  <DropdownMenuPrimitive.Separator
+    ref={ref}
+    className={cn("-mx-1 my-1 h-px bg-muted", className)}
+    {...props}
+  />
+))
+DropdownMenuSeparator.displayName = DropdownMenuPrimitive.Separator.displayName
+
