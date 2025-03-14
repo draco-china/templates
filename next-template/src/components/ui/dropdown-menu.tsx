427f7f088ea62407b3491292616a3f192c6bae50
Draco <draco.coder@gmail.com>
2025-03-14 15:50:30 +0800

    chore: extend next-template/src/components/ui/dropdown-menu.tsx

--- next-template/src/components/ui/dropdown-menu.tsx
+++ next-template/src/components/ui/dropdown-menu.tsx
@@ -127,3 +127,17 @@ const DropdownMenuRadioItem = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <DropdownMenuPrimitive.RadioItem
     ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      className
+    )}
+    {...props}
+  >
+    <span className="absolute left-2 flex size-3.5 items-center justify-center">
+      <DropdownMenuPrimitive.ItemIndicator>
+        <DotFilledIcon className="size-4 fill-current" />
+      </DropdownMenuPrimitive.ItemIndicator>
+    </span>
+    {children}
+  </DropdownMenuPrimitive.RadioItem>
+))
