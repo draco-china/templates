a718fdc42fc3db09a55bebd85edff6179cbbcf1f
Draco <draco.coder@gmail.com>
2025-03-15 14:07:21 +0800

    chore: extend next-template/src/components/ui/dropdown-menu.tsx

--- next-template/src/components/ui/dropdown-menu.tsx
+++ next-template/src/components/ui/dropdown-menu.tsx
@@ -188,3 +188,18 @@ DropdownMenuShortcut.displayName = "DropdownMenuShortcut"
 
 export {
   DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+}
