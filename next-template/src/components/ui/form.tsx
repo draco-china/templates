d57597ab7666b66e56d00776256e37fd949d50c6
Draco <draco.coder@gmail.com>
2025-03-15 15:52:27 +0800

    chore: extend next-template/src/components/ui/form.tsx

--- next-template/src/components/ui/form.tsx
+++ next-template/src/components/ui/form.tsx
@@ -10,3 +10,19 @@ import {
   useFormContext,
 } from "react-hook-form"
 
+import { cn } from "@/lib/utils"
+import { Label } from "@/components/ui/label"
+
+const Form = FormProvider
+
+type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = {
+  name: TName
+}
+
+const FormFieldContext = React.createContext<FormFieldContextValue>(
+  {} as FormFieldContextValue
+)
+
