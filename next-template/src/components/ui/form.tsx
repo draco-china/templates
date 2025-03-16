7718b6e82bd1f620e3f808a1ed842fecf5c14d32
Draco <draco.coder@gmail.com>
2025-03-16 11:31:42 +0800

    chore: extend next-template/src/components/ui/form.tsx

--- next-template/src/components/ui/form.tsx
+++ next-template/src/components/ui/form.tsx
@@ -39,3 +39,18 @@ const FormField = <
   )
 }
 
+const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext)
+  const itemContext = React.useContext(FormItemContext)
+  const { getFieldState, formState } = useFormContext()
+
+  const fieldState = getFieldState(fieldContext.name, formState)
+
+  if (!fieldContext) {
+    throw new Error("useFormField should be used within <FormField>")
+  }
+
+  const { id } = itemContext
+
+  return {
+    id,
