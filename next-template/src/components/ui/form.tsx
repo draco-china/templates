9ae20eabf921fc973c4b7721ce550967cbfbec3a
Draco <draco.coder@gmail.com>
2025-03-17 11:53:48 +0800

    chore: extend next-template/src/components/ui/form.tsx

--- next-template/src/components/ui/form.tsx
+++ next-template/src/components/ui/form.tsx
@@ -68,3 +68,17 @@ type FormItemContextValue = {
 
 const FormItemContext = React.createContext<FormItemContextValue>(
   {} as FormItemContextValue
+)
+
+const FormItem = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  const id = React.useId()
+
+  return (
+    <FormItemContext.Provider value={{ id }}>
+      <div ref={ref} className={cn("space-y-2", className)} {...props} />
+    </FormItemContext.Provider>
+  )
+})
