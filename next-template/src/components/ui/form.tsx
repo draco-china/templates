9dbcef7653c0468a250a4aba432d7c728a4497f2
Draco <draco.coder@gmail.com>
2025-03-19 13:05:16 +0800

    chore: extend next-template/src/components/ui/form.tsx

--- next-template/src/components/ui/form.tsx
+++ next-template/src/components/ui/form.tsx
@@ -95,3 +95,17 @@ const FormLabel = React.forwardRef<
       ref={ref}
       className={cn(error && "text-destructive", className)}
       htmlFor={formItemId}
+      {...props}
+    />
+  )
+})
+FormLabel.displayName = "FormLabel"
+
+const FormControl = React.forwardRef<
+  React.ElementRef<typeof Slot>,
+  React.ComponentPropsWithoutRef<typeof Slot>
+>(({ ...props }, ref) => {
+  const { error, formItemId, formDescriptionId, formMessageId } = useFormField()
+
+  return (
+    <Slot
