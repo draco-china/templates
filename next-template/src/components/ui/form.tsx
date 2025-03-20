119fb1c6957886b9e0aec1d9ccfc38509fda660c
Draco <draco.coder@gmail.com>
2025-03-20 11:00:30 +0800

    chore: extend next-template/src/components/ui/form.tsx

--- next-template/src/components/ui/form.tsx
+++ next-template/src/components/ui/form.tsx
@@ -148,3 +148,16 @@ const FormMessage = React.forwardRef<
   const body = error ? String(error?.message) : children
 
   if (!body) {
+    return null
+  }
+
+  return (
+    <p
+      ref={ref}
+      id={formMessageId}
+      className={cn("text-[0.8rem] font-medium text-destructive", className)}
+      {...props}
+    >
+      {body}
+    </p>
+  )
