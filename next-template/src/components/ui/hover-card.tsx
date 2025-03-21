5f4464a18e37166bbfa234af4c49e448cbf2d25c
Draco <draco.coder@gmail.com>
2025-03-21 12:48:08 +0800

    chore: extend next-template/src/components/ui/hover-card.tsx

--- next-template/src/components/ui/hover-card.tsx
+++ next-template/src/components/ui/hover-card.tsx
@@ -10,3 +10,4 @@ const HoverCard = HoverCardPrimitive.Root
 const HoverCardTrigger = HoverCardPrimitive.Trigger
 
 const HoverCardContent = React.forwardRef<
+  React.ElementRef<typeof HoverCardPrimitive.Content>,
