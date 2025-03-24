d846b7b2c0f58b44cb7053aa5a20412f2fc04c05
Draco <draco.coder@gmail.com>
2025-03-24 18:08:59 +0800

    chore: extend next-template/src/components/ui/input-otp.tsx

--- next-template/src/components/ui/input-otp.tsx
+++ next-template/src/components/ui/input-otp.tsx
@@ -10,3 +10,8 @@ const InputOTP = React.forwardRef<
   React.ElementRef<typeof OTPInput>,
   React.ComponentPropsWithoutRef<typeof OTPInput>
 >(({ className, containerClassName, ...props }, ref) => (
+  <OTPInput
+    ref={ref}
+    containerClassName={cn(
+      "flex items-center gap-2 has-[:disabled]:opacity-50",
+      containerClassName
