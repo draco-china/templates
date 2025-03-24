7d6c38765760c68cad4d215cded567a6d560462c
Draco <draco.coder@gmail.com>
2025-03-24 18:40:32 +0800

    chore: extend next-template/src/components/ui/input-otp.tsx

--- next-template/src/components/ui/input-otp.tsx
+++ next-template/src/components/ui/input-otp.tsx
@@ -15,3 +15,8 @@ const InputOTP = React.forwardRef<
     containerClassName={cn(
       "flex items-center gap-2 has-[:disabled]:opacity-50",
       containerClassName
+    )}
+    className={cn("disabled:cursor-not-allowed", className)}
+    {...props}
+  />
+))
