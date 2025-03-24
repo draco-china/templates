4b8e55ec83106244cc63f59a0323569a14c1eba7
Draco <draco.coder@gmail.com>
2025-03-24 15:36:11 +0800

    chore: extend next-template/src/components/ui/input-otp.tsx

--- next-template/src/components/ui/input-otp.tsx
+++ next-template/src/components/ui/input-otp.tsx
@@ -4,3 +4,9 @@ import * as React from "react"
 import { DashIcon } from "@radix-ui/react-icons"
 import { OTPInput, OTPInputContext } from "input-otp"
 
+import { cn } from "@/lib/utils"
+
+const InputOTP = React.forwardRef<
+  React.ElementRef<typeof OTPInput>,
+  React.ComponentPropsWithoutRef<typeof OTPInput>
+>(({ className, containerClassName, ...props }, ref) => (
