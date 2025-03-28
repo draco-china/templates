af718c745b5b361b7e7af5e78d0dc0ecd2c5807a
Draco <draco.coder@gmail.com>
2025-03-28 18:23:42 +0800

    chore: extend next-template/src/components/ui/input.tsx

--- next-template/src/components/ui/input.tsx
+++ next-template/src/components/ui/input.tsx
@@ -5,3 +5,5 @@ import { cn } from "@/lib/utils"
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
