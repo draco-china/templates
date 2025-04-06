512dc4292fb1629799f707e903315edeaa85fed8
Draco <draco.coder@gmail.com>
2025-04-06 18:44:06 +0800

    chore: extend next-template/src/components/ui/menubar.tsx

--- next-template/src/components/ui/menubar.tsx
+++ next-template/src/components/ui/menubar.tsx
@@ -148,3 +148,21 @@ const MenubarCheckboxItem = React.forwardRef<
       </MenubarPrimitive.ItemIndicator>
     </span>
     {children}
+  </MenubarPrimitive.CheckboxItem>
+))
+MenubarCheckboxItem.displayName = MenubarPrimitive.CheckboxItem.displayName
+
+const MenubarRadioItem = React.forwardRef<
+  React.ElementRef<typeof MenubarPrimitive.RadioItem>,
+  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.RadioItem>
+>(({ className, children, ...props }, ref) => (
+  <MenubarPrimitive.RadioItem
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      className
+    )}
+    {...props}
+  >
+    <span className="absolute left-2 flex size-3.5 items-center justify-center">
+      <MenubarPrimitive.ItemIndicator>
