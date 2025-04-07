32c31a8a05c7730f66c3deffb9912db448058511
Draco <draco.coder@gmail.com>
2025-04-07 11:06:06 +0800

    chore: extend next-template/src/components/ui/menubar.tsx

--- next-template/src/components/ui/menubar.tsx
+++ next-template/src/components/ui/menubar.tsx
@@ -184,3 +184,21 @@ const MenubarLabel = React.forwardRef<
     ref={ref}
     className={cn(
       "px-2 py-1.5 text-sm font-semibold",
+      inset && "pl-8",
+      className
+    )}
+    {...props}
+  />
+))
+MenubarLabel.displayName = MenubarPrimitive.Label.displayName
+
+const MenubarSeparator = React.forwardRef<
+  React.ElementRef<typeof MenubarPrimitive.Separator>,
+  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Separator>
+>(({ className, ...props }, ref) => (
+  <MenubarPrimitive.Separator
+    ref={ref}
+    className={cn("-mx-1 my-1 h-px bg-muted", className)}
+    {...props}
+  />
+))
