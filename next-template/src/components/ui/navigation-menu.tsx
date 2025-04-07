b306c2e61c8274a6461b34215ea9fb15256910f5
Draco <draco.coder@gmail.com>
2025-04-07 20:15:05 +0800

    chore: add next-template/src/components/ui/navigation-menu.tsx

---
+++ next-template/src/components/ui/navigation-menu.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { ChevronDownIcon } from "@radix-ui/react-icons"
+import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu"
+import { cva } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const NavigationMenu = React.forwardRef<
+  React.ElementRef<typeof NavigationMenuPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
+>(({ className, children, ...props }, ref) => (
