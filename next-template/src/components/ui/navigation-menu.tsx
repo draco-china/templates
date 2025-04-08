56ef3e4bf1e2ffc601016f627c32ec7e2667f043
Draco <draco.coder@gmail.com>
2025-04-08 16:47:30 +0800

    chore: extend next-template/src/components/ui/navigation-menu.tsx

--- next-template/src/components/ui/navigation-menu.tsx
+++ next-template/src/components/ui/navigation-menu.tsx
@@ -9,3 +9,12 @@ const NavigationMenu = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Root>
 >(({ className, children, ...props }, ref) => (
+  <NavigationMenuPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative z-10 flex max-w-max flex-1 items-center justify-center",
+      className
+    )}
+    {...props}
+  >
+    {children}
