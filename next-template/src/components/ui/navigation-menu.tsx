a41ef49cef2c4f8faeb2ed08babaf9d24b8ad617
Draco <draco.coder@gmail.com>
2025-04-09 15:08:59 +0800

    chore: extend next-template/src/components/ui/navigation-menu.tsx

--- next-template/src/components/ui/navigation-menu.tsx
+++ next-template/src/components/ui/navigation-menu.tsx
@@ -28,3 +28,13 @@ const NavigationMenuList = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.List>
 >(({ className, ...props }, ref) => (
   <NavigationMenuPrimitive.List
+    ref={ref}
+    className={cn(
+      "group flex flex-1 list-none items-center justify-center space-x-1",
+      className
+    )}
+    {...props}
+  />
+))
+NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
+
