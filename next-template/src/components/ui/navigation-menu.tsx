1d420a343784fc8d0cbc18dad554c6a14ab38cfb
Draco <draco.coder@gmail.com>
2025-04-11 15:38:53 +0800

    chore: extend next-template/src/components/ui/navigation-menu.tsx

--- next-template/src/components/ui/navigation-menu.tsx
+++ next-template/src/components/ui/navigation-menu.tsx
@@ -98,3 +98,12 @@ NavigationMenuViewport.displayName =
   NavigationMenuPrimitive.Viewport.displayName
 
 const NavigationMenuIndicator = React.forwardRef<
+  React.ElementRef<typeof NavigationMenuPrimitive.Indicator>,
+  React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Indicator>
+>(({ className, ...props }, ref) => (
+  <NavigationMenuPrimitive.Indicator
+    ref={ref}
+    className={cn(
+      "top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in",
+      className
+    )}
