b180387afd99a0f2ebb4b4d4c0e65e2444c9697c
Draco <draco.coder@gmail.com>
2025-04-12 11:55:30 +0800

    chore: extend next-template/src/components/ui/pagination.tsx

--- next-template/src/components/ui/pagination.tsx
+++ next-template/src/components/ui/pagination.tsx
@@ -8,3 +8,13 @@ import {
 import { cn } from "@/lib/utils"
 import { ButtonProps, buttonVariants } from "@/components/ui/button"
 
+const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
+  <nav
+    role="navigation"
+    aria-label="pagination"
+    className={cn("mx-auto flex w-full justify-center", className)}
+    {...props}
+  />
+)
+Pagination.displayName = "Pagination"
+
