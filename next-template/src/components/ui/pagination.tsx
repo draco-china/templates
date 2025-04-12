eae7a6741fb004c75b4f71585a666965ff7b35fd
Draco <draco.coder@gmail.com>
2025-04-12 09:24:53 +0800

    chore: add next-template/src/components/ui/pagination.tsx

---
+++ next-template/src/components/ui/pagination.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DotsHorizontalIcon,
+} from "@radix-ui/react-icons"
+
+import { cn } from "@/lib/utils"
+import { ButtonProps, buttonVariants } from "@/components/ui/button"
+
