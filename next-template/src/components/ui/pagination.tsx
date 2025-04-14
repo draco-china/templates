86f86199707845edef08331955f953209bb47e15
Draco <draco.coder@gmail.com>
2025-04-14 20:14:05 +0800

    chore: extend next-template/src/components/ui/pagination.tsx

--- next-template/src/components/ui/pagination.tsx
+++ next-template/src/components/ui/pagination.tsx
@@ -46,3 +46,12 @@ type PaginationLinkProps = {
 const PaginationLink = ({
   className,
   isActive,
+  size = "icon",
+  ...props
+}: PaginationLinkProps) => (
+  <a
+    aria-current={isActive ? "page" : undefined}
+    className={cn(
+      buttonVariants({
+        variant: isActive ? "outline" : "ghost",
+        size,
