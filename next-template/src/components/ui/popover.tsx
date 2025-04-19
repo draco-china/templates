e90a702c568f52abad404faa1454ddadc903f896
Draco <draco.coder@gmail.com>
2025-04-19 19:17:07 +0800

    chore: extend next-template/src/components/ui/popover.tsx

--- next-template/src/components/ui/popover.tsx
+++ next-template/src/components/ui/popover.tsx
@@ -28,3 +28,6 @@ const PopoverContent = React.forwardRef<
     />
   </PopoverPrimitive.Portal>
 ))
+PopoverContent.displayName = PopoverPrimitive.Content.displayName
+
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
