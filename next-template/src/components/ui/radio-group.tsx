85c13f24fb9708fbba694a2adfd752164bdd91d5
Draco <draco.coder@gmail.com>
2025-04-24 19:28:22 +0800

    chore: extend next-template/src/components/ui/radio-group.tsx

--- next-template/src/components/ui/radio-group.tsx
+++ next-template/src/components/ui/radio-group.tsx
@@ -23,3 +23,6 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
