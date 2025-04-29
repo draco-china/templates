a7445c67e5b8866c1f90dc1bf5c315cd018fdaa9
Draco <draco.coder@gmail.com>
2025-04-29 19:09:11 +0800

    chore: extend next-template/src/components/ui/resizable.tsx

--- next-template/src/components/ui/resizable.tsx
+++ next-template/src/components/ui/resizable.tsx
@@ -33,3 +33,6 @@ const ResizableHandle = ({
       className
     )}
     {...props}
+  >
+    {withHandle && (
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
