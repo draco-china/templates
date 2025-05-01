2b840a53833e472ad3b7f2f3af6f90f475d8001e
Draco <draco.coder@gmail.com>
2025-05-01 18:58:21 +0800

    chore: extend next-template/src/components/ui/scroll-area.tsx

--- next-template/src/components/ui/scroll-area.tsx
+++ next-template/src/components/ui/scroll-area.tsx
@@ -7,3 +7,6 @@ import { cn } from "@/lib/utils"
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
+>(({ className, children, ...props }, ref) => (
+  <ScrollAreaPrimitive.Root
