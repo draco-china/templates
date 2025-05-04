536b97621d5af273769e61153a3a695eae859fe2
Draco <draco.coder@gmail.com>
2025-05-04 16:55:44 +0800

    chore: extend next-template/src/components/ui/scroll-area.tsx

--- next-template/src/components/ui/scroll-area.tsx
+++ next-template/src/components/ui/scroll-area.tsx
@@ -28,3 +28,6 @@ const ScrollBar = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
 >(({ className, orientation = "vertical", ...props }, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
+    ref={ref}
+    orientation={orientation}
+    className={cn(
