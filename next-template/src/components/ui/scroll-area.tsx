b43a0df2905d4cecc599f8699494bd19be0a9ada
Draco <draco.coder@gmail.com>
2025-05-05 14:14:11 +0800

    chore: extend next-template/src/components/ui/scroll-area.tsx

--- next-template/src/components/ui/scroll-area.tsx
+++ next-template/src/components/ui/scroll-area.tsx
@@ -41,3 +41,8 @@ const ScrollBar = React.forwardRef<
     {...props}
   >
     <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+  </ScrollAreaPrimitive.ScrollAreaScrollbar>
+))
+ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
+
+export { ScrollArea, ScrollBar }
