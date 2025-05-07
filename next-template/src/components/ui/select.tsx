0d09e34aec734ee29168ffcc109e2965299a3f04
Draco <draco.coder@gmail.com>
2025-05-07 11:37:49 +0800

    chore: extend next-template/src/components/ui/select.tsx

--- next-template/src/components/ui/select.tsx
+++ next-template/src/components/ui/select.tsx
@@ -39,3 +39,15 @@ SelectTrigger.displayName = SelectPrimitive.Trigger.displayName
 
 const SelectScrollUpButton = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.ScrollUpButton>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
+>(({ className, ...props }, ref) => (
+  <SelectPrimitive.ScrollUpButton
+    ref={ref}
+    className={cn(
+      "flex cursor-default items-center justify-center py-1",
+      className
+    )}
+    {...props}
+  >
+    <ChevronUpIcon />
+  </SelectPrimitive.ScrollUpButton>
