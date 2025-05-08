c658881d52364ecdb426685333b23fcfde423eaf
Draco <draco.coder@gmail.com>
2025-05-08 13:21:49 +0800

    chore: extend next-template/src/components/ui/select.tsx

--- next-template/src/components/ui/select.tsx
+++ next-template/src/components/ui/select.tsx
@@ -88,3 +88,14 @@ const SelectContent = React.forwardRef<
       position={position}
       {...props}
     >
+      <SelectScrollUpButton />
+      <SelectPrimitive.Viewport
+        className={cn(
+          "p-1",
+          position === "popper" &&
+            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+        )}
+      >
+        {children}
+      </SelectPrimitive.Viewport>
+      <SelectScrollDownButton />
