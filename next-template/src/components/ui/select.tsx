7bd90105f8e208f9dd758669c19120052b62458a
Draco <draco.coder@gmail.com>
2025-05-09 09:19:21 +0800

    chore: extend next-template/src/components/ui/select.tsx

--- next-template/src/components/ui/select.tsx
+++ next-template/src/components/ui/select.tsx
@@ -125,3 +125,14 @@ const SelectItem = React.forwardRef<
     className={cn(
       "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
+    )}
+    {...props}
+  >
+    <span className="absolute right-2 flex size-3.5 items-center justify-center">
+      <SelectPrimitive.ItemIndicator>
+        <CheckIcon className="size-4" />
+      </SelectPrimitive.ItemIndicator>
+    </span>
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+  </SelectPrimitive.Item>
+))
