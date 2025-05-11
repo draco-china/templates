5f4b306462d42ac07d8b58331d666464fb84b45e
Draco <draco.coder@gmail.com>
2025-05-11 09:54:15 +0800

    chore: extend next-template/src/components/ui/separator.tsx

--- next-template/src/components/ui/separator.tsx
+++ next-template/src/components/ui/separator.tsx
@@ -7,3 +7,5 @@ import { cn } from "@/lib/utils"
 
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
