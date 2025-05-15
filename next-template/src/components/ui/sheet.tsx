296040902bc26fde1c42977169e1a7f67a01f575
Draco <draco.coder@gmail.com>
2025-05-15 12:22:12 +0800

    chore: add next-template/src/components/ui/sheet.tsx

---
+++ next-template/src/components/ui/sheet.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import * as React from "react"
+import * as SheetPrimitive from "@radix-ui/react-dialog"
+import { Cross2Icon } from "@radix-ui/react-icons"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const Sheet = SheetPrimitive.Root
+
+const SheetTrigger = SheetPrimitive.Trigger
+
+const SheetClose = SheetPrimitive.Close
+
