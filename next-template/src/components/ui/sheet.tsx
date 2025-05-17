7521bf9019307f5b7088728e6b87298bf5e643f2
Draco <draco.coder@gmail.com>
2025-05-17 17:41:41 +0800

    chore: extend next-template/src/components/ui/sheet.tsx

--- next-template/src/components/ui/sheet.tsx
+++ next-template/src/components/ui/sheet.tsx
@@ -76,3 +76,12 @@ SheetContent.displayName = SheetPrimitive.Content.displayName
 
 const SheetHeader = ({
   className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col space-y-2 text-center sm:text-left",
+      className
+    )}
+    {...props}
+  />
