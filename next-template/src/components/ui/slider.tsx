f28dd5720e3d63219cd625d77bf7c3c0430ffc7c
Draco <draco.coder@gmail.com>
2025-05-25 19:50:08 +0800

    chore: extend next-template/src/components/ui/slider.tsx

--- next-template/src/components/ui/slider.tsx
+++ next-template/src/components/ui/slider.tsx
@@ -6,3 +6,5 @@ import * as SliderPrimitive from "@radix-ui/react-slider"
 import { cn } from "@/lib/utils"
 
 const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
