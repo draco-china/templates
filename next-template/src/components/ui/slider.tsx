f824c09e7b17947bb77a8c4a6559a6ce5a0e192b
Draco <draco.coder@gmail.com>
2025-05-27 10:18:56 +0800

    chore: extend next-template/src/components/ui/slider.tsx

--- next-template/src/components/ui/slider.tsx
+++ next-template/src/components/ui/slider.tsx
@@ -17,3 +17,5 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
+    <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
+      <SliderPrimitive.Range className="absolute h-full bg-primary" />
