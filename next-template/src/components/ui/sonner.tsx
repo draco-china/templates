1249b0fa5ef01d5d674b1b3cfbd13230f560162c
Draco <draco.coder@gmail.com>
2025-05-29 18:00:30 +0800

    chore: extend next-template/src/components/ui/sonner.tsx

--- next-template/src/components/ui/sonner.tsx
+++ next-template/src/components/ui/sonner.tsx
@@ -5,3 +5,6 @@ import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme()
+
