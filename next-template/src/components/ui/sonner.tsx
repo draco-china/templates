4c6eec14d6562f26328221c25e0d94bbabae6b20
Draco <draco.coder@gmail.com>
2025-05-31 20:57:10 +0800

    chore: extend next-template/src/components/ui/sonner.tsx

--- next-template/src/components/ui/sonner.tsx
+++ next-template/src/components/ui/sonner.tsx
@@ -16,3 +16,5 @@ const Toaster = ({ ...props }: ToasterProps) => {
         classNames: {
           toast:
             "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
