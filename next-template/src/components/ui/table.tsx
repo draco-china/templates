a68951714ec2e015027798159b80bb1e6da28952
Draco <draco.coder@gmail.com>
2025-06-07 20:52:02 +0800

    chore: extend next-template/src/components/ui/table.tsx

--- next-template/src/components/ui/table.tsx
+++ next-template/src/components/ui/table.tsx
@@ -8,3 +8,12 @@ const Table = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
     <table
+      ref={ref}
+      className={cn("w-full caption-bottom text-sm", className)}
+      {...props}
+    />
+  </div>
+))
+Table.displayName = "Table"
+
+const TableHeader = React.forwardRef<
