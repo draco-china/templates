6ba24cb9e4f4a1ef057dc0ab51bb6337aed16bfc
Draco <draco.coder@gmail.com>
2025-06-08 17:46:22 +0800

    chore: extend next-template/src/components/ui/table.tsx

--- next-template/src/components/ui/table.tsx
+++ next-template/src/components/ui/table.tsx
@@ -27,3 +27,12 @@ TableHeader.displayName = "TableHeader"
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => (
+  <tbody
+    ref={ref}
+    className={cn("[&_tr:last-child]:border-0", className)}
+    {...props}
+  />
+))
+TableBody.displayName = "TableBody"
+
