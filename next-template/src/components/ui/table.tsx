c050b5ee47564086dcf8812e64504184a05e74cd
Draco <draco.coder@gmail.com>
2025-06-11 17:05:32 +0800

    chore: extend next-template/src/components/ui/table.tsx

--- next-template/src/components/ui/table.tsx
+++ next-template/src/components/ui/table.tsx
@@ -100,3 +100,12 @@ const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
 >(({ className, ...props }, ref) => (
+  <caption
+    ref={ref}
+    className={cn("mt-4 text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+TableCaption.displayName = "TableCaption"
+
+export {
