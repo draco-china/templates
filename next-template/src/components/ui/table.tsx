35cfe0479b53fde0fa856d88cce9c823de2a544e
Draco <draco.coder@gmail.com>
2025-06-11 13:17:34 +0800

    chore: extend next-template/src/components/ui/table.tsx

--- next-template/src/components/ui/table.tsx
+++ next-template/src/components/ui/table.tsx
@@ -90,3 +90,13 @@ const TableCell = React.forwardRef<
     className={cn(
       "p-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className
+    )}
+    {...props}
+  />
+))
+TableCell.displayName = "TableCell"
+
+const TableCaption = React.forwardRef<
+  HTMLTableCaptionElement,
+  React.HTMLAttributes<HTMLTableCaptionElement>
+>(({ className, ...props }, ref) => (
