b8251039d4b6c5efe7803d71f45a593b6662334d
Draco <draco.coder@gmail.com>
2025-06-13 12:24:27 +0800

    chore: extend next-template/src/components/ui/tabs.tsx

--- next-template/src/components/ui/tabs.tsx
+++ next-template/src/components/ui/tabs.tsx
@@ -25,3 +25,6 @@ TabsList.displayName = TabsPrimitive.List.displayName
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
