4b2df05222d8fdb3dc43b52a29826b88368b0749
Draco <draco.coder@gmail.com>
2025-06-17 10:37:32 +0800

    chore: extend next-template/src/components/ui/textarea.tsx

--- next-template/src/components/ui/textarea.tsx
+++ next-template/src/components/ui/textarea.tsx
@@ -5,3 +5,5 @@ import { cn } from "@/lib/utils"
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
