5d4e9b6c8947c2f9a9bd4ef54126a64386a0b03c
Draco <draco.coder@gmail.com>
2025-06-20 13:46:45 +0800

    chore: extend next-template/src/components/ui/toast.tsx

--- next-template/src/components/ui/toast.tsx
+++ next-template/src/components/ui/toast.tsx
@@ -29,3 +29,12 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
+        default: "border bg-background text-foreground",
+        destructive:
+          "destructive group border-destructive bg-destructive text-destructive-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
