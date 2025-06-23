1a137f96604766af7ef5df9beb289dd622795fe5
Draco <draco.coder@gmail.com>
2025-06-23 20:07:18 +0800

    chore: extend next-template/src/components/ui/toast.tsx

--- next-template/src/components/ui/toast.tsx
+++ next-template/src/components/ui/toast.tsx
@@ -118,3 +118,12 @@ type ToastActionElement = React.ReactElement<typeof ToastAction>
 
 export {
   type ToastProps,
+  type ToastActionElement,
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+}
