624be0ca3cc7ea03788630caf1b50d0809739126
Draco <draco.coder@gmail.com>
2025-06-25 14:16:47 +0800

    chore: extend next-template/src/components/ui/toaster.tsx

--- next-template/src/components/ui/toaster.tsx
+++ next-template/src/components/ui/toaster.tsx
@@ -8,3 +8,8 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
+
+export function Toaster() {
+  const { toasts } = useToast()
+
