08667badb4ae671fc8a22d5e8c6aa493433dac62
Draco <draco.coder@gmail.com>
2025-06-27 18:58:01 +0800

    chore: extend next-template/src/components/ui/toaster.tsx

--- next-template/src/components/ui/toaster.tsx
+++ next-template/src/components/ui/toaster.tsx
@@ -20,3 +20,6 @@ export function Toaster() {
           <Toast key={id} {...props}>
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
