05fda5d4b2776258bbea4c077fc02f1cf68991f3
Draco <draco.coder@gmail.com>
2025-07-07 14:06:55 +0800

    chore: extend next-template/src/components/ui/toggle.tsx

--- next-template/src/components/ui/toggle.tsx
+++ next-template/src/components/ui/toggle.tsx
@@ -29,3 +29,6 @@ const toggleVariants = cva(
 )
 
 const Toggle = React.forwardRef<
+  React.ElementRef<typeof TogglePrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
+    VariantProps<typeof toggleVariants>
