c33ffb1d3f975ff889e250356623141d422d2d0b
Draco <draco.coder@gmail.com>
2025-07-07 15:41:01 +0800

    chore: extend next-template/src/components/ui/toggle.tsx

--- next-template/src/components/ui/toggle.tsx
+++ next-template/src/components/ui/toggle.tsx
@@ -32,3 +32,6 @@ const Toggle = React.forwardRef<
   React.ElementRef<typeof TogglePrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
     VariantProps<typeof toggleVariants>
+>(({ className, variant, size, ...props }, ref) => (
+  <TogglePrimitive.Root
+    ref={ref}
