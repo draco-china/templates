86ecde8fb1e50a20f7b9f418ca6317b83c310704
Draco <draco.coder@gmail.com>
2025-07-12 17:34:17 +0800

    chore: extend next-template/src/components/ui/tooltip.tsx

--- next-template/src/components/ui/tooltip.tsx
+++ next-template/src/components/ui/tooltip.tsx
@@ -25,3 +25,6 @@ const TooltipContent = React.forwardRef<
     {...props}
   />
 ))
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
