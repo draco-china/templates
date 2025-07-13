f8c8d4aa1bf6f098caccf2806ed1c6ffbeaeef36
Draco <draco.coder@gmail.com>
2025-07-13 11:11:30 +0800

    chore: extend next-template/src/components/ui/use-toast.ts

--- next-template/src/components/ui/use-toast.ts
+++ next-template/src/components/ui/use-toast.ts
@@ -15,3 +15,18 @@ type ToasterToast = ToastProps & {
   id: string
   title?: React.ReactNode
   description?: React.ReactNode
+  action?: ToastActionElement
+}
+
+const actionTypes = {
+  ADD_TOAST: "ADD_TOAST",
+  UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+} as const
+
+let count = 0
+
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER
+  return count.toString()
