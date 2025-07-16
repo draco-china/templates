3da91885606dc4e26b65f7eba5695cfba69b12f7
Draco <draco.coder@gmail.com>
2025-07-16 17:03:28 +0800

    chore: extend next-template/src/components/ui/use-toast.ts

--- next-template/src/components/ui/use-toast.ts
+++ next-template/src/components/ui/use-toast.ts
@@ -75,3 +75,16 @@ const addToRemoveQueue = (toastId: string) => {
 }
 
 export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "ADD_TOAST":
+      return {
+        ...state,
+        toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
+      }
+
+    case "UPDATE_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === action.toast.id ? { ...t, ...action.toast } : t
+        ),
