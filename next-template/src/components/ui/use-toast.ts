95d9eaa8f93c30dcca170009c00644c6028de6b9
Draco <draco.coder@gmail.com>
2025-07-16 19:50:48 +0800

    chore: extend next-template/src/components/ui/use-toast.ts

--- next-template/src/components/ui/use-toast.ts
+++ next-template/src/components/ui/use-toast.ts
@@ -88,3 +88,21 @@ export const reducer = (state: State, action: Action): State => {
         toasts: state.toasts.map((t) =>
           t.id === action.toast.id ? { ...t, ...action.toast } : t
         ),
+      }
+
+    case "DISMISS_TOAST": {
+      const { toastId } = action
+
+      // ! Side effects ! - This could be extracted into a dismissToast() action,
+      // but I'll keep it here for simplicity
+      if (toastId) {
+        addToRemoveQueue(toastId)
+      } else {
+        state.toasts.forEach((toast) => {
+          addToRemoveQueue(toast.id)
+        })
+      }
+
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
