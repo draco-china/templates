0f984a71b18e0a8c2dbe2ce10d155de5f5df84f0
Draco <draco.coder@gmail.com>
2025-07-17 19:55:32 +0800

    chore: extend next-template/src/components/ui/use-toast.ts

--- next-template/src/components/ui/use-toast.ts
+++ next-template/src/components/ui/use-toast.ts
@@ -149,3 +149,16 @@ function toast({ ...props }: Toast) {
     dispatch({
       type: "UPDATE_TOAST",
       toast: { ...props, id },
+    })
+  const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id })
+
+  dispatch({
+    type: "ADD_TOAST",
+    toast: {
+      ...props,
+      id,
+      open: true,
+      onOpenChange: (open) => {
+        if (!open) dismiss()
+      },
+    },
