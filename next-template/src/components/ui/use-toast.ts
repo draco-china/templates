de0dda16ff12f351d4b5323a7b59cfcac21c59da
Draco <draco.coder@gmail.com>
2025-07-18 14:15:24 +0800

    chore: extend next-template/src/components/ui/use-toast.ts

--- next-template/src/components/ui/use-toast.ts
+++ next-template/src/components/ui/use-toast.ts
@@ -177,3 +177,18 @@ function useToast() {
   React.useEffect(() => {
     listeners.push(setState)
     return () => {
+      const index = listeners.indexOf(setState)
+      if (index > -1) {
+        listeners.splice(index, 1)
+      }
+    }
+  }, [state])
+
+  return {
+    ...state,
+    toast,
+    dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+  }
+}
+
+export { useToast, toast }
