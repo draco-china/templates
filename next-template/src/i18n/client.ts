b13f3d1a06fddbaec4801db3ee7b3034f8ac153c
Draco <draco.coder@gmail.com>
2025-07-22 09:40:21 +0800

    chore: add next-template/src/i18n/client.ts

---
+++ next-template/src/i18n/client.ts
@@ -0,0 +1,6 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useParams, usePathname, useRouter } from 'next/navigation';
+import i18next from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
