c63724d9bafe3aea28b0399c5af4b097c155d5a3
Draco <draco.coder@gmail.com>
2025-07-23 11:17:30 +0800

    chore: extend next-template/src/i18n/client.ts

--- next-template/src/i18n/client.ts
+++ next-template/src/i18n/client.ts
@@ -4,3 +4,11 @@ import { useEffect, useState } from 'react';
 import { useParams, usePathname, useRouter } from 'next/navigation';
 import i18next from 'i18next';
 import resourcesToBackend from 'i18next-resources-to-backend';
+import { initReactI18next, useTranslation as useTranslationOrg } from 'react-i18next';
+import { isBrowser } from '@/lib/constants';
+import { getCookie, setCookie } from '@/lib/cookies';
+import { cookieName, fallbackLng, getOptions, languages } from './config';
+
+const runsOnServerSide = typeof window === 'undefined';
+
+i18next
