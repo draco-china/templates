26c88271ac7bd2c8d737872fcadb5634576c8efe
Draco <draco.coder@gmail.com>
2025-07-24 18:31:40 +0800

    chore: extend next-template/src/i18n/client.ts

--- next-template/src/i18n/client.ts
+++ next-template/src/i18n/client.ts
@@ -12,3 +12,9 @@ import { cookieName, fallbackLng, getOptions, languages } from './config';
 const runsOnServerSide = typeof window === 'undefined';
 
 i18next
+  .use(initReactI18next)
+  .use(
+    resourcesToBackend(
+      (language: string, namespace: string) => import(`./locales/${language}/${namespace}.json`),
+    ),
+  )
