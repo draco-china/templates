9f65bf81662fdf66562223c6baec7beef76c1ee3
Draco <draco.coder@gmail.com>
2025-07-25 19:24:25 +0800

    chore: extend next-template/src/i18n/client.ts

--- next-template/src/i18n/client.ts
+++ next-template/src/i18n/client.ts
@@ -37,3 +37,11 @@ export function useTranslation(
   const ret = useTranslationOrg(ns, options);
   const { i18n } = ret;
   if (runsOnServerSide && lng && i18n.resolvedLanguage !== lng) {
+    i18n.changeLanguage(lng);
+  } else {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [activeLng, setActiveLng] = useState(i18n.resolvedLanguage);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+      if (activeLng === i18n.resolvedLanguage) return;
+      setActiveLng(i18n.resolvedLanguage);
