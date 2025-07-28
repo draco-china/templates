75c80073b1814e11b21fb2e21ec98aec928a6a53
Draco <draco.coder@gmail.com>
2025-07-28 11:46:28 +0800

    chore: extend next-template/src/i18n/config.ts

--- next-template/src/i18n/config.ts
+++ next-template/src/i18n/config.ts
@@ -4,3 +4,6 @@ import i18nrc from '../../.i18nrc';
 export const fallbackLng = DEFAULT_LANGUAGE || i18nrc.entryLocale;
 
 export const languages = i18nrc.outputLocales;
+export const defaultNS = 'index';
+export const cookieName = 'language';
+
