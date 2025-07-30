e1d68ec7d2d85b70e1afcfab07671c614c549554
Draco <draco.coder@gmail.com>
2025-07-30 09:57:15 +0800

    chore: extend next-template/src/i18n/config.ts

--- next-template/src/i18n/config.ts
+++ next-template/src/i18n/config.ts
@@ -10,3 +10,5 @@ export const cookieName = 'language';
 export function getOptions(lng = fallbackLng, ns: string | string[] = defaultNS) {
   return {
     supportedLngs: languages,
+    fallbackLng,
+    lng,
