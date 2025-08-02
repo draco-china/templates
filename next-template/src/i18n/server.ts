62438aeb94c9fcf8db89365d694a4eaddaa608d5
Draco <draco.coder@gmail.com>
2025-08-02 15:32:29 +0800

    chore: extend next-template/src/i18n/server.ts

--- next-template/src/i18n/server.ts
+++ next-template/src/i18n/server.ts
@@ -1,2 +1,5 @@
 import { createInstance } from 'i18next';
 import resourcesToBackend from 'i18next-resources-to-backend';
+import { initReactI18next } from 'react-i18next/initReactI18next';
+import { getOptions } from './config';
+
