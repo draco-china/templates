2ddd176b9413c87d93fd53ff8903e0d1917e8240
Draco <draco.coder@gmail.com>
2025-08-05 18:04:12 +0800

    chore: extend next-template/src/i18n/server.ts

--- next-template/src/i18n/server.ts
+++ next-template/src/i18n/server.ts
@@ -24,3 +24,6 @@ export async function getTranslation(
   const i18nextInstance = await initI18next(lng, ns);
   return {
     t: i18nextInstance.getFixedT(lng, Array.isArray(ns) ? ns[0] : ns, options?.keyPrefix),
+    i18n: i18nextInstance,
+  };
+}
