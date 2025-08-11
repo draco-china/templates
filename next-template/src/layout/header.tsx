70bde80f7e1f7ee9776fb916cd567d89ec9cdd8b
Draco <draco.coder@gmail.com>
2025-08-11 19:28:30 +0800

    chore: extend next-template/src/layout/header.tsx

--- next-template/src/layout/header.tsx
+++ next-template/src/layout/header.tsx
@@ -2,3 +2,5 @@ import Link from 'next/link';
 import { VercelLogoIcon } from '@radix-ui/react-icons';
 import SwitchLanguage from './switch-language';
 
+export function LayoutHeader({ lng }: { lng: string }) {
+  return (
