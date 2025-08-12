0d435f99f8dd00a9068d25f7418a2b7afc1466dc
Draco <draco.coder@gmail.com>
2025-08-12 09:51:30 +0800

    chore: extend next-template/src/layout/header.tsx

--- next-template/src/layout/header.tsx
+++ next-template/src/layout/header.tsx
@@ -5,3 +5,5 @@ import SwitchLanguage from './switch-language';
 export function LayoutHeader({ lng }: { lng: string }) {
   return (
     <header className='sticky top-0 z-40 h-auto w-full border-b pt-[calc(env(safe-area-inset-top))] backdrop-blur-md'>
+      <div className='container flex h-16 items-center justify-between'>
+        <div className='flex gap-6'>
