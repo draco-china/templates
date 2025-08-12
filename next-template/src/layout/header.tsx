aff7c00b2f23921bb53284a6ec4002f5379289b2
Draco <draco.coder@gmail.com>
2025-08-12 09:57:24 +0800

    chore: extend next-template/src/layout/header.tsx

--- next-template/src/layout/header.tsx
+++ next-template/src/layout/header.tsx
@@ -7,3 +7,5 @@ export function LayoutHeader({ lng }: { lng: string }) {
     <header className='sticky top-0 z-40 h-auto w-full border-b pt-[calc(env(safe-area-inset-top))] backdrop-blur-md'>
       <div className='container flex h-16 items-center justify-between'>
         <div className='flex gap-6'>
+          <Link href={`/${lng}`} className='flex items-center space-x-2'>
+            <VercelLogoIcon className='size-6' />
