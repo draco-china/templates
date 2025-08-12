784d7ad845f2d3a54973f6d8f4e2eb43e2e0c514
Draco <draco.coder@gmail.com>
2025-08-12 09:05:21 +0800

    chore: extend next-template/src/layout/header.tsx

--- next-template/src/layout/header.tsx
+++ next-template/src/layout/header.tsx
@@ -4,3 +4,4 @@ import SwitchLanguage from './switch-language';
 
 export function LayoutHeader({ lng }: { lng: string }) {
   return (
+    <header className='sticky top-0 z-40 h-auto w-full border-b pt-[calc(env(safe-area-inset-top))] backdrop-blur-md'>
