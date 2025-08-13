b0766e1903a3e2f8f4452a85490ffd3c77bd0aad
Draco <draco.coder@gmail.com>
2025-08-13 09:07:00 +0800

    chore: extend next-template/src/layout/header.tsx

--- next-template/src/layout/header.tsx
+++ next-template/src/layout/header.tsx
@@ -9,3 +9,4 @@ export function LayoutHeader({ lng }: { lng: string }) {
         <div className='flex gap-6'>
           <Link href={`/${lng}`} className='flex items-center space-x-2'>
             <VercelLogoIcon className='size-6' />
+            <span className='inline-block font-bold'>Next Template</span>
