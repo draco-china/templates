d4f2ac75f7c38d2acf117225b79521918dff55fa
Draco <draco.coder@gmail.com>
2025-08-18 17:49:48 +0800

    chore: extend next-template/src/layout/switch-language.tsx

--- next-template/src/layout/switch-language.tsx
+++ next-template/src/layout/switch-language.tsx
@@ -1,3 +1,6 @@
 'use client';
 
 import { languages, useLanguage, useTranslation } from '@/i18n';
+import { getCountry } from '@/lib/countries';
+import useMounted from '@/hooks/useMounted';
+import { Select, SelectContent, SelectItem, SelectTrigger } from '@/components/ui/select';
