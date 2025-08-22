8f840962f4292cdf0d2ae6a65c701f9d6f77cfc4
Draco <draco.coder@gmail.com>
2025-08-22 10:54:26 +0800

    chore: extend next-template/src/layout/switch-mode.tsx

--- next-template/src/layout/switch-mode.tsx
+++ next-template/src/layout/switch-mode.tsx
@@ -1,3 +1,6 @@
 'use client';
 
 import { useEffect } from 'react';
+import { checkMode, globalState, toggleMode, type GlobalState } from '@/stores/global';
+import { LaptopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useSnapshot } from 'valtio';
