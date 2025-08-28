130cc4a9744868a6049b2060d22bfd85e7e232c6
Draco <draco.coder@gmail.com>
2025-08-28 20:47:59 +0800

    chore: extend next-template/src/lib/constants.ts

--- next-template/src/lib/constants.ts
+++ next-template/src/lib/constants.ts
@@ -5,3 +5,4 @@ export const DEFAULT_MODE = (process.env.DEFAULT_MODE || 'system') as 'light' | 
 export const DEFAULT_SYSTEM_MODE = DEFAULT_MODE === 'system' ? 'light' : DEFAULT_MODE;
 export const DEFAULT_THEME = process.env.DEFAULT_THEME || 'blue';
 
+export const SITE_URL = process.env.SITE_URL || 'http://localhost:3000';
