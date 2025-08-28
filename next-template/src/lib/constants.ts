ab06098b4dc6f665764dce65a6c4d6ad7b01fdde
Draco <draco.coder@gmail.com>
2025-08-28 10:45:02 +0800

    chore: extend next-template/src/lib/constants.ts

--- next-template/src/lib/constants.ts
+++ next-template/src/lib/constants.ts
@@ -1,3 +1,5 @@
 export const isBrowser = () => typeof window !== 'undefined';
 
 export const DEFAULT_LANGUAGE = process.env.DEFAULT_LANGUAGE;
+export const DEFAULT_MODE = (process.env.DEFAULT_MODE || 'system') as 'light' | 'system' | 'dark';
+export const DEFAULT_SYSTEM_MODE = DEFAULT_MODE === 'system' ? 'light' : DEFAULT_MODE;
