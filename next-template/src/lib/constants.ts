58db4da00977f08e1e913138db068fa32fcef020
Draco <draco.coder@gmail.com>
2025-08-29 19:30:43 +0800

    chore: extend next-template/src/lib/constants.ts

--- next-template/src/lib/constants.ts
+++ next-template/src/lib/constants.ts
@@ -13,3 +13,4 @@ export const EMAIL = process.env.EMAIL;
 export const GITHUB_LINK = process.env.GITHUB_LINK;
 export const TWITTER_LINK = process.env.TWITTER_LINK;
 export const DISCORD_LINK = process.env.DISCORD_LINK;
+export const INSTAGRAM_LINK = process.env.INSTAGRAM_LINK;
