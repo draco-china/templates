5542f4d9f731f46bcf48dff8134d96d457d13a4d
Draco <draco.coder@gmail.com>
2025-08-31 16:03:59 +0800

    chore: extend next-template/src/lib/constants.ts

--- next-template/src/lib/constants.ts
+++ next-template/src/lib/constants.ts
@@ -19,3 +19,5 @@ export const LINKEDIN_LINK = process.env.LINKEDIN_LINK;
 export const GOOGLE_TAG_MANAGER_ID = process.env.GOOGLE_TAG_MANAGER_ID;
 export const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID;
 export const YANDEX_ANALYTICS_ID = process.env.YANDEX_ANALYTICS_ID;
+export const FACEBOOK_ANALYTICS_ID = process.env.FACEBOOK_ANALYTICS_ID;
+export const BAIDU_ANALYTICS_ID = process.env.BAIDU_ANALYTICS_ID;
