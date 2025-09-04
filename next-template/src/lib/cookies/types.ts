4bc5e0608906afb046e201b43d92b0fd4ef90dea
Draco <draco.coder@gmail.com>
2025-09-04 11:47:42 +0800

    chore: extend next-template/src/lib/cookies/types.ts

--- next-template/src/lib/cookies/types.ts
+++ next-template/src/lib/cookies/types.ts
@@ -1,3 +1,5 @@
 import type { IncomingMessage, ServerResponse } from 'http';
 import type { cookies } from 'next/headers';
 import type { NextRequest, NextResponse } from 'next/server';
+import type { CookieSerializeOptions } from 'cookie';
+
