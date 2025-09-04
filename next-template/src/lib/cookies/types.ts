8fe308f5622aef70cc9f584df010f69f00a398a7
Draco <draco.coder@gmail.com>
2025-09-04 16:39:05 +0800

    chore: extend next-template/src/lib/cookies/types.ts

--- next-template/src/lib/cookies/types.ts
+++ next-template/src/lib/cookies/types.ts
@@ -3,3 +3,5 @@ import type { cookies } from 'next/headers';
 import type { NextRequest, NextResponse } from 'next/server';
 import type { CookieSerializeOptions } from 'cookie';
 
+export type OptionsType = DefaultOptions | AppRouterOptions;
+export interface DefaultOptions extends CookieSerializeOptions {
