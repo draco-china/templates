3a68e37f47365a6305eefff788785921842fe064
Draco <draco.coder@gmail.com>
2025-09-11 19:24:36 +0800

    chore: extend next-template/src/lib/request.ts

--- next-template/src/lib/request.ts
+++ next-template/src/lib/request.ts
@@ -1,3 +1,7 @@
 import { getTranslation } from '@/i18n';
 import axios, { type InternalAxiosRequestConfig } from 'axios';
 import i18next from 'i18next';
+import { toast } from 'sonner';
+import { API_URL, SITE_URL } from './constants';
+import { getCookie } from './cookies';
+
