cd7714a69b2f7b58d6f9c8e5145c0336286bb9ff
Draco <draco.coder@gmail.com>
2025-09-14 20:19:35 +0800

    chore: extend next-template/src/lib/request.ts

--- next-template/src/lib/request.ts
+++ next-template/src/lib/request.ts
@@ -37,3 +37,7 @@ request.interceptors.response.use(
     const code = error.response?.data?.code;
     let message;
     if ([401, 403, 404, 500, 503, 504].includes(code)) {
+      message = t(`request.error.${code}`);
+    }
+    if (message) {
+      toast.error(message || error.response?.data?.message || error.response?.statusText);
