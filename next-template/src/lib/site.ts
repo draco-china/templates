5d2300a17b60a7837d56134a4ab427ac5a4a49a7
Draco <draco.coder@gmail.com>
2025-09-16 18:17:11 +0800

    chore: extend next-template/src/lib/site.ts

--- next-template/src/lib/site.ts
+++ next-template/src/lib/site.ts
@@ -17,3 +17,8 @@ export const metadata: Metadata = {
         url: '/favicon-16x16.png',
         sizes: '16x16',
       },
+      {
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+      },
+      {
