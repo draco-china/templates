1ec710854690bd9e4ccf5def39fffe936c3481f3
Draco <draco.coder@gmail.com>
2025-09-20 18:04:45 +0800

    chore: extend next-template/src/providers/react-query.tsx

--- next-template/src/providers/react-query.tsx
+++ next-template/src/providers/react-query.tsx
@@ -1,3 +1,7 @@
 'use client';
 
 import { lazy, ReactNode, Suspense, useEffect, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
