80e0e360fda3b1a6062180b515af1d789295c5b0
Draco <draco.coder@gmail.com>
2025-09-25 15:02:14 +0800

    chore: extend next-template/src/providers/react-query.tsx

--- next-template/src/providers/react-query.tsx
+++ next-template/src/providers/react-query.tsx
@@ -40,3 +40,6 @@ export default function ReactQueryProvider(props: { children: ReactNode }) {
           <ReactQueryDevtoolsProduction />
         </Suspense>
       )}
+    </QueryClientProvider>
+  );
+}
