1333f5cf92cc819709e6cf4b7d3a7ab86708a20d
Draco <draco.coder@gmail.com>
2025-09-25 16:35:46 +0800

    chore: add next-template/src/stores/global.ts

---
+++ next-template/src/stores/global.ts
@@ -0,0 +1,7 @@
+import { proxy } from 'valtio';
+import { DEFAULT_MODE, DEFAULT_SYSTEM_MODE, DEFAULT_THEME, isBrowser } from '@/lib/constants';
+import { getCookie, setCookie } from '@/lib/cookies';
+
+export interface GlobalState {
+  currentMode?: 'light' | 'dark';
+  systemMode?: 'light' | 'dark';
