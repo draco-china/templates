c690418e9d34f0503caa1d87b38169447f85c9d5
Draco <draco.coder@gmail.com>
2025-10-01 12:38:15 +0800

    chore: extend next-template/tailwind.config.ts

--- next-template/tailwind.config.ts
+++ next-template/tailwind.config.ts
@@ -24,3 +24,10 @@ const config = {
       },
       colors: {
         border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
