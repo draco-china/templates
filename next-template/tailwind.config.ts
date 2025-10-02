e035626edc0c4e2409f776e86cc8c76a2fb5097a
Draco <draco.coder@gmail.com>
2025-10-02 18:15:44 +0800

    chore: extend next-template/tailwind.config.ts

--- next-template/tailwind.config.ts
+++ next-template/tailwind.config.ts
@@ -62,3 +62,9 @@ const config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
