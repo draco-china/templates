fa19a9fd5c3f7a3f9a90c5c4880107c85de7a972
Draco <draco.coder@gmail.com>
2025-10-07 20:56:10 +0800

    chore: add shadcn-admin/knip.config.ts

---
+++ shadcn-admin/knip.config.ts
@@ -0,0 +1,5 @@
+import type { KnipConfig } from 'knip';
+
+const config: KnipConfig = {
+  ignore: ['src/components/ui/**', 'src/routeTree.gen.ts', 'i18next.config.ts'],
+  ignoreDependencies: ['tailwindcss', 'tw-animate-css', '@semantic-release/*'],
