284f381a8fae44a89cf28b1b0cb4a74fe7b8accd
Draco <draco.coder@gmail.com>
2025-10-10 13:52:51 +0800

    chore: add shadcn-admin/src/assets/brand-icons/icon-docker.tsx

---
+++ shadcn-admin/src/assets/brand-icons/icon-docker.tsx
@@ -0,0 +1,17 @@
+import type { SVGProps } from 'react';
+import { cn } from '@/lib/utils';
+
+export function IconDocker({ className, ...props }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      className={cn('[&>path]:stroke-current', className)}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
