830ceeb128d70f8d45acc27c3f73f43f2cf449e6
Draco <draco.coder@gmail.com>
2025-10-11 10:20:12 +0800

    chore: add shadcn-admin/src/assets/brand-icons/icon-figma.tsx

---
+++ shadcn-admin/src/assets/brand-icons/icon-figma.tsx
@@ -0,0 +1,14 @@
+import type { SVGProps } from 'react';
+import { cn } from '@/lib/utils';
+
+export function IconFigma({ className, ...props }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      role="img"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      className={cn('[&>path]:stroke-current', className)}
+      fill="none"
+      stroke="currentColor"
