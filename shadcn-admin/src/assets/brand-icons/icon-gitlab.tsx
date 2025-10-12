c88d89d7454b5483d123867aaee70d9c975c5702
Draco <draco.coder@gmail.com>
2025-10-12 19:01:39 +0800

    chore: extend shadcn-admin/src/assets/brand-icons/icon-gitlab.tsx

--- shadcn-admin/src/assets/brand-icons/icon-gitlab.tsx
+++ shadcn-admin/src/assets/brand-icons/icon-gitlab.tsx
@@ -11,3 +11,15 @@ export function IconGitlab({ className, ...props }: SVGProps<SVGSVGElement>) {
       height="24"
       className={cn('[&>path]:stroke-current', className)}
       fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <title>GitLab</title>
+      <path strokeWidth="0" d="M0 0h24v24H0z" fill="none" />
+      <path d="M21 14l-9 7l-9 -7l3 -11l3 7h6l3 -7z" />
+    </svg>
+  );
+}
