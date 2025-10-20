024d2b6c6b71c55b0c8d9785b804f07d2eb5328e
Draco <draco.coder@gmail.com>
2025-10-20 10:41:23 +0800

    chore: add shadcn-admin/src/assets/brand-icons/index.ts

---
+++ shadcn-admin/src/assets/brand-icons/index.ts
@@ -0,0 +1,8 @@
+export { IconDiscord } from './icon-discord';
+export { IconDocker } from './icon-docker';
+export { IconFacebook } from './icon-facebook';
+export { IconFigma } from './icon-figma';
+export { IconGithub } from './icon-github';
+export { IconGitlab } from './icon-gitlab';
+export { IconGmail } from './icon-gmail';
+export { IconMedium } from './icon-medium';
