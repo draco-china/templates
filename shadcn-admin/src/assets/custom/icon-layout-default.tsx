bc44153dadeaf3d575808e3d62d2fa8bb7166810
Draco <draco.coder@gmail.com>
2025-10-21 16:22:50 +0800

    chore: add shadcn-admin/src/assets/custom/icon-layout-default.tsx

---
+++ shadcn-admin/src/assets/custom/icon-layout-default.tsx
@@ -0,0 +1,62 @@
+import type { SVGProps } from 'react';
+
+export function IconLayoutDefault(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="con-layout-default"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 79.86 51.14"
+      {...props}
+    >
+      <path
+        d="M39.22 15.99h-8.16c-.79 0-1.43-.67-1.43-1.5s.64-1.5 1.43-1.5h8.16c.79 0 1.43.67 1.43 1.5s-.64 1.5-1.43 1.5z"
+        opacity={0.75}
+      />
+      <rect
+        x={29.63}
+        y={18.39}
+        width={16.72}
+        height={2.73}
+        rx={1.36}
+        ry={1.36}
+        opacity={0.5}
+      />
+      <path
+        d="M75.1 6.68v1.45c0 .63-.49 1.14-1.09 1.14H30.72c-.6 0-1.09-.51-1.09-1.14V6.68c0-.62.49-1.14 1.09-1.14h43.29c.6 0 1.09.52 1.09 1.14z"
+        opacity={0.9}
+      />
+      <rect
+        x={29.63}
+        y={24.22}
+        width={21.8}
+        height={19.95}
+        rx={2.11}
+        ry={2.11}
+        opacity={0.4}
+      />
+      <g strokeLinecap="round" strokeMiterlimit={10}>
+        <rect
+          x={61.06}
+          y={38.15}
+          width={2.01}
+          height={3.42}
+          rx={0.33}
+          ry={0.33}
+          opacity={0.32}
+        />
+        <rect
+          x={56.78}
+          y={34.99}
+          width={2.01}
+          height={6.58}
+          rx={0.33}
+          ry={0.33}
+          opacity={0.44}
+        />
+        <rect
+          x={65.17}
+          y={32.86}
+          width={2.01}
+          height={8.7}
+          rx={0.33}
+          ry={0.33}
