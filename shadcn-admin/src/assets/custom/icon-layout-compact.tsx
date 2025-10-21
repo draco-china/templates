3dce3edfcf7b07a5be2b0da45c5d97b5e0480ef6
Draco <draco.coder@gmail.com>
2025-10-21 11:40:34 +0800

    chore: add shadcn-admin/src/assets/custom/icon-layout-compact.tsx

---
+++ shadcn-admin/src/assets/custom/icon-layout-compact.tsx
@@ -0,0 +1,66 @@
+import type { SVGProps } from 'react';
+
+export function IconLayoutCompact(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-layout-compact"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 79.86 51.14"
+      {...props}
+    >
+      <rect
+        x={5.84}
+        y={5.2}
+        width={4}
+        height={40}
+        rx={2}
+        ry={2}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <g stroke="#fff" strokeLinecap="round" strokeMiterlimit={10}>
+        <path
+          fill="none"
+          opacity={0.66}
+          strokeWidth="2px"
+          d="M7.26 11.56L8.37 11.56"
+        />
+        <path
+          fill="none"
+          opacity={0.51}
+          strokeWidth="2px"
+          d="M7.26 14.49L8.37 14.49"
+        />
+        <path
+          fill="none"
+          opacity={0.52}
+          strokeWidth="2px"
+          d="M7.26 17.39L8.37 17.39"
+        />
+        <circle cx={7.81} cy={7.25} r={1.16} fill="#fff" opacity={0.8} />
+      </g>
+      <path
+        fill="none"
+        opacity={0.75}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="3px"
+        d="M15.81 14.49L22.89 14.49"
+      />
+      <rect
+        x={14.93}
+        y={18.39}
+        width={22.19}
+        height={2.73}
+        rx={0.64}
+        ry={0.64}
+        opacity={0.5}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <rect
+        x={14.93}
+        y={5.89}
+        width={59.16}
+        height={2.73}
+        rx={0.64}
