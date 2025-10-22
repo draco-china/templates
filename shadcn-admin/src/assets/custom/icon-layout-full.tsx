a118adabed69d9cdfe12f6bb113328d23b2bc926
Draco <draco.coder@gmail.com>
2025-10-22 20:05:04 +0800

    chore: add shadcn-admin/src/assets/custom/icon-layout-full.tsx

---
+++ shadcn-admin/src/assets/custom/icon-layout-full.tsx
@@ -0,0 +1,50 @@
+import type { SVGProps } from 'react';
+
+export function IconLayoutFull(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-layout-full"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 79.86 51.14"
+      {...props}
+    >
+      <path
+        fill="none"
+        opacity={0.75}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="3px"
+        d="M6.85 14.49L15.02 14.49"
+      />
+      <rect
+        x={5.84}
+        y={18.39}
+        width={25.6}
+        height={2.73}
+        rx={0.64}
+        ry={0.64}
+        opacity={0.5}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <rect
+        x={5.84}
+        y={5.89}
+        width={68.26}
+        height={2.73}
+        rx={0.64}
+        ry={0.64}
+        opacity={0.9}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <rect
+        x={5.84}
+        y={24.22}
+        width={37.71}
+        height={19.95}
+        rx={2.11}
+        ry={2.11}
+        opacity={0.4}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
