26b3d5fe5335689fa2ae81c8bb9926e31e741ce2
Draco <draco.coder@gmail.com>
2025-10-23 09:00:29 +0800

    chore: add shadcn-admin/src/assets/custom/icon-sidebar-floating.tsx

---
+++ shadcn-admin/src/assets/custom/icon-sidebar-floating.tsx
@@ -0,0 +1,41 @@
+import type { SVGProps } from 'react';
+
+export function IconSidebarFloating(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-sidebar-floating"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 79.86 51.14"
+      {...props}
+    >
+      <rect
+        x={5.89}
+        y={5.15}
+        width={19.74}
+        height={40}
+        rx={2}
+        ry={2}
+        opacity={0.8}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <g stroke="#fff" strokeLinecap="round" strokeMiterlimit={10}>
+        <path
+          fill="none"
+          opacity={0.72}
+          strokeWidth="2px"
+          d="M9.81 18.36L22.04 18.36"
+        />
+        <path
+          fill="none"
+          opacity={0.48}
+          strokeWidth="2px"
+          d="M9.81 25.57L20.33 25.57"
+        />
+        <path
+          fill="none"
+          opacity={0.55}
+          strokeWidth="2px"
+          d="M9.81 21.85L19.18 21.85"
+        />
+        <circle cx={11.76} cy={10.88} r={2.54} fill="#fff" opacity={0.8} />
