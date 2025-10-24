547c8761a07e06dbd924b0bb5e60862e4e03ea30
Draco <draco.coder@gmail.com>
2025-10-24 11:25:41 +0800

    chore: add shadcn-admin/src/assets/custom/icon-sidebar-inset.tsx

---
+++ shadcn-admin/src/assets/custom/icon-sidebar-inset.tsx
@@ -0,0 +1,29 @@
+import type { SVGProps } from 'react';
+
+export function IconSidebarInset(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-sidebar-inset"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 79.86 51.14"
+      {...props}
+    >
+      <rect
+        x={23.39}
+        y={5.57}
+        width={50.22}
+        height={40}
+        rx={2}
+        ry={2}
+        opacity={0.2}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <path
+        fill="none"
+        opacity={0.72}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="2px"
+        d="M5.08 17.05L17.31 17.05"
+      />
