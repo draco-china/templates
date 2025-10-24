e6b90663d45b7d394de950f64263ac4b6ef0859c
Draco <draco.coder@gmail.com>
2025-10-24 16:44:45 +0800

    chore: add shadcn-admin/src/assets/custom/icon-sidebar-sidebar.tsx

---
+++ shadcn-admin/src/assets/custom/icon-sidebar-sidebar.tsx
@@ -0,0 +1,27 @@
+import type { SVGProps } from 'react';
+
+export function IconSidebarSidebar(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-sidebar-sidebar"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 79.86 51.14"
+      {...props}
+    >
+      <path
+        d="M23.42.51h51.99c2.21 0 4 1.79 4 4v42.18c0 2.21-1.79 4-4 4H23.42s-.04-.02-.04-.04V.55s.02-.04.04-.04z"
+        opacity={0.2}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <path
+        fill="none"
+        opacity={0.72}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="2px"
+        d="M5.56 14.88L17.78 14.88"
+      />
+      <path
+        fill="none"
+        opacity={0.48}
