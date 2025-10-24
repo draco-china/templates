3954bacdbbec1065c02cf34ef6ae19463343fda7
Draco <draco.coder@gmail.com>
2025-10-24 14:27:25 +0800

    chore: extend shadcn-admin/src/assets/custom/icon-sidebar-inset.tsx

--- shadcn-admin/src/assets/custom/icon-sidebar-inset.tsx
+++ shadcn-admin/src/assets/custom/icon-sidebar-inset.tsx
@@ -27,3 +27,32 @@ export function IconSidebarInset(props: SVGProps<SVGSVGElement>) {
         strokeWidth="2px"
         d="M5.08 17.05L17.31 17.05"
       />
+      <path
+        fill="none"
+        opacity={0.48}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="2px"
+        d="M5.08 24.25L15.6 24.25"
+      />
+      <path
+        fill="none"
+        opacity={0.55}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="2px"
+        d="M5.08 20.54L14.46 20.54"
+      />
+      <g strokeLinecap="round" strokeMiterlimit={10}>
+        <circle cx={7.04} cy={9.57} r={2.54} opacity={0.8} />
+        <path
+          fill="none"
+          opacity={0.8}
+          strokeWidth="2px"
+          d="M11.59 8.3L17.31 8.3"
+        />
+        <path fill="none" opacity={0.6} d="M11.38 10.95L16.44 10.95" />
+      </g>
+    </svg>
+  );
+}
