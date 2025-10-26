ec13ad9aa2facfd6ec448056fe7f361374a7a51f
Draco <draco.coder@gmail.com>
2025-10-26 14:52:23 +0800

    chore: add shadcn-admin/src/components/coming-soon.tsx

---
+++ shadcn-admin/src/components/coming-soon.tsx
@@ -0,0 +1,12 @@
+import { Telescope } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
+import { Main } from './layout/main';
+
+export function ComingSoon() {
+  const { t } = useTranslation('components');
+
+  return (
+    <Main fixed>
+      <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
+        <Telescope size={72} />
+        <h1 className="text-4xl leading-tight font-bold">
