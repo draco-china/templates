64fb56ca41e39207e23dff08ce32805c9649950c
Draco <draco.coder@gmail.com>
2026-02-05 18:26:46 +0800

    chore: extend react-template/config/build.ts

--- react-template/config/build.ts
+++ react-template/config/build.ts
@@ -14,3 +14,22 @@ export const rollupOutput = {
     const cleanName = rawName.split("?")[0]?.split("#")[0] ?? rawName;
     const ext = cleanName.split(".").pop()?.toLowerCase() ?? "";
 
+    if (ext === "css") {
+      return "css/[name]-[hash][extname]";
+    }
+
+    if (["png", "jpg", "jpeg", "gif", "webp", "avif", "svg"].includes(ext)) {
+      return "img/[name]-[hash][extname]";
+    }
+
+    if (["woff", "woff2", "ttf", "otf", "eot"].includes(ext)) {
+      return "fonts/[name]-[hash][extname]";
+    }
+
+    if (["mp4", "webm", "ogg", "mp3", "wav", "flac", "aac"].includes(ext)) {
+      return "media/[name]-[hash][extname]";
+    }
+
+    return "other/[name]-[hash][extname]";
+  },
+} as const;
