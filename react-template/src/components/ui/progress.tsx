badbed92b590856d4281f62fed28814f3857d072
Draco <draco.coder@gmail.com>
2026-03-07 11:28:40 +0800

    chore: add react-template/src/components/ui/progress.tsx

---
+++ react-template/src/components/ui/progress.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import * as React from "react"
+import { Progress as ProgressPrimitive } from "radix-ui"
+
+import { cn } from "@/lib/utils"
+
+function Progress({
+  className,
+  value,
+  ...props
+}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+  return (
+    <ProgressPrimitive.Root
+      data-slot="progress"
+      className={cn(
