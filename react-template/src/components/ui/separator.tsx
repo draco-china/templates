c6578d741a41520c7bfcbf6abdb6bcc112f27955
Draco <draco.coder@gmail.com>
2026-03-10 19:59:39 +0800

    chore: add react-template/src/components/ui/separator.tsx

---
+++ react-template/src/components/ui/separator.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Separator as SeparatorPrimitive } from "radix-ui"
+
+import { cn } from "@/lib/utils"
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+  return (
+    <SeparatorPrimitive.Root
