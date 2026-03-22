752e67b49070951d6e3ff8470e68eb7a35bdd50e
Draco <draco.coder@gmail.com>
2026-03-22 19:11:36 +0800

    chore: add react-template/src/lib/api/client.ts

---
+++ react-template/src/lib/api/client.ts
@@ -0,0 +1,10 @@
+import createClient, { type FetchOptions } from "openapi-fetch";
+import type { paths } from "./schema";
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL ?? "";
+
+export const apiClient = createClient<paths>({
+  baseUrl,
+});
+
+export type ApiFetchOptions = FetchOptions<paths>;
