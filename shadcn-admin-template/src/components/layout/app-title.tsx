904d500a11138f055b627e9f094921b31a7efa11
Draco <draco.coder@gmail.com>
2026-04-29 11:33:29 +0800

    chore: add shadcn-admin-template source files

---
+++ shadcn-admin-template/src/components/layout/app-title.tsx
@@ -0,0 +1,64 @@
+import { Menu, X } from "lucide-react";
+import {
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	useSidebar,
+} from "#/components/ui/sidebar";
+import { Link } from "#/hooks/react-router";
+import { cn } from "#/lib/utils";
+import { Button } from "../ui/button";
+
+export function AppTitle() {
+	const { setOpenMobile } = useSidebar();
+	return (
+		<SidebarMenu>
+			<SidebarMenuItem>
+				<SidebarMenuButton
+					asChild
+					className="gap-0 py-0 hover:bg-transparent active:bg-transparent"
+					size="lg"
+				>
+					<div>
+						<Link
+							className="grid flex-1 text-start text-sm leading-tight"
+							onClick={() => setOpenMobile(false)}
+							to="/$locale/"
+						>
+							<span className="truncate font-bold">Shadcn-Admin</span>
+							<span className="truncate text-xs">Vite + ShadcnUI</span>
+						</Link>
+						<ToggleSidebar />
+					</div>
+				</SidebarMenuButton>
+			</SidebarMenuItem>
+		</SidebarMenu>
+	);
+}
+
+function ToggleSidebar({
+	className,
+	onClick,
+	...props
+}: React.ComponentProps<typeof Button>) {
+	const { toggleSidebar } = useSidebar();
+
+	return (
+		<Button
+			className={cn("aspect-square size-8 max-md:scale-125", className)}
+			data-sidebar="trigger"
+			data-slot="sidebar-trigger"
+			onClick={(event) => {
+				onClick?.(event);
+				toggleSidebar();
+			}}
+			size="icon"
+			variant="ghost"
+			{...props}
+		>
+			<X className="md:hidden" />
+			<Menu className="max-md:hidden" />
+			<span className="sr-only">Toggle Sidebar</span>
+		</Button>
+	);
+}
