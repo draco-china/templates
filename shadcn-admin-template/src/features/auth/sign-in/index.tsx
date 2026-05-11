9655aec2ac281ce31fa44cd88f08f1c79979a9a6
Draco <draco.coder@gmail.com>
2026-05-11 14:52:54 +0800

    chore: add shadcn-admin-template/src/features/auth/sign-in/index.tsx

---
+++ shadcn-admin-template/src/features/auth/sign-in/index.tsx
@@ -0,0 +1,51 @@
+import { useSearch } from "@tanstack/react-router";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "#/components/ui/card";
+import { AuthLayout } from "../auth-layout";
+import { UserAuthForm } from "./components/user-auth-form";
+
+export function SignIn() {
+	const { redirect } = useSearch({ from: "/$locale/(auth)/sign-in" });
+
+	return (
+		<AuthLayout>
+			<Card className="gap-4">
+				<CardHeader>
+					<CardTitle className="text-lg tracking-tight">Sign in</CardTitle>
+					<CardDescription>
+						Enter your email and password below to <br />
+						log into your account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<UserAuthForm redirectTo={redirect} />
+				</CardContent>
+				<CardFooter>
+					<p className="px-8 text-center text-muted-foreground text-sm">
+						By clicking sign in, you agree to our{" "}
+						<a
+							className="underline underline-offset-4 hover:text-primary"
+							href="/terms"
+						>
+							Terms of Service
+						</a>{" "}
+						and{" "}
+						<a
+							className="underline underline-offset-4 hover:text-primary"
+							href="/privacy"
+						>
+							Privacy Policy
+						</a>
+						.
+					</p>
+				</CardFooter>
+			</Card>
+		</AuthLayout>
+	);
+}
