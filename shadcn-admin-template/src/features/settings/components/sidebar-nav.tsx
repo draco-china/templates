4b114990cc4a9b9bcb022ed79920367988b424b6
Draco <draco.coder@gmail.com>
2026-05-14 16:19:44 +0800

    chore: add shadcn-admin-template source files

---
+++ shadcn-admin-template/src/features/settings/components/sidebar-nav.tsx
@@ -0,0 +1,87 @@
+import type { JSX } from "react";
+import { buttonVariants } from "#/components/ui/button";
+import { ScrollArea } from "#/components/ui/scroll-area";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "#/components/ui/select";
+import { Link, useMatchRoute, useNavigate } from "#/hooks/react-router";
+import { cn } from "#/lib/utils";
+
+type SidebarNavProps = React.HTMLAttributes<HTMLElement> & {
+	items: {
+		href: string;
+		title: string;
+		icon: JSX.Element;
+	}[];
+};
+
+export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
+	const navigate = useNavigate();
+	const val =
+		useMatchRoute({ to: items.map((item) => item.href) }) ??
+		items[0]?.href ??
+		"";
+
+	const handleSelect = (e: string) => {
+		navigate({ to: e });
+	};
+
+	return (
+		<>
+			<div className="p-1 md:hidden">
+				<Select onValueChange={handleSelect} value={val}>
+					<SelectTrigger className="h-12 sm:w-48">
+						<SelectValue placeholder="Theme" />
+					</SelectTrigger>
+					<SelectContent>
+						{items.map((item) => (
+							<SelectItem key={item.href} value={item.href}>
+								<div className="flex gap-x-4 px-2 py-1">
+									<span className="scale-125">{item.icon}</span>
+									<span className="text-md">{item.title}</span>
+								</div>
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
+			</div>
+
+			<ScrollArea
+				className="hidden w-full min-w-40 bg-background px-1 py-2 md:block"
+				type="always"
+			>
+				<nav
+					className={cn(
+						"flex space-x-2 py-1 lg:flex-col lg:space-x-0 lg:space-y-1",
+						className
+					)}
+					{...props}
+				>
+					{items.map((item) => (
+						<Link
+							activeProps={{
+								className: cn(
+									buttonVariants({ variant: "ghost" }),
+									"justify-start bg-muted hover:bg-accent"
+								),
+							}}
+							className={cn(
+								buttonVariants({ variant: "ghost" }),
+								"justify-start hover:bg-accent hover:underline"
+							)}
+							key={item.href}
+							to={item.href}
+						>
+							<span className="me-2">{item.icon}</span>
+							{item.title}
+						</Link>
+					))}
+				</nav>
+			</ScrollArea>
+		</>
+	);
+}
