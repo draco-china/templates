8a55c17f0cf9f8d215d3ab70c640f64715134b60
Draco <draco.coder@gmail.com>
2026-05-14 11:08:01 +0800

    chore: add shadcn-admin-template source files

---
+++ shadcn-admin-template/src/features/settings/account/index.tsx
@@ -0,0 +1,14 @@
+import { ContentSection } from "../components/content-section";
+import { AccountForm } from "./account-form";
+
+export function SettingsAccount() {
+	return (
+		<ContentSection
+			desc="Update your account settings. Set your preferred language and
+          timezone."
+			title="Account"
+		>
+			<AccountForm />
+		</ContentSection>
+	);
+}
