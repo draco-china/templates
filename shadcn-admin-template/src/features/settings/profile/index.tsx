28ec3c433367abe9b8351feea5990b99907c70ee
Draco <draco.coder@gmail.com>
2026-05-15 18:48:46 +0800

    chore: add shadcn-admin-template/src/features/settings/profile/index.tsx

---
+++ shadcn-admin-template/src/features/settings/profile/index.tsx
@@ -0,0 +1,13 @@
+import { ContentSection } from "../components/content-section";
+import { ProfileForm } from "./profile-form";
+
+export function SettingsProfile() {
+	return (
+		<ContentSection
+			desc="This is how others will see you on the site."
+			title="Profile"
+		>
+			<ProfileForm />
+		</ContentSection>
+	);
+}
