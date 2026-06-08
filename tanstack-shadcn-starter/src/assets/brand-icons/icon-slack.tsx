82aaff630faac52d1c74128ef9a9b4ff3a0d8d11
Draco <draco.coder@gmail.com>
2026-06-08 17:23:47 +0800

    chore: add tanstack-shadcn-starter/src/assets/brand-icons/icon-slack.tsx

---
+++ tanstack-shadcn-starter/src/assets/brand-icons/icon-slack.tsx
@@ -0,0 +1,14 @@
+import type { SVGProps } from "react";
+import { cn } from "#/utils/cn";
+
+export function IconSlack({ className, ...props }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      className={cn("[&>path]:stroke-current", className)}
+      fill="none"
+      height="24"
+      role="img"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
