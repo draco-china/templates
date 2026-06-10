4e0fde51ac4dd600ef0254069d38f4ada14740ba
Draco <draco.coder@gmail.com>
2026-06-10 19:33:37 +0800

    chore: extend tanstack-shadcn-starter/src/assets/brand-icons/icon-telegram.tsx

--- tanstack-shadcn-starter/src/assets/brand-icons/icon-telegram.tsx
+++ tanstack-shadcn-starter/src/assets/brand-icons/icon-telegram.tsx
@@ -11,3 +11,15 @@ export function IconTelegram({ className, ...props }: SVGProps<SVGSVGElement>) {
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>Telegram</title>
+      <path d="M0 0h24v24H0z" fill="none" strokeWidth="0" />
+      <path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
+    </svg>
+  );
+}
