fc10ad66136afcb06911bac979dc712ed0bb4ac7
Draco <draco.coder@gmail.com>
2026-06-13 10:17:21 +0800

    chore: add tanstack-shadcn-starter/src/assets/custom/icon-layout-compact.tsx

---
+++ tanstack-shadcn-starter/src/assets/custom/icon-layout-compact.tsx
@@ -0,0 +1,66 @@
+import type { SVGProps } from "react";
+
+export function IconLayoutCompact(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-layout-compact"
+      viewBox="0 0 79.86 51.14"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>Compact Layout</title>
+      <rect
+        height={40}
+        rx={2}
+        ry={2}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        width={4}
+        x={5.84}
+        y={5.2}
+      />
+      <g stroke="#fff" strokeLinecap="round" strokeMiterlimit={10}>
+        <path
+          d="M7.26 11.56L8.37 11.56"
+          fill="none"
+          opacity={0.66}
+          strokeWidth="2px"
+        />
+        <path
+          d="M7.26 14.49L8.37 14.49"
+          fill="none"
+          opacity={0.51}
+          strokeWidth="2px"
+        />
+        <path
+          d="M7.26 17.39L8.37 17.39"
+          fill="none"
+          opacity={0.52}
+          strokeWidth="2px"
+        />
+        <circle cx={7.81} cy={7.25} fill="#fff" opacity={0.8} r={1.16} />
+      </g>
+      <path
+        d="M15.81 14.49L22.89 14.49"
+        fill="none"
+        opacity={0.75}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="3px"
+      />
+      <rect
+        height={2.73}
+        opacity={0.5}
+        rx={0.64}
+        ry={0.64}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        width={22.19}
+        x={14.93}
+        y={18.39}
+      />
+      <rect
+        height={2.73}
+        opacity={0.9}
+        rx={0.64}
+        ry={0.64}
