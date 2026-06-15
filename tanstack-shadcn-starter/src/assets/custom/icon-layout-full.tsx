d27b180ab2b024e47c9976f83e23620549f5b213
Draco <draco.coder@gmail.com>
2026-06-15 14:55:47 +0800

    chore: add tanstack-shadcn-starter/src/assets/custom/icon-layout-full.tsx

---
+++ tanstack-shadcn-starter/src/assets/custom/icon-layout-full.tsx
@@ -0,0 +1,51 @@
+import type { SVGProps } from "react";
+
+export function IconLayoutFull(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-layout-full"
+      viewBox="0 0 79.86 51.14"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>Full Layout</title>
+      <path
+        d="M6.85 14.49L15.02 14.49"
+        fill="none"
+        opacity={0.75}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="3px"
+      />
+      <rect
+        height={2.73}
+        opacity={0.5}
+        rx={0.64}
+        ry={0.64}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        width={25.6}
+        x={5.84}
+        y={18.39}
+      />
+      <rect
+        height={2.73}
+        opacity={0.9}
+        rx={0.64}
+        ry={0.64}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        width={68.26}
+        x={5.84}
+        y={5.89}
+      />
+      <rect
+        height={19.95}
+        opacity={0.4}
+        rx={2.11}
+        ry={2.11}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        width={37.71}
+        x={5.84}
+        y={24.22}
