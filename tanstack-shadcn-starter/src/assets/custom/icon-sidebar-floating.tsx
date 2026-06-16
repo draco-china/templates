4daa6ae1ea728ebf10dd9dd4f8e4c074c69e98ff
Draco <draco.coder@gmail.com>
2026-06-16 20:24:48 +0800

    chore: add tanstack-shadcn-starter/src/assets/custom/icon-sidebar-floating.tsx

---
+++ tanstack-shadcn-starter/src/assets/custom/icon-sidebar-floating.tsx
@@ -0,0 +1,42 @@
+import type { SVGProps } from "react";
+
+export function IconSidebarFloating(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-sidebar-floating"
+      viewBox="0 0 79.86 51.14"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>Floating Sidebar</title>
+      <rect
+        height={40}
+        opacity={0.8}
+        rx={2}
+        ry={2}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        width={19.74}
+        x={5.89}
+        y={5.15}
+      />
+      <g stroke="#fff" strokeLinecap="round" strokeMiterlimit={10}>
+        <path
+          d="M9.81 18.36L22.04 18.36"
+          fill="none"
+          opacity={0.72}
+          strokeWidth="2px"
+        />
+        <path
+          d="M9.81 25.57L20.33 25.57"
+          fill="none"
+          opacity={0.48}
+          strokeWidth="2px"
+        />
+        <path
+          d="M9.81 21.85L19.18 21.85"
+          fill="none"
+          opacity={0.55}
+          strokeWidth="2px"
+        />
+        <circle cx={11.76} cy={10.88} fill="#fff" opacity={0.8} r={2.54} />
