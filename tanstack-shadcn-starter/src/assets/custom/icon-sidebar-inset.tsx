4ff9cb13878dd76f88626bd4e4dcbe499092f458
Draco <draco.coder@gmail.com>
2026-06-18 11:24:02 +0800

    chore: add tanstack-shadcn-starter/src/assets/custom/icon-sidebar-inset.tsx

---
+++ tanstack-shadcn-starter/src/assets/custom/icon-sidebar-inset.tsx
@@ -0,0 +1,30 @@
+import type { SVGProps } from "react";
+
+export function IconSidebarInset(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      data-name="icon-sidebar-inset"
+      viewBox="0 0 79.86 51.14"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>Inset Sidebar</title>
+      <rect
+        height={40}
+        opacity={0.2}
+        rx={2}
+        ry={2}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        width={50.22}
+        x={23.39}
+        y={5.57}
+      />
+      <path
+        d="M5.08 17.05L17.31 17.05"
+        fill="none"
+        opacity={0.72}
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+        strokeWidth="2px"
+      />
