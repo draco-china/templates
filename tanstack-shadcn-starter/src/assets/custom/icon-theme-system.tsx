4c01097988c96558d4719d89017fd305fce8add4
Draco <draco.coder@gmail.com>
2026-06-20 20:56:05 +0800

    chore: add tanstack-shadcn-starter/src/assets/custom/icon-theme-system.tsx

---
+++ tanstack-shadcn-starter/src/assets/custom/icon-theme-system.tsx
@@ -0,0 +1,59 @@
+import type { SVGProps } from "react";
+import { cn } from "#/utils/cn";
+
+export function IconThemeSystem({
+  className,
+  ...props
+}: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      aria-label="System theme icon"
+      className={cn(
+        "overflow-hidden rounded-[6px]",
+        "fill-primary stroke-primary group-data-[state=unchecked]:fill-muted-foreground group-data-[state=unchecked]:stroke-muted-foreground",
+        className
+      )}
+      data-name="icon-theme-system"
+      viewBox="0 0 79.86 51.14"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>System Theme</title>
+      <path d="M0 0.03H22.88V51.17H0z" opacity={0.2} />
+      <circle
+        cx={6.7}
+        cy={7.04}
+        fill="#fff"
+        opacity={0.8}
+        r={3.54}
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeMiterlimit={10}
+      />
+      <path
+        d="M18.12 6.39h-5.87c-.6 0-1.09-.45-1.09-1s.49-1 1.09-1h5.87c.6 0 1.09.45 1.09 1s-.49 1-1.09 1zM16.55 9.77h-4.24c-.55 0-1-.45-1-1s.45-1 1-1h4.24c.55 0 1 .45 1 1s-.45 1-1 1z"
+        fill="#fff"
+        opacity={0.75}
+        stroke="none"
+      />
+      <path
+        d="M18.32 17.37H4.59c-.69 0-1.25-.47-1.25-1.05s.56-1.05 1.25-1.05h13.73c.69 0 1.25.47 1.25 1.05s-.56 1.05-1.25 1.05z"
+        fill="#fff"
+        opacity={0.72}
+        stroke="none"
+      />
+      <path
+        d="M15.34 21.26h-11c-.55 0-1-.41-1-.91s.45-.91 1-.91h11c.55 0 1 .41 1 .91s-.45.91-1 .91z"
+        fill="#fff"
+        opacity={0.55}
+        stroke="none"
+      />
+      <path
+        d="M16.46 25.57H4.43c-.6 0-1.09-.44-1.09-.98s.49-.98 1.09-.98h12.03c.6 0 1.09.44 1.09.98s-.49.98-1.09.98z"
+        fill="#fff"
+        opacity={0.67}
+        stroke="none"
+      />
+      <rect
+        height={3.42}
+        opacity={0.31}
