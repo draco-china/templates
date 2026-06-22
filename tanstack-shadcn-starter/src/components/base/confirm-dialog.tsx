b366dd65de731a9135852f77ec5f65f5a9bb2488
Draco <draco.coder@gmail.com>
2026-06-22 14:29:00 +0800

    chore: add tanstack-shadcn-starter/src/components/base/confirm-dialog.tsx

---
+++ tanstack-shadcn-starter/src/components/base/confirm-dialog.tsx
@@ -0,0 +1,37 @@
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "#/components/ui/alert-dialog";
+import { Button } from "#/components/ui/button";
+import { cn } from "#/utils/cn";
+
+type ConfirmDialogProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  title: React.ReactNode;
+  disabled?: boolean;
+  desc: React.JSX.Element | string;
+  cancelBtnText?: string;
+  confirmText?: React.ReactNode;
+  destructive?: boolean;
+  isLoading?: boolean;
+  className?: string;
+  children?: React.ReactNode;
+} & (
+  | { form: string; handleConfirm?: undefined }
+  | { form?: undefined; handleConfirm: () => void }
+);
+
+export function ConfirmDialog(props: ConfirmDialogProps) {
+  const {
+    title,
+    desc,
+    children,
+    className,
+    confirmText,
+    cancelBtnText,
