bb3db87c01dcd479aa5ef6a1fd86deff92964b97
Draco <draco.coder@gmail.com>
2026-06-23 16:16:11 +0800

    chore: add tanstack-shadcn-starter/src/components/base/data-table/column-header.tsx

---
+++ tanstack-shadcn-starter/src/components/base/data-table/column-header.tsx
@@ -0,0 +1,37 @@
+import type { Column } from "@tanstack/react-table";
+import { ArrowDown, ArrowUp, ChevronsUpDown, EyeOff } from "lucide-react";
+import { Button } from "#/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "#/components/ui/dropdown-menu";
+import { cn } from "#/utils/cn";
+
+type DataTableColumnHeaderProps<TData, TValue> =
+  React.HTMLAttributes<HTMLDivElement> & {
+    column: Column<TData, TValue>;
+    title: string;
+  };
+
+export function DataTableColumnHeader<TData, TValue>({
+  column,
+  title,
+  className,
+}: DataTableColumnHeaderProps<TData, TValue>) {
+  if (!column.getCanSort()) {
+    return <div className={cn(className)}>{title}</div>;
+  }
+
+  return (
+    <div className={cn("flex items-center space-x-2", className)}>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            className="h-8 data-[state=open]:bg-accent"
+            size="sm"
+            variant="ghost"
+          >
+            <span>{title}</span>
