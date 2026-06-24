54ee2b9afbb930b39f45aca7d80483c65d44fe8e
Draco <draco.coder@gmail.com>
2026-06-24 09:02:19 +0800

    chore: add tanstack-shadcn-starter/src/components/base/data-table/faceted-filter.tsx

---
+++ tanstack-shadcn-starter/src/components/base/data-table/faceted-filter.tsx
@@ -0,0 +1,74 @@
+import type { Column } from "@tanstack/react-table";
+import { Check, PlusCircle } from "lucide-react";
+import type * as React from "react";
+import { Badge } from "#/components/ui/badge";
+import { Button } from "#/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "#/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "#/components/ui/popover";
+import { Separator } from "#/components/ui/separator";
+import { cn } from "#/utils/cn";
+
+interface DataTableFacetedFilterProps<TData, TValue> {
+  column?: Column<TData, TValue>;
+  options: {
+    label: string;
+    value: string;
+    icon?: React.ComponentType<{ className?: string }>;
+  }[];
+  title?: string;
+}
+
+export function DataTableFacetedFilter<TData, TValue>({
+  column,
+  title,
+  options,
+}: DataTableFacetedFilterProps<TData, TValue>) {
+  const facets = column?.getFacetedUniqueValues();
+  const selectedValues = new Set(column?.getFilterValue() as string[]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-8 border-dashed" size="sm" variant="outline">
+          <PlusCircle className="size-4" />
+          {title}
+          {selectedValues?.size > 0 && (
+            <>
+              <Separator className="mx-2 h-4" orientation="vertical" />
+              <Badge
+                className="rounded-sm px-1 font-normal lg:hidden"
+                variant="secondary"
+              >
+                {selectedValues.size}
+              </Badge>
+              <div className="hidden space-x-1 lg:flex">
+                {selectedValues.size > 2 ? (
+                  <Badge
+                    className="rounded-sm px-1 font-normal"
+                    variant="secondary"
+                  >
+                    {selectedValues.size} selected
+                  </Badge>
+                ) : (
+                  options
+                    .filter((option) => selectedValues.has(option.value))
+                    .map((option) => (
+                      <Badge
+                        className="rounded-sm px-1 font-normal"
+                        key={option.value}
+                        variant="secondary"
+                      >
+                        {option.label}
+                      </Badge>
