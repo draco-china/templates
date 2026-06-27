ad0e4dee77543cd2510801c0e265141256203ca7
Draco <draco.coder@gmail.com>
2026-06-27 12:46:45 +0800

    chore: extend tanstack-shadcn-starter/src/components/base/data-table/view-options.tsx

--- tanstack-shadcn-starter/src/components/base/data-table/view-options.tsx
+++ tanstack-shadcn-starter/src/components/base/data-table/view-options.tsx
@@ -26,3 +26,29 @@ export function DataTableViewOptions<TData>({
           variant="outline"
         >
           <SlidersHorizontal className="size-4" />
+          View
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[150px]">
+        <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {table
+          .getAllColumns()
+          .filter(
+            (column) =>
+              typeof column.accessorFn !== "undefined" && column.getCanHide()
+          )
+          .map((column) => (
+            <DropdownMenuCheckboxItem
+              checked={column.getIsVisible()}
+              className="capitalize"
+              key={column.id}
+              onCheckedChange={(value) => column.toggleVisibility(!!value)}
+            >
+              {column.id}
+            </DropdownMenuCheckboxItem>
+          ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
