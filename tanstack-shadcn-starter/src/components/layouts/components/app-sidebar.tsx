6449a4adfa49ca1a62935749c083b789da237bdc
Draco <draco.coder@gmail.com>
2026-07-03 19:23:31 +0800

    chore: extend tanstack-shadcn-starter/src/components/layouts/components/app-sidebar.tsx

--- tanstack-shadcn-starter/src/components/layouts/components/app-sidebar.tsx
+++ tanstack-shadcn-starter/src/components/layouts/components/app-sidebar.tsx
@@ -16,3 +16,22 @@ export function AppSidebar() {
   const { collapsible, variant } = useLayout();
   return (
     <Sidebar collapsible={collapsible} variant={variant}>
+      <SidebarHeader>
+        <TeamSwitcher teams={sidebarData.teams} />
+
+        {/* Replace <TeamSwitch /> with the following <AppTitle />
+         /* if you want to use the normal app title instead of TeamSwitch dropdown */}
+        {/* <AppTitle /> */}
+      </SidebarHeader>
+      <SidebarContent>
+        {sidebarData.navGroups.map((props) => (
+          <NavGroup key={props.title} {...props} />
+        ))}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={sidebarData.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
