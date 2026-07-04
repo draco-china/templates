481fd72ecf3e19fc587a3e50c526ef94785e4474
Draco <draco.coder@gmail.com>
2026-07-04 16:32:53 +0800

    chore: extend tanstack-shadcn-starter/src/components/layouts/components/header.tsx

--- tanstack-shadcn-starter/src/components/layouts/components/header.tsx
+++ tanstack-shadcn-starter/src/components/layouts/components/header.tsx
@@ -36,3 +36,32 @@ export function Header({ className, fixed, topNav, ...props }: HeaderProps) {
     return () => document.removeEventListener("scroll", onScroll);
   }, []);
 
+  return (
+    <header
+      className={cn(
+        "z-50 h-16",
+        fixed && "header-fixed peer/header sticky top-0 w-[inherit]",
+        offset > 10 && fixed ? "shadow" : "shadow-none",
+        className
+      )}
+      {...props}
+    >
+      <div
+        className={cn(
+          "relative flex h-full items-center gap-3 p-4 sm:gap-4",
+          offset > 10 &&
+            fixed &&
+            "after:absolute after:inset-0 after:-z-10 after:bg-background/20 after:backdrop-blur-lg"
+        )}
+      >
+        <SidebarTrigger className="max-md:scale-125" variant="outline" />
+        <Separator className="h-6" orientation="vertical" />
+        {topNav ? <TopNav className="me-auto" links={topNav} /> : null}
+        <Search className={topNav ? undefined : "me-auto"} />
+        <ThemeSwitch />
+        <ConfigDrawer />
+        <ProfileDropdown />
+      </div>
+    </header>
+  );
+}
