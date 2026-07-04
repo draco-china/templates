f8034b400afcbfe3efde1f455c37ee474e507f1b
Draco <draco.coder@gmail.com>
2026-07-04 13:01:09 +0800

    chore: extend tanstack-shadcn-starter/src/components/layouts/components/data/sidebar-data.ts

--- tanstack-shadcn-starter/src/components/layouts/components/data/sidebar-data.ts
+++ tanstack-shadcn-starter/src/components/layouts/components/data/sidebar-data.ts
@@ -91,3 +91,96 @@ export const sidebarData: SidebarData = {
               url: "/sign-in",
             },
             {
+              title: "Sign In (2 Col)",
+              url: "/sign-in-2",
+            },
+            {
+              title: "Sign Up",
+              url: "/sign-up",
+            },
+            {
+              title: "Forgot Password",
+              url: "/forgot-password",
+            },
+            {
+              title: "OTP",
+              url: "/otp",
+            },
+          ],
+        },
+        {
+          title: "Errors",
+          icon: Bug,
+          items: [
+            {
+              title: "Unauthorized",
+              url: "/errors/unauthorized",
+              icon: Lock,
+            },
+            {
+              title: "Forbidden",
+              url: "/errors/forbidden",
+              icon: UserX,
+            },
+            {
+              title: "Not Found",
+              url: "/errors/not-found",
+              icon: FileX,
+            },
+            {
+              title: "Internal Server Error",
+              url: "/errors/internal-server-error",
+              icon: ServerOff,
+            },
+            {
+              title: "Maintenance Error",
+              url: "/errors/maintenance-error",
+              icon: Construction,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Other",
+      items: [
+        {
+          title: "Settings",
+          icon: Settings,
+          items: [
+            {
+              title: "Profile",
+              url: "/settings",
+              icon: UserCog,
+            },
+            {
+              title: "Account",
+              url: "/settings/account",
+              icon: Wrench,
+            },
+            {
+              title: "Appearance",
+              url: "/settings/appearance",
+              icon: Palette,
+            },
+            {
+              title: "Notifications",
+              url: "/settings/notifications",
+              icon: Bell,
+            },
+            {
+              title: "Display",
+              url: "/settings/display",
+              icon: Monitor,
+            },
+          ],
+        },
+        {
+          title: "Help Center",
+          url: "/help-center",
+          icon: HelpCircle,
+        },
+      ],
+    },
+  ],
+};
