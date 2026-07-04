b0cf88281c9b8a4d8aebe9e89037825fee04c99d
Draco <draco.coder@gmail.com>
2026-07-04 12:34:15 +0800

    chore: add tanstack-shadcn-starter/src/components/layouts/components/data/sidebar-data.ts

---
+++ tanstack-shadcn-starter/src/components/layouts/components/data/sidebar-data.ts
@@ -0,0 +1,93 @@
+import {
+  AudioWaveform,
+  Bell,
+  Bug,
+  Command,
+  Construction,
+  FileX,
+  GalleryVerticalEnd,
+  HelpCircle,
+  LayoutDashboard,
+  ListTodo,
+  Lock,
+  MessagesSquare,
+  Monitor,
+  Package,
+  Palette,
+  ServerOff,
+  Settings,
+  ShieldCheck,
+  UserCog,
+  Users,
+  UserX,
+  Wrench,
+} from "lucide-react";
+import type { SidebarData } from "../types";
+
+export const sidebarData: SidebarData = {
+  user: {
+    name: "Admin",
+    email: "admin@example.com",
+    avatar: "/avatars/avatar.jpg",
+  },
+  teams: [
+    {
+      name: "TanStack Shadcn Starter",
+      logo: Command,
+      plan: "Vite + ShadcnUI",
+    },
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+  ],
+  navGroups: [
+    {
+      title: "General",
+      items: [
+        {
+          title: "Dashboard",
+          url: "/",
+          icon: LayoutDashboard,
+        },
+        {
+          title: "Tasks",
+          url: "/tasks",
+          icon: ListTodo,
+        },
+        {
+          title: "Apps",
+          url: "/apps",
+          icon: Package,
+        },
+        {
+          title: "Chats",
+          url: "/chats",
+          badge: "3",
+          icon: MessagesSquare,
+        },
+        {
+          title: "Users",
+          url: "/users",
+          icon: Users,
+        },
+      ],
+    },
+    {
+      title: "Pages",
+      items: [
+        {
+          title: "Auth",
+          icon: ShieldCheck,
+          items: [
+            {
+              title: "Sign In",
+              url: "/sign-in",
+            },
+            {
