bafc6666f3b5338224ad8b1a206a61f5ecd2839b
Draco <draco.coder@gmail.com>
2026-07-06 20:34:25 +0800

    chore: add tanstack-shadcn-starter/src/components/layouts/components/team-switcher.tsx

---
+++ tanstack-shadcn-starter/src/components/layouts/components/team-switcher.tsx
@@ -0,0 +1,44 @@
+import { ChevronsUpDown, Plus } from "lucide-react";
+import { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "#/components/ui/dropdown-menu";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "#/components/ui/sidebar";
+
+interface TeamSwitcherProps {
+  teams: {
+    name: string;
+    logo: React.ElementType;
+    plan: string;
+  }[];
+}
+
+export function TeamSwitcher({ teams }: TeamSwitcherProps) {
+  const { isMobile } = useSidebar();
+  const [activeTeam, setActiveTeam] = useState(teams[0]);
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              size="lg"
+            >
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <activeTeam.logo className="size-4" />
+              </div>
+              <div className="grid flex-1 text-start text-sm leading-tight">
+                <span className="truncate font-semibold">
