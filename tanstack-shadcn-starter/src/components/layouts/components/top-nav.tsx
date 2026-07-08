b23afbe81139e85f90b9307f980e2d3252ecd2a3
Draco <draco.coder@gmail.com>
2026-07-08 19:07:04 +0800

    chore: extend tanstack-shadcn-starter/src/components/layouts/components/top-nav.tsx

--- tanstack-shadcn-starter/src/components/layouts/components/top-nav.tsx
+++ tanstack-shadcn-starter/src/components/layouts/components/top-nav.tsx
@@ -33,3 +33,38 @@ export function TopNav({ className, links, ...props }: TopNavProps) {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="start" side="bottom">
+          {links.map(({ title, href, isActive, disabled }) => (
+            <DropdownMenuItem asChild key={`${title}-${href}`}>
+              <Link
+                className={isActive ? "" : "text-muted-foreground"}
+                disabled={disabled}
+                to={href}
+              >
+                {title}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <nav
+        className={cn(
+          "hidden items-center space-x-4 lg:flex lg:space-x-4 xl:space-x-6",
+          className
+        )}
+        {...props}
+      >
+        {links.map(({ title, href, isActive, disabled }) => (
+          <Link
+            className={`font-medium text-sm transition-colors hover:text-primary ${isActive ? "" : "text-muted-foreground"}`}
+            disabled={disabled}
+            key={`${title}-${href}`}
+            to={href}
+          >
+            {title}
+          </Link>
+        ))}
+      </nav>
+    </>
+  );
+}
