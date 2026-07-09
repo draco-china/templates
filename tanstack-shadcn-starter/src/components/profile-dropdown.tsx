2702bc48a8ba01322eb36d415ab8c0178e04c90e
Draco <draco.coder@gmail.com>
2026-07-09 16:06:01 +0800

    chore: add tanstack-shadcn-starter/src/components/profile-dropdown.tsx

---
+++ tanstack-shadcn-starter/src/components/profile-dropdown.tsx
@@ -0,0 +1,38 @@
+import { Link } from "@tanstack/react-router";
+import { SignOutDialog } from "#/components/sign-out-dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "#/components/ui/avatar";
+import { Button } from "#/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "#/components/ui/dropdown-menu";
+import useDialogState from "#/hooks/use-dialog-state";
+
+export function ProfileDropdown() {
+  const [open, setOpen] = useDialogState();
+
+  return (
+    <>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button className="relative h-8 w-8 rounded-full" variant="ghost">
+            <Avatar className="h-8 w-8">
+              <AvatarImage alt="@shadcn" src="/avatars/01.png" />
+              <AvatarFallback>SN</AvatarFallback>
+            </Avatar>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col gap-1.5">
+              <p className="font-medium text-sm leading-none">Admin</p>
+              <p className="text-muted-foreground text-xs leading-none">
+                admin@example.com
+              </p>
+            </div>
