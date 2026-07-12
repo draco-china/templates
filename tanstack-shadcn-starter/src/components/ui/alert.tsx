41d1fce04c08dd52d8acecd28ba2c1bb2aaff0be
Draco <draco.coder@gmail.com>
2026-07-12 16:58:55 +0800

    chore: extend tanstack-shadcn-starter/src/components/ui/alert.tsx

--- tanstack-shadcn-starter/src/components/ui/alert.tsx
+++ tanstack-shadcn-starter/src/components/ui/alert.tsx
@@ -31,3 +31,36 @@ function Alert({
       className={cn(alertVariants({ variant }), className)}
       {...props}
     />
+  )
+}
+
+function AlertTitle({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="alert-title"
+      className={cn(
+        "col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function AlertDescription({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="alert-description"
+      className={cn(
+        "col-start-2 grid justify-items-start gap-1 text-sm text-muted-foreground [&_p]:leading-relaxed",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Alert, AlertTitle, AlertDescription }
