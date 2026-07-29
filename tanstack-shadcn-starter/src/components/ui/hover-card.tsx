59a8511f7f76a759a96b36b6561a1d6e334175b7
Draco <draco.coder@gmail.com>
2026-07-29 20:55:32 +0800

    chore: add tanstack-shadcn-starter/src/components/ui/hover-card.tsx

---
+++ tanstack-shadcn-starter/src/components/ui/hover-card.tsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+import { HoverCard as HoverCardPrimitive } from "radix-ui"
+
+import { cn } from "#/utils/cn"
+
+function HoverCard({
+  ...props
+}: React.ComponentProps<typeof HoverCardPrimitive.Root>) {
+  return <HoverCardPrimitive.Root data-slot="hover-card" {...props} />
+}
+
+function HoverCardTrigger({
+  ...props
+}: React.ComponentProps<typeof HoverCardPrimitive.Trigger>) {
+  return (
+    <HoverCardPrimitive.Trigger data-slot="hover-card-trigger" {...props} />
+  )
+}
+
+function HoverCardContent({
+  className,
+  align = "center",
