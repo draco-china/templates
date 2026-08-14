77a2786d0a441dc67dc2e4218768d4c9ae794169
Draco <draco.coder@gmail.com>
2026-08-14 11:31:54 +0800

    chore: add tanstack-shadcn-starter/src/features/auth/sign-in/sign-in-2.tsx

---
+++ tanstack-shadcn-starter/src/features/auth/sign-in/sign-in-2.tsx
@@ -0,0 +1,82 @@
+import { Link } from "@tanstack/react-router";
+import { cn } from "#/utils/cn";
+import dashboardDark from "./assets/dashboard-dark.png";
+import dashboardLight from "./assets/dashboard-light.png";
+import { UserAuthForm } from "./components/user-auth-form";
+
+export function SignIn2() {
+  return (
+    <div className="container relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-2 py-8 sm:w-[480px] sm:p-8">
+          <div className="mb-4 flex items-center justify-center">
+            <img
+              alt="Logo"
+              className="me-2 size-6"
+              height={24}
+              src="/favicon.svg"
+              width={24}
+            />
+            <h1 className="font-medium text-xl">TanStack Shadcn Starter</h1>
+          </div>
+        </div>
+        <div className="mx-auto flex w-full max-w-sm flex-col justify-center space-y-2">
+          <div className="flex flex-col space-y-2 text-start">
+            <h2 className="font-semibold text-lg tracking-tight">Sign in</h2>
+            <p className="text-muted-foreground text-sm">
+              Enter your email and password below to log into{" "}
+              <br className="max-sm:hidden" /> your account. Don't have an
+              account?{" "}
+              <Link
+                className="text-nowrap underline underline-offset-4 hover:text-primary"
+                to="/sign-up"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </div>
+          <UserAuthForm />
+          <p className="px-8 text-center text-muted-foreground text-sm">
+            By clicking sign in, you agree to our{" "}
+            <a
+              className="underline underline-offset-4 hover:text-primary"
+              href="/terms"
+            >
+              Terms of Service
+            </a>{" "}
+            and{" "}
+            <a
+              className="underline underline-offset-4 hover:text-primary"
+              href="/privacy"
+            >
+              Privacy Policy
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+
+      <div
+        className={cn(
+          "relative h-full overflow-hidden bg-muted max-lg:hidden",
+          "[&>img]:absolute [&>img]:top-[15%] [&>img]:left-20 [&>img]:h-full [&>img]:w-full [&>img]:select-none [&>img]:object-cover [&>img]:object-top-left"
+        )}
+      >
+        <img
+          alt="TanStack Shadcn Starter"
+          className="dark:hidden"
+          height={1151}
+          src={dashboardLight}
+          width={1024}
+        />
+        <img
+          alt="TanStack Shadcn Starter"
+          className="hidden dark:block"
+          height={1138}
+          src={dashboardDark}
+          width={1024}
+        />
+      </div>
+    </div>
+  );
+}
